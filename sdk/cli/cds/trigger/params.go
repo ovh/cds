869a8ee24540963fead8c74ec1d2aa545b7e79ed
Yannick briffa <yannick.briffa@corp.ovh.com>
2016-10-27 17:25:18 +0200

    feat: (cli) adds management trigger params
    Signed-off-by: Yannick briffa <yannick.briffa@corp.ovh.com>

---
+++ sdk/cli/cds/trigger/params.go
@@ -0,0 +1,33 @@
+package trigger
+
+import (
+	"github.com/spf13/cobra"
+	"stash.ovh.net/cds/sdk"
+)
+
+var cmdParamPipelineTrigger *sdk.PipelineTrigger
+
+func paramTriggerCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "list, update, delete trigger params",
+		Long:  ``,
+	}
+
+	cmd.PersistentPreRun = paramPreRun
+
+	cmd.AddCommand(listParamTriggerCmd())
+	cmd.AddCommand(updateParamTriggerCmd())
+	cmd.AddCommand(deleteParamTriggerCmd())
+
+	return cmd
+}
+
+func paramPreRun(cmd *cobra.Command, args []string) {
+
+	if len(args) < 2 {
+		sdk.Exit("Wrong usage: see %s\n", cmd.Short)
+	}
+
+	cmdParamPipelineTrigger = retrieveTrigger(args[0], args[1])
+}
