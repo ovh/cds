62479e20fc8f4de5667936c216c01dff70213952
François Samin <francois.samin@corp.ovh.com>
2016-11-16 17:32:44 +0100

    fix (worker): windows compilation error

--- engine/worker/memory_windows.go
+++ engine/worker/memory_windows.go
@@ -3,27 +3,26 @@
 package main
 
 import (
-	"fmt"
 	"syscall"
 	"unsafe"
 )
 
-func systemTotalMemory() (uint64, error) (
+func systemTotalMemory() (uint64, error) {
 	var mod = syscall.NewLazyDLL("kernel32.dll")
 	var proc = mod.NewProc("GetPhysicallyInstalledSystemMemory")
 	var mem uint64
 
 	ret, _, err := proc.Call(uintptr(unsafe.Pointer(&mem)))
-    if err != nil {
-        return 0, nil
-    }
+	if err != nil {
+		return 0, nil
+	}
 
 	var mod = syscall.NewLazyDLL("kernel32.dll")
 	var mem uint64
 
 	_, _, err := proc.Call(uintptr(unsafe.Pointer(&mem)))
 	if err != nil {
-        return 0, nil
-    }
-	return mem
+		return 0, nil
+	}
+	return 0, mem
 }
