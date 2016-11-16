588757cead458e6a04a8d62bde75d6a14f2a0187
François Samin <francois.samin@corp.ovh.com>
2016-11-16 17:39:57 +0100

    fix (worker): windows compilation error

--- engine/worker/memory_windows.go
+++ engine/worker/memory_windows.go
@@ -12,17 +12,10 @@ func systemTotalMemory() (uint64, error) {
 	var proc = mod.NewProc("GetPhysicallyInstalledSystemMemory")
 	var mem uint64
 
-	ret, _, err := proc.Call(uintptr(unsafe.Pointer(&mem)))
-	if err != nil {
-		return 0, nil
-	}
-
-	var mod = syscall.NewLazyDLL("kernel32.dll")
-	var mem uint64
-
 	_, _, err := proc.Call(uintptr(unsafe.Pointer(&mem)))
 	if err != nil {
 		return 0, nil
 	}
-	return 0, mem
+
+	return mem, nil
 }
