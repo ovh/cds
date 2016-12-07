fd7cbe1f5bc65a6e6267749f551fe894c834df1e
François Samin <francois.samin+github@gmail.com>
2016-12-07 09:53:02 +0100

    fix (sql): migration file #3 (#87)

--- engine/sql/migrations/003_worker_model-permissions.sql
+++ engine/sql/migrations/003_worker_model-permissions.sql
@@ -5,7 +5,8 @@ ALTER TABLE worker_model ADD COLUMN group_id BIGINT;
 ALTER TABLE worker_model ADD COLUMN created_by JSONB;
 
 UPDATE worker_model set group_id = (SELECT id FROM "group" WHERE name = 'shared.infra' LIMIT 1);
-ALTER TABLE worker_model ALTER COLUMN group_id BIGINT NOT NULL;
+ALTER TABLE worker_model ALTER group_id TYPE BIGINT; 
+ALTER TABLE worker_model ALTER group_id SET NOT NULL;
 
 select create_index('worker_model','IDX_WORKER_MODEL_GROUP_ID','group_id');
 SELECT create_foreign_key('FK_WORKER_MODEL_GROUP', 'worker_model', 'group', 'group_id', 'id');
