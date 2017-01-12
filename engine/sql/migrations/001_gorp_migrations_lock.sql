83096b14ddfcc159c38ea6457f7390102c159357
François Samin <francois.samin+github@gmail.com>
2017-01-12 11:49:04 +0100

    feat (api): merge all creation and migration scripts (#150)

--- engine/sql/migrations/001_gorp_migrations_lock.sql
+++
@@ -1,5 +0,0 @@
--- +migrate Up
-CREATE TABLE IF NOT EXISTS gorp_migrations_lock (id TEXT, locked TIMESTAMP WITH TIME ZONE, unlocked TIMESTAMP WITH TIME ZONE);
-
--- +migrate Down
-DROP TABLE gorp_migrations_lock;
