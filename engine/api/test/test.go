20ae085bd7c1d7ffd9c62d69bbd5c390b27a4e3a
François Samin <francois.samin@corp.ovh.com>
2017-01-16 14:28:18 +0100

    test (api): fix all unit tests flags

--- engine/api/test/test.go
+++ engine/api/test/test.go
@@ -32,18 +32,16 @@ var (
 )
 
 func init() {
-	if flag.Lookup("dbDriver") == nil {
-		flag.StringVar(&DBDriver, "dbDriver", "", "driver")
-		flag.StringVar(&dbUser, "dbUser", "cds", "user")
-		flag.StringVar(&dbPassword, "dbPassword", "cds", "password")
-		flag.StringVar(&dbName, "dbName", "cds", "database name")
-		flag.StringVar(&dbHost, "dbHost", "localhost", "host")
-		flag.StringVar(&dbPort, "dbPort", "15432", "port")
-		flag.StringVar(&dbSSLMode, "sslMode", "disable", "ssl mode")
-		flag.Parse()
+	flag.StringVar(&DBDriver, "dbDriver", "", "driver")
+	flag.StringVar(&dbUser, "dbUser", "cds", "user")
+	flag.StringVar(&dbPassword, "dbPassword", "cds", "password")
+	flag.StringVar(&dbName, "dbName", "cds", "database name")
+	flag.StringVar(&dbHost, "dbHost", "localhost", "host")
+	flag.StringVar(&dbPort, "dbPort", "15432", "port")
+	flag.StringVar(&dbSSLMode, "sslMode", "disable", "ssl mode")
+	flag.Parse()
 
-		log.SetLevel(log.DebugLevel)
-	}
+	log.SetLevel(log.DebugLevel)
 }
 
 type bootstrap func(db *sql.DB) error
