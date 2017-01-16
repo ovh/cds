87e0a2167749dff5d6adc5b5500424df754de1e7
François Samin <francois.samin@corp.ovh.com>
2017-01-16 14:48:41 +0100

    test (api): fix all unit tests flags

--- engine/api/secret/test.go
+++ engine/api/secret/test.go
@@ -2,29 +2,21 @@ package secret
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/ovh/cds/engine/log"
 )
 
-var (
-	DBDriver   string
-	dbUser     string
-	dbPassword string
-	dbName     string
-	dbHost     string
-	dbPort     string
-	dbSSLMode  string
-)
-
 func init() {
 	if flag.Lookup("dbDriver") == nil {
-		flag.StringVar(&DBDriver, "dbDriver", "", "driver")
-		flag.StringVar(&dbUser, "dbUser", "cds", "user")
-		flag.StringVar(&dbPassword, "dbPassword", "cds", "password")
-		flag.StringVar(&dbName, "dbName", "cds", "database name")
-		flag.StringVar(&dbHost, "dbHost", "localhost", "host")
-		flag.StringVar(&dbPort, "dbPort", "15432", "port")
-		flag.StringVar(&dbSSLMode, "sslMode", "disable", "ssl mode")
+		fmt.Println("secret")
+		flag.String("dbDriver", "", "driver")
+		flag.String("dbUser", "cds", "user")
+		flag.String("dbPassword", "cds", "password")
+		flag.String("dbName", "cds", "database name")
+		flag.String("dbHost", "localhost", "host")
+		flag.String("dbPort", "15432", "port")
+		flag.String("sslMode", "disable", "ssl mode")
 		flag.Parse()
 
 		log.SetLevel(log.DebugLevel)
