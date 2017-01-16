bb1f7b8f90a5dd8ce5c896be76830b5d20f20756
François Samin <francois.samin@corp.ovh.com>
2017-01-16 14:13:55 +0100

    test (api): fix all unit tests flags

--- engine/api/auth/test.go
+++ engine/api/auth/test.go
@@ -3,11 +3,10 @@ package auth
 import (
 	"testing"
 
-	"github.com/ovh/cds/engine/api/auth"
 	"github.com/ovh/cds/engine/api/sessionstore"
 )
 
-func TestLocalAuth(t *testing.T) auth.Driver {
+func TestLocalAuth(t *testing.T) Driver {
 	authDriver, err := GetDriver("local", nil, sessionstore.Options{Mode: "local"})
 	NoError(t, err)
 	return authDriver
