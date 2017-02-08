62a795e3f13e3e61f57b72a901c0d918712ae1a4
Guiheux Steven <steven.guiheux+github@gmail.com>
2017-02-08 17:45:32 +0100

    feat: Displayed failed tests + breadcrumb improvment (#242)

--- ui/src/app/shared/breadcrumb/breadcrumb.component.ts
+++ ui/src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -15,6 +15,9 @@ export class BreadcrumbComponent {
     @Input() application: Application;
     @Input() pipeline: Pipeline;
     @Input() action: Action;
+    @Input() version = 0;
+    @Input() buildNumber = 0;
+    @Input() envName: string;
 
     constructor(private _router: Router) { }
 
@@ -27,6 +30,26 @@ export class BreadcrumbComponent {
     }
 
     navigateToPipeline(): void {
-        this._router.navigate(['project', this.project.key, 'pipeline', this.pipeline.name]);
+        let queryParams = { queryParams: {}};
+        if (this.application) {
+            queryParams.queryParams['application'] = this.application.name;
+        }
+        if (this.version && this.buildNumber && this.envName) {
+            queryParams.queryParams['version'] = this.version;
+            queryParams.queryParams['buildNumber'] = this.buildNumber;
+            queryParams.queryParams['envName'] = this.envName;
+        }
+        this._router.navigate(['project', this.project.key, 'pipeline', this.pipeline.name], queryParams);
+    }
+
+    navigateToBuild(): void {
+        let queryParams = { queryParams: {}};
+        queryParams.queryParams['envName'] = this.envName;
+        this._router.navigate([
+            '/project',  this.project.key,
+            'application', this.application.name,
+            'pipeline', this.pipeline.name,
+            'build', this.buildNumber
+        ], queryParams);
     }
 }
