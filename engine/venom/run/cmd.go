65ef0ef99d8079e8375576b6f1acdb546c4b6c46
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-02-13 19:38:13 +0100

    fix(venom): output files
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/venom/run/cmd.go
+++ engine/venom/run/cmd.go
@@ -130,8 +130,10 @@ func outputResult(tests sdk.Tests, elapsed time.Duration) {
 	if outputDir != "" {
 		if format == "xml" {
 			for i, ts := range tests.TestSuites {
+
+				tuple := strings.Split(ts.Name, " ") // remove " [...] from name"
 				dataxml := append([]byte("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"), data...)
-				filename := fmt.Sprintf("%s/test_results_%d_%s.xml", outputDir, i, strings.Replace(ts.Name, " ", "", -1))
+				filename := fmt.Sprintf("%s/test_results_%d_%s.xml", outputDir, i, strings.Replace(tuple[0], " ", "", -1))
 
 				if err := ioutil.WriteFile(filename, dataxml, 0644); err != nil {
 					fmt.Printf("Error while creating file %s, err:%s", filename, err)
