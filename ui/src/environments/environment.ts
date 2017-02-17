c7b9ef6b258f301e07c48240c7ca9cc5fd9903a2
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-02-17 14:08:52 +0100

    feat (ui): update angular and angular/cli version

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,9 +1,4 @@
-// The file for the current environment will overwrite this one during build.
-// Different environments can be found in ./environment.{dev|prod}.ts, and
-// you can create your own and use it with the --env flag.
-// The build system defaults to the dev environment.
-
 export const environment = {
   production: false,
-  apiURL: 'foo.bar'
+  apiURL: 'http://localhost:8081'
 };
