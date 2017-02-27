ad65785e27178e7b8303eb5257fbb31a569e6548
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-02-27 11:04:09 +0100

    feat: add final step

---
+++ ui/src/app/shared/action/step/step.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, EventEmitter, Output} from '@angular/core';
+import {Action} from '../../../model/action.model';
+import {StepEvent} from './step.event';
+
+@Component({
+    selector: 'app-action-step',
+    templateUrl: './step.html',
+    styleUrls: ['./step.scss']
+})
+export class ActionStepComponent {
+
+    @Input() action: Action;
+    @Input() step: Action;
+    @Input() edit: boolean;
+
+    @Output() removeEvent = new EventEmitter<StepEvent>();
+
+    constructor() { }
+
+    updateStepBool(b: boolean): boolean {
+        this.action.hasChanged = true;
+        return !b;
+    }
+
+    removeStep(): void {
+        this.removeEvent.emit(new StepEvent('delete', this.step));
+    }
+}
