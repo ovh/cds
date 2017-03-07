ce155882a274b5cecf182615532bcfe3b7e89a39
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-03-07 15:25:13 +0100

    feat (cli): cds admin queue status
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- cli/cds/admin/queue/cmd.go
+++ cli/cds/admin/queue/cmd.go
@@ -2,7 +2,10 @@ package queue
 
 import (
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 
 	"github.com/ovh/cds/sdk"
@@ -22,7 +25,7 @@ var (
 				if err != nil {
 					fmt.Printf("Error : %v\n", err)
 				}
-				sdk.Exit("You are not allowed to run this command")
+				sdk.Exit("You are not allowed to run this command\n")
 			}
 
 			pbs, err := sdk.GetBuildQueue()
@@ -30,13 +33,63 @@ var (
 				sdk.Exit("Error: %s\n", err)
 			}
 
-			for _, pb := range pbs {
-				fmt.Printf("PB: action:%s status:%s model:%s queued:%s Requirements:%+v\n", pb.Job.Action.Name, pb.Status, pb.Model, pb.Queued, pb.Job.Action.Requirements)
+			if len(pbs) == 0 {
+				sdk.Exit("No job in queue\n")
 			}
 
+			table := tablewriter.NewWriter(os.Stdout)
+			table.SetHeader([]string{"Prj", "App", "Status", "Job", "Queued", "Requirements"})
+			table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+			table.SetCenterSeparator("|")
+
+			var maxQueued time.Duration
+			for _, pb := range pbs {
+				req := ""
+				for _, r := range pb.Job.Action.Requirements {
+					req += fmt.Sprintf("%s(%s):%s ", r.Name, r.Type, r.Value)
+				}
+				prj := getVarsInPbj("cds.project", pb.Parameters)
+				app := getVarsInPbj("cds.application", pb.Parameters)
+				duration := time.Since(pb.Queued)
+				if maxQueued < duration {
+					maxQueued = duration
+				}
+				table.Append([]string{prj, app, pb.Status, pb.Job.Action.Name, round(duration, time.Second).String(), req})
+			}
+			table.SetFooter([]string{"", "Total", fmt.Sprintf("%d", len(pbs)), "Max Queued", round(maxQueued, time.Second).String(), ""})
+
+			table.Render()
 		},
 	}
 )
+
+func round(d, r time.Duration) time.Duration {
+	if r <= 0 {
+		return d
+	}
+	neg := d < 0
+	if neg {
+		d = -d
+	}
+	if m := d % r; m+m < r {
+		d = d - m
+	} else {
+		d = d + r - m
+	}
+	if neg {
+		return -d
+	}
+	return d
+}
+
+func getVarsInPbj(key string, ps []sdk.Parameter) string {
+	for _, p := range ps {
+		if p.Name == key {
+			return p.Value
+		}
+	}
+	return ""
+}
 
 func init() {
 	rootCmd.AddCommand(queueStatusCmd)
