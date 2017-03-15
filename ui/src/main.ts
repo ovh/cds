3753b4eb898a21d0358bc1a247e687df9d27378e
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-03-15 11:24:07 +0100

    feat (ui): update angular cli

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,11 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app/app.module';
-import './polyfills';
+import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
