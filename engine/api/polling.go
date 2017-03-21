c1ebdfa08013f9880ae8e94c7fe4c926c97c7cb3
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-03-21 11:30:15 +0100

    fix (api): use🌯

--- engine/api/polling.go
+++ engine/api/polling.go
@@ -25,13 +25,12 @@ func addPollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap, c 
 	//Load the application
 	app, err := application.LoadByName(db, projectKey, appName, c.User, application.LoadOptions.Default)
 	if err != nil {
-		return err
+		return sdk.WrapError(err, "addPollerHandler> Cannot load application")
 	}
 
 	app.RepositoryPollers, err = poller.LoadByApplication(db, app.ID)
 	if err != nil {
-		log.Warning("addPollerHandler> cannot load application poller %s/%s: %s\n", projectKey, appName, err)
-		return err
+		return sdk.WrapError(err, "addPollerHandler> cannot load application poller %s/%s", projectKey, appName)
 	}
 
 	//Find the pipeline
@@ -46,12 +45,12 @@ func addPollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap, c 
 	//Check if pipeline has been found
 	if pip == nil {
 		log.Warning("addPollerHandler> Cannot load pipeline: %s", pipName)
-		return sdk.ErrPipelineNotFound
+		return sdk.WrapError(sdk.ErrPipelineNotFound, "sdk.ErrPipelineNotFound", pipName)
 	}
 
 	var h sdk.RepositoryPoller
 	if err := UnmarshalBody(r, &h); err != nil {
-		return err
+		return sdk.WrapError(err, "addPollerHandler> Cannot unmarshal body")
 	}
 
 	h.Application = *app
@@ -60,43 +59,38 @@ func addPollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap, c 
 
 	//Check it the application is attached to a repository
 	if app.RepositoriesManager == nil {
-		return sdk.ErrNoReposManagerClientAuth
+		return sdk.WrapError(sdk.ErrNoReposManagerClientAuth, "addPollerHandler> No repository on application")
 	}
 
 	b, e := repositoriesmanager.CheckApplicationIsAttached(db, app.RepositoriesManager.Name, projectKey, appName)
 	if e != nil {
-		log.Warning("addPollerHandler> Cannot check app (%s,%s,%s): %s", app.RepositoriesManager.Name, projectKey, appName, e)
-		return e
+		return sdk.WrapError(e, "addPollerHandler> Cannot check app (%s,%s,%s)", app.RepositoriesManager.Name, projectKey, appName)
 	}
 
 	if !b && app.RepositoryFullname == "" {
-		return sdk.ErrNoReposManagerClientAuth
+		return sdk.WrapError(sdk.ErrNoReposManagerClientAuth, "addPollerHandler> No repository on application")
 	}
 
 	tx, err := db.Begin()
 	if err != nil {
-		log.Warning("addPollerHandler> Cannot start transaction: %s", e)
-		return e
+		return sdk.WrapError(e, "addPollerHandler> Cannot start transaction")
 	}
 	defer tx.Rollback()
 
 	// Insert poller in database
 	err = poller.Insert(db, &h)
 	if err != nil {
-		log.Warning("addPollerHandler: cannot insert poller in db: %s\n", err)
-		return err
+		return sdk.WrapError(err, "addPollerHandler: cannot insert poller in db")
 	}
 
 	err = application.UpdateLastModified(tx, app, c.User)
 	if err != nil {
-		log.Warning("addPollerHandler: cannot update application (%s) lastmodified date: %s\n", app.Name, err)
-		return err
+		return sdk.WrapError(err, "addPollerHandler: cannot update application (%s) lastmodified date", app.Name)
 	}
 
 	err = tx.Commit()
 	if err != nil {
-		log.Warning("addPollerHandler> Cannot commit transaction: %s", e)
-		return e
+		return sdk.WrapError(err, "addPollerHandler> Cannot commit transaction")
 	}
 
 	app.RepositoryPollers = append(app.RepositoryPollers, h)
@@ -118,13 +112,12 @@ func updatePollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap,
 	//Load the application
 	app, err := application.LoadByName(db, projectKey, appName, c.User, application.LoadOptions.Default)
 	if err != nil {
-		return err
+		return sdk.WrapError(err, "updatePollerHandler> Cannot load application")
 	}
 
 	app.RepositoryPollers, err = poller.LoadByApplication(db, app.ID)
 	if err != nil {
-		log.Warning("updatePollerHandler> cannot load application poller %s/%s: %s\n", projectKey, appName, err)
-		return err
+		return sdk.WrapError(err, "updatePollerHandler> cannot load application poller %s/%s", projectKey, appName)
 	}
 
 	//Find the pipeline
@@ -138,8 +131,7 @@ func updatePollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap,
 
 	//Check if pipeline has been found
 	if pip == nil {
-		log.Warning("addPollerHandler> Cannot load pipeline: %s", pipName)
-		return sdk.ErrPipelineNotFound
+		return sdk.WrapError(sdk.ErrPipelineNotFound, "updatePollerHandler> Cannot load pipeline: %s", pipName)
 	}
 
 	var h sdk.RepositoryPoller
@@ -152,8 +144,7 @@ func updatePollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap,
 
 	tx, err := db.Begin()
 	if err != nil {
-		log.Warning("updatePollerHandler> cannot start transaction: %s\n", err)
-		return err
+		return sdk.WrapError(err, "updatePollerHandler> cannot start transaction")
 
 	}
 	defer tx.Rollback()
@@ -161,27 +152,21 @@ func updatePollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap,
 	// Update poller in database
 	err = poller.Update(tx, &h)
 	if err != nil {
-		log.Warning("updatePollerHandler: cannot update poller in db: %s\n", err)
-		return err
+		return sdk.WrapError(err, "updatePollerHandler: cannot update poller in db")
 
 	}
 
 	if err = application.UpdateLastModified(tx, app, c.User); err != nil {
-		log.Warning("updatePollerHandler: cannot update application last modified date: %s\n", err)
-		return err
-
+		return sdk.WrapError(err, "updatePollerHandler: cannot update application last modified date")
 	}
 
 	if err = tx.Commit(); err != nil {
-		log.Warning("updatePollerHandler> cannot commit transaction: %s\n", err)
-		return err
-
+		return sdk.WrapError(err,"updatePollerHandler> cannot commit transaction" )
 	}
 
 	app.RepositoryPollers, err = poller.LoadByApplication(db, app.ID)
 	if err != nil {
-		log.Warning("updatePollerHandler> cannot load pollers: %s\n", err)
-		return err
+		return sdk.WrapError(err, "updatePollerHandler> cannot load pollers")
 	}
 	var errW error
 	app.Workflows, errW = workflow.LoadCDTree(db, projectKey, appName, c.User)
@@ -199,14 +184,13 @@ func getApplicationPollersHandler(w http.ResponseWriter, r *http.Request, db *go
 
 	a, err := application.LoadByName(db, projectName, appName, c.User)
 	if err != nil {
-		log.Warning("getApplicationHooksHandler> cannot load application %s/%s: %s\n", projectName, appName, err)
-		return err
+		return sdk.WrapError(err, "getApplicationHooksHandler> cannot load application %s/%s", projectName, appName)
 	}
 
 	a.RepositoryPollers, err = poller.LoadByApplication(db, a.ID)
 	if err != nil {
 		log.Warning("getApplicationHooksHandler> cannot load application poller %s/%s: %s\n", projectName, appName, err)
-		return err
+		return sdk.WrapError(err, "getApplicationHooksHandler> cannot load application poller %s/%s", projectName, appName,)
 	}
 
 	return WriteJSON(w, r, a.RepositoryPollers, http.StatusOK)
@@ -220,23 +204,19 @@ func getPollersHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap, c
 
 	p, err := pipeline.LoadPipeline(db, projectName, pipelineName, false)
 	if err != nil {
-		if err != sdk.ErrPipelineNotFound {
-			log.Warning("getPollersHandler> cannot load pipeline %s/%s: %s\n", projectName, pipelineName, err)
-		}
-		return err
+		return sdk.WrapError(err, "getPollersHandler> cannot load pipeline %s/%s", projectName, pipelineName)
 	}
 
 	a, err := application.LoadByName(db, projectName, appName, c.User)
 	if err != nil {
 		log.Warning("getPollersHandler> cannot load application %s/%s: %s\n", projectName, appName, err)
-		return err
+		return sdk.WrapError(err, "getPollersHandler> cannot load application %s/%s", projectName, appName)
 
 	}
 
 	poller, err := poller.LoadByApplicationAndPipeline(db, a.ID, p.ID)
 	if err != nil {
-		log.Warning("getPollersHandler> cannot load poller with ID %d %d: %s\n", p.ID, a.ID, err)
-		return err
+		return sdk.WrapError(err, "getPollersHandler> cannot load poller with ID %d %d", p.ID, a.ID)
 
 	}
 
@@ -251,56 +231,46 @@ func deletePollerHandler(w http.ResponseWriter, r *http.Request, db *gorp.DbMap,
 
 	p, err := pipeline.LoadPipeline(db, projectKey, pipelineName, false)
 	if err != nil {
-		if err != sdk.ErrPipelineNotFound {
-			log.Warning("deletePollerHandler> cannot load pipeline %s/%s: %s\n", projectKey, pipelineName, err)
-		}
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot load pipeline %s/%s", projectKey, pipelineName)
 
 	}
 
 	a, err := application.LoadByName(db, projectKey, appName, c.User)
 	if err != nil {
-		log.Warning("deletePollerHandler> cannot load application %s/%s: %s\n", projectKey, appName, err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot load application %s/%s", projectKey, appName)
 	}
 
 	po, err := poller.LoadByApplicationAndPipeline(db, a.ID, p.ID)
 	if err != nil {
-		log.Warning("deletePollerHandler> cannot load poller: %s\n", err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot load poller")
 
 	}
 
 	tx, err := db.Begin()
 	if err != nil {
-		log.Warning("deletePollerHandler> cannot start transaction: %s\n", err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot start transaction")
 
 	}
 	defer tx.Rollback()
 
 	if err = poller.Delete(tx, po); err != nil {
-		log.Warning("deletePollerHandler> cannot delete poller: %s\n", err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot delete poller")
 
 	}
 
 	if err = application.UpdateLastModified(tx, a, c.User); err != nil {
-		log.Warning("deletePollerHandler> cannot update application last modified date: %s\n", err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot update application last modified date")
 
 	}
 
 	if err = tx.Commit(); err != nil {
-		log.Warning("deletePollerHandler> cannot commit transaction: %s\n", err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot commit transaction")
 
 	}
 
 	a.RepositoryPollers, err = poller.LoadByApplication(db, a.ID)
 	if err != nil {
-		log.Warning("deletePollerHandler> cannot load pollers: %s\n", err)
-		return err
+		return sdk.WrapError(err, "deletePollerHandler> cannot load pollers")
 
 	}
 	var errW error
