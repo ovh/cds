ba396e674580a774c112344dbb84315e0277a5f6
François Samin <francois.samin@corp.ovh.com>
2017-03-31 16:12:31 +0200

    test (sdk): fix exportentities unit tests

--- sdk/exportentities/common_test.go
+++ sdk/exportentities/common_test.go
@@ -1,7 +1,6 @@
 package exportentities
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,10 +8,8 @@ import (
 
 func TestReadURL(t *testing.T) {
 	b, f, err := ReadURL("https://raw.githubusercontent.com/ovh/cds/master/contrib/actions/cds-docker-package.hcl", "hcl")
-	fmt.Println(len(b), f, err)
 	assert.True(t, len(b) > 0)
 
 	b, f, err = ReadURL("https://raw.githubusercontent.com/ovh/tat/master/.travis.yml", "yml")
-	fmt.Println(len(b), f, err)
 	assert.True(t, len(b) > 0)
 }
