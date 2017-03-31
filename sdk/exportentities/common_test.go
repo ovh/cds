23a2cb0a730eed86f3a5cb6992d017d30180b197
François Samin <francois.samin@corp.ovh.com>
2017-03-31 16:29:49 +0200

    Merge branch 'master' into fsamin/fix-2017-03-31-16-03-26

--- sdk/exportentities/common_test.go
+++ sdk/exportentities/common_test.go
@@ -7,16 +7,9 @@ import (
 )
 
 func TestReadURL(t *testing.T) {
-<<<<<<< HEAD
 	b, _, _ := ReadURL("https://raw.githubusercontent.com/ovh/cds/master/contrib/actions/cds-docker-package.hcl", "hcl")
 	assert.True(t, len(b) > 0)
 
 	b, _, _ = ReadURL("https://raw.githubusercontent.com/ovh/tat/master/.travis.yml", "yml")
-=======
-	b, f, err := ReadURL("https://raw.githubusercontent.com/ovh/cds/master/contrib/actions/cds-docker-package.hcl", "hcl")
-	assert.True(t, len(b) > 0)
-
-	b, f, err = ReadURL("https://raw.githubusercontent.com/ovh/tat/master/.travis.yml", "yml")
->>>>>>> master
 	assert.True(t, len(b) > 0)
 }
