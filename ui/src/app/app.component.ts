df6f55d0b5a9e720fbfa63cb5ed9aff8372fbd6c
Guiheux Steven <steven.guiheux+github@gmail.com>
2017-04-14 15:40:05 +0200

    feat (ui): add language switch. Close #461 (#514)

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,31 +1,50 @@
-import {Component, OnInit, NgZone} from '@angular/core';
+import {Component, OnInit, NgZone, OnDestroy} from '@angular/core';
 import {TranslateService} from 'ng2-translate';
 import {AuthentificationStore} from './service/auth/authentification.store';
 import {environment} from '../environments/environment';
 import {WarningStore} from './service/warning/warning.store';
 import {CDSWorker} from './shared/worker/worker';
 import {Subscription} from 'rxjs/Rx';
+import {LanguageStore} from './service/language/language.store';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit {
+export class AppComponent  implements OnInit, OnDestroy {
 
     open: boolean;
     isConnected = false;
     worker: CDSWorker;
     workerSubscription: Subscription;
+    languageSubscriber: Subscription;
     zone: NgZone;
 
-    constructor(private _translate: TranslateService,
+    constructor(private _translate: TranslateService, private _language: LanguageStore,
                 private _authStore: AuthentificationStore, private _warnStore: WarningStore) {
         this.zone = new NgZone({enableLongStackTrace: false});
         _translate.addLangs(['en', 'fr']);
         _translate.setDefaultLang('en');
         let browserLang = _translate.getBrowserLang();
         _translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+
+        this.languageSubscriber = this._language.get().subscribe( l => {
+            if (l) {
+                _translate.use(l);
+            } else {
+                _language.set(browserLang.match(/en|fr/) ? browserLang : 'en');
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        if (this.workerSubscription) {
+            this.workerSubscription.unsubscribe();
+        }
+        if (this.languageSubscriber) {
+            this.languageSubscriber.unsubscribe();
+        }
     }
 
     ngOnInit(): void {
