387c8a4ee29773b33393e15375b19e05b7f4b977
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-04-26 17:39:10 +0200

    chore: wip Dockerfile
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- cli/cds/build.sh
+++ cli/cds/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 for GOOS in windows darwin linux freebsd; do
-    for GOARCH in 386 amd64 arm; do
+    for GOARCH in amd64 arm; do
         if [[ $GOARCH == "arm" && $GOOS != "linux" ]]; then
           continue;
         fi;
