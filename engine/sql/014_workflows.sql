8f1eec815132df1b86f30b0b3650a905ea0cd872
François Samin <francois.samin@corp.ovh.com>
2017-04-28 11:32:05 +0200

    feat (sql): workflows indexes and foreign keys

--- engine/sql/014_workflows.sql
+++ engine/sql/014_workflows.sql
@@ -6,11 +6,18 @@ CREATE TABLE IF NOT EXISTS "workflow" (
     root_node_id BIGINT
 );
 
+SELECT create_index('workflow', 'IDX_WORKFLOW_NAME', 'name');
+SELECT create_foreign_key('FK_WORKFLOW_PROJECT', 'workflow', 'project', 'project_id', 'id');
+SELECT create_foreign_key('FK_WORKFLOW_ROOT_NODE', 'workflow', 'workflow_node', 'root_node_id', 'id');
+
 CREATE TABLE IF NOT EXISTS "workflow_node" (
     id BIGSERIAL PRIMARY KEY,
     workflow_id BIGINT NOT NULL,
     pipeline_id BIGINT NOT NULL
 );
+
+SELECT create_foreign_key('FK_WORKFLOW_NODE_WORKFLOW', 'workflow_node', 'workflow', 'workflow_id', 'id');
+SELECT create_foreign_key('FK_WORKFLOW_NODE_PIPELINE', 'workflow_node', 'pipeline', 'pipeline_id', 'id');
 
 CREATE TABLE IF NOT EXISTS "workflow_node_trigger" (
     id BIGSERIAL PRIMARY KEY,
@@ -19,12 +26,19 @@ CREATE TABLE IF NOT EXISTS "workflow_node_trigger" (
     conditions JSONB
 );
 
+SELECT create_foreign_key('FK_WORKFLOW_TRIGGER_WORKFLOW_NODE', 'workflow_node_trigger', 'workflow_node', 'workflow_node_id', 'id');
+SELECT create_foreign_key('FK_WORKFLOW_TRIGGER_WORKFLOW_NODE', 'workflow_node_trigger', 'workflow_node', 'workflow_dest_node_id', 'id');
+
 CREATE TABLE IF NOT EXISTS "workflow_node_context" (
     id BIGSERIAL PRIMARY KEY,
     workflow_node_id BIGINT NOT NULL,
     application_id BIGINT,
     environment_id BIGINT
 );
+
+SELECT create_foreign_key('FK_WORKFLOW_CONTEXT_WORKFLOW_NODE', 'workflow_node_context', 'workflow_node', 'workflow_node_id', 'id');
+SELECT create_foreign_key('FK_WORKFLOW_NODE_APPLICATION', 'workflow_node_context', 'application', 'application_id', 'id');
+SELECT create_foreign_key('FK_WORKFLOW_NODE_ENVIRONMENT', 'workflow_node_context', 'environment', 'environment_id', 'id');
 
 CREATE TABLE IF NOT EXISTS "workflow_node_hook" (
     id BIGSERIAL PRIMARY KEY,
@@ -34,6 +48,10 @@ CREATE TABLE IF NOT EXISTS "workflow_node_hook" (
     conditions JSONB,
     config JSONB
 );
+
+SELECT create_index('workflow_node_hook', 'IDX_WORKFLOW_NODE_HOOK_UUID', 'uuid');
+SELECT create_foreign_key('FK_WORKFLOW_NODE_HOOK_WORKFLOW_NODE', 'workflow_node_hook', 'workflow_node', 'workflow_node_id', 'id');
+SELECT create_foreign_key('FK_WORKFLOW_NODE_HOOK_WORKFLOW_HOOK_MODEL', 'workflow_node_hook', 'workflow_hook_model', 'workflow_hook_model_id', 'id');
 
 CREATE TABLE IF NOT EXISTS "workflow_hook_model" (
     id BIGSERIAL PRIMARY KEY,
