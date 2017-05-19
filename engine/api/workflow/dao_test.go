69be59198a1544632761bd11531d3a0604fb02c6
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-05-19 17:35:59 +0200

    fix (api): tu on workflow

--- engine/api/workflow/dao_test.go
+++ engine/api/workflow/dao_test.go
@@ -361,12 +361,13 @@ func TestUpdateSimpleWorkflowWithApplicationEnvPipelineParametersAndPayload(t *t
 
 	t.Logf("Modifying workflow... with %d instead of %d", app2.ID, app.ID)
 
+	w1old := w1
 	w1.Name = "test_2"
 	w1.Root.PipelineID = pip2.ID
 	w1.Root.Context.Application = &app2
 	w1.Root.Context.ApplicationID = app2.ID
 
-	test.NoError(t, Update(db, w1, u))
+	test.NoError(t, Update(db, w1, w1old, u))
 
 	t.Logf("Reloading workflow...")
 	w2, err := LoadByID(db, w1.ID, u)
@@ -821,6 +822,7 @@ func TestUpdateWorkflowWithJoins(t *testing.T) {
 	w1, err := Load(db, key, "test_1", u)
 	test.NoError(t, err)
 
+	w1old := w1
 	w1.Name = "test_2"
 	w1.Root.PipelineID = pip2.ID
 	w1.Root.Pipeline = pip2
@@ -840,7 +842,7 @@ func TestUpdateWorkflowWithJoins(t *testing.T) {
 		},
 	}
 
-	test.NoError(t, Update(db, w1, u))
+	test.NoError(t, Update(db, w1, w1old, u))
 
 	t.Logf("Reloading workflow...")
 	w2, err := LoadByID(db, w1.ID, u)
