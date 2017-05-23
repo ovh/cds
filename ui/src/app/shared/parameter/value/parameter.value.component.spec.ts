b56b1b7796eb9f56e4228ca698e54c576517f8f7
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-05-23 16:34:58 +0200

    fix (ui): fix TU

--- ui/src/app/shared/parameter/value/parameter.value.component.spec.ts
+++ ui/src/app/shared/parameter/value/parameter.value.component.spec.ts
@@ -81,7 +81,7 @@ describe('CDS: Parameter Value Component', () => {
         let compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('codemirror')).toBeTruthy('textarea must be displayed');
 
-        tick(100);
+        tick(500);
     }));
 
     it('should create a select for pipeline', fakeAsync( () => {
