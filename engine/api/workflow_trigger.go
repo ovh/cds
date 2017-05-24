ebbf6b117e6056ec6cbc0dbe9a85077ee6b4e1e2
Guiheux Steven <steven.guiheux+github@gmail.com>
2017-05-24 17:07:09 +0200

    fix(api): fix constraint on workflow_node_join_src (#661)

--- engine/api/workflow_trigger.go
+++ engine/api/workflow_trigger.go
@@ -21,3 +21,17 @@ func getWorkflowTriggerCondition(w http.ResponseWriter, r *http.Request, db *gor
 
 	return WriteJSON(w, r, data, http.StatusOK)
 }
+
+func getWorkflowTriggerJoinCondition(w http.ResponseWriter, r *http.Request, db *gorp.DbMap, c *context.Ctx) error {
+	operators := []string{"=", "!=", "<", "<=", ">=", ">"}
+
+	data := struct {
+		Operators      []string `json:"operators" db:"id"`
+		ConditionNames []string `json:"names" db:"id"`
+	}{
+		operators,
+		[]string{"git.branch"},
+	}
+
+	return WriteJSON(w, r, data, http.StatusOK)
+}
