09a4c37154d652c85404f9bc29a2dbc596beb001
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-06-07 11:41:55 +0200

    fix (api): add indexes

--- engine/sql/023_workflows_artifacts.sql
+++ engine/sql/023_workflows_artifacts.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS "workflow_run_artifacts" (
 
 SELECT create_foreign_key_idx_cascade('FK_WORKFLOW_RUN_WORKFLOW', 'workflow_run_artifacts', 'workflow_run', 'workflow_id', 'id');
 SELECT create_foreign_key_idx_cascade('FK_WORKFLOW_RUN_PROJECT', 'workflow_run_artifacts', 'workflow_node_run', 'workflow_node_run_id', 'id');
-
+SELECT create_index('workflow_run_artifacts', 'IDX_WORKFLOW_ARTIFACT_WORKFLOW_ID', 'workflow_id');
+SELECT create_index('workflow_run_artifacts', 'IDX_WORKFLOW_ARTIFACT_WORKFLOW_NODE_RUN_ID', 'workflow_node_run_id');
 
 -- +migrate Down
 DROP TABLE workflow_run_artifacts;
