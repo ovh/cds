ef2989f7e27c2bde0b3f81a9a7b33ab58abd1a83
Guiheux Steven <steven.guiheux+github@gmail.com>
2017-06-28 17:37:11 +0200

    feat (ui): add hyperlink on history (#780)

--- ui/src/app/shared/history/history.component.ts
+++ ui/src/app/shared/history/history.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Table} from '../table/table';
 import {PipelineBuild} from '../../model/pipeline.model';
+import {Project} from '../../model/project.model';
 
 @Component({
     selector: 'app-history',
@@ -9,6 +10,7 @@ import {PipelineBuild} from '../../model/pipeline.model';
 })
 export class HistoryComponent extends Table {
 
+    @Input() project: Project;
     @Input() history: Array<PipelineBuild>;
     @Input() currentBuild: PipelineBuild;
 
