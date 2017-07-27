de8018a0c90873f7ff64c80309abc467f60bbde6
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-07-27 14:13:22 +0200

    test header

--- engine/api/lastupdate.go
+++ engine/api/lastupdate.go
@@ -164,6 +164,7 @@ func (b *LastUpdateBroker) ServeHTTP(w http.ResponseWriter, r *http.Request, db 
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("X-Accel-Buffering", "no")
 
 leave:
 	for {
