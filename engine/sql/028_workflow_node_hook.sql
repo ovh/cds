8ef7472838ad061dfa690dc66894b73d748e0d37
François Samin <francois.samin@corp.ovh.com>
2017-07-28 17:05:47 +0200

    fix (sql): workflow hook model

--- engine/sql/028_workflow_node_hook.sql
+++ engine/sql/028_workflow_node_hook.sql
@@ -2,7 +2,7 @@
 ALTER TABLE workflow_hook_model 
     ADD COLUMN author VARCHAR(256),
     ADD COLUMN description TEXT,
-    ADD COLUMN identifier VARCHAR(256)
+    ADD COLUMN identifier VARCHAR(256),
     ADD COLUMN icon BYTEA;
 
 SELECT create_unique_index('workflow_hook_model', 'IDX_WORKFLOW_HOOK_MODEL_NAME', 'name');
