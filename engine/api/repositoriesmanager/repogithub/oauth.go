06d01e328790c25fedb0f008f95f3d3cfec75acc
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2017-07-28 14:10:46 +0200

    Revert "fix(api): fix oauth token creation for github"
    This reverts commit dc849e2d9ea2a8d7dac88bfed9b64e1f57ec7471.

--- engine/api/repositoriesmanager/repogithub/oauth.go
+++ engine/api/repositoriesmanager/repogithub/oauth.go
@@ -10,8 +10,6 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/ovh/cds/engine/api/secret"
-	"github.com/ovh/cds/engine/api/secret/filesecretbackend"
 	"github.com/ovh/cds/sdk"
 	"github.com/ovh/cds/sdk/log"
 )
@@ -62,18 +60,13 @@ func New(ClientID, ClientSecret, AuthorizationCallbackURL string) *GithubConsume
 }
 
 func (g *GithubConsumer) getClientSecretValue() ([]byte, error) {
-	switch secret.Client.(type) {
-	case *filesecretbackend.FileSecretBackend:
-		return []byte(g.ClientSecret), nil
-	default:
-		b, err := ioutil.ReadFile(g.ClientSecret)
-		if err != nil {
-			log.Error("GithubConsumer> Unable to read client secret value %s : %s", g.ClientSecret, err)
-			return nil, err
-		}
-		b = bytes.Replace(b, []byte{'\n'}, []byte{}, -1)
-		return b, err
+	b, err := ioutil.ReadFile(g.ClientSecret)
+	if err != nil {
+		log.Error("GithubConsumer> Unable to read client secret value %s : %s", g.ClientSecret, err)
+		return nil, err
 	}
+	b = bytes.Replace(b, []byte{'\n'}, []byte{}, -1)
+	return b, err
 }
 
 //Data returns a serilized version of specific data
--- engine/api/secret/filesecretbackend/filesecretbackend.go
+++ engine/api/secret/filesecretbackend/filesecretbackend.go
@@ -9,22 +9,22 @@ import (
 	"github.com/ovh/cds/engine/api/secret/secretbackend"
 )
 
-type FileSecretBackend struct {
+type fileSecretBackend struct {
 	secrets map[string]string
 }
 
 //Client return a SecretBackend
 func Client(opts map[string]string) secretbackend.Driver {
-	c := &FileSecretBackend{}
+	c := &fileSecretBackend{}
 	c.Init(secretbackend.NewOptions(opts))
 	return c
 }
 
-func (c *FileSecretBackend) Name() string {
+func (c *fileSecretBackend) Name() string {
 	return "File Secret Backend - CDS Embbeded"
 }
 
-func (c *FileSecretBackend) Init(opts secretbackend.MapVar) error {
+func (c *fileSecretBackend) Init(opts secretbackend.MapVar) error {
 	dir := opts.Get("secret_directory")
 	if dir == "" {
 		dir = ".secrets"
@@ -33,7 +33,7 @@ func (c *FileSecretBackend) Init(opts secretbackend.MapVar) error {
 }
 
 //Load Loads secrets from a directory
-func (c *FileSecretBackend) load(dirname string) error {
+func (c *fileSecretBackend) load(dirname string) error {
 	c.secrets = map[string]string{}
 	d, err := os.Open(dirname)
 	if err != nil {
@@ -72,6 +72,6 @@ func (c *FileSecretBackend) load(dirname string) error {
 }
 
 //GetSecrets is for dev purpose only
-func (c *FileSecretBackend) GetSecrets() secretbackend.Secrets {
+func (c *fileSecretBackend) GetSecrets() secretbackend.Secrets {
 	return *secretbackend.NewSecrets(c.secrets)
 }
