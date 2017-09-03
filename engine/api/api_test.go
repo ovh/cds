90e066a29607b152993350114b96ac303d837b58
François SAMIN <francois.samin@gmail.com>
2017-09-03 18:03:35 +0200

    test (api): api unit tests are compiling \o/
    Signed-off-by: François SAMIN <francois.samin@gmail.com>

---
+++ engine/api/api_test.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-gorp/gorp"
+	"github.com/gorilla/mux"
+
+	"github.com/ovh/cds/engine/api/auth"
+	"github.com/ovh/cds/engine/api/test"
+)
+
+func newTestAPI(t *testing.T, bootstrapFunc ...test.Bootstrapf) (*API, *gorp.DbMap, *Router) {
+	db := test.SetupPG(t, bootstrapFunc...)
+	router := newRouter(auth.TestLocalAuth(t, db), mux.NewRouter(), "/"+test.GetTestName(t))
+	return &API{
+		StartupTime:         time.Now(),
+		Router:              router,
+		DBConnectionFactory: test.DBConnectionFactory,
+		Config:              Configuration{},
+	}, db, router
+}
