1e37ce1b58e67974441ba990ed1e74309b6f6d5c
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-09-14 14:13:46 +0200

    doc: key lower case
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/hatchery/vsphere/types.go
+++ engine/hatchery/vsphere/types.go
@@ -12,37 +12,37 @@ import (
 
 // HatcheryConfiguration is the configuration for hatchery
 type HatcheryConfiguration struct {
-	hatchery.CommonConfiguration
+	hatchery.CommonConfiguration `toml:"commonConfiguration"`
 
 	// User vsphere-user
-	VSphereUser string `default:"" commented:"true" comment:"VSphere User"`
+	VSphereUser string `toml:"user" default:"" commented:"true" comment:"VSphere User"`
 
 	// Endpoint vsphere-endpoint
-	VSphereEndpoint string `default:"" commented:"true" comment:"VShpere Endpoint, example:pcc-11-222-333-444.ovh.com"`
+	VSphereEndpoint string `toml:"endpoint" default:"" commented:"true" comment:"VShpere Endpoint, example:pcc-11-222-333-444.ovh.com"`
 
 	// Password vsphere-password
-	VSpherePassword string `default:"" commented:"true" comment:"VShpere Password"`
+	VSpherePassword string `toml:"password" default:"" commented:"true" comment:"VShpere Password"`
 
 	// DatacenterString vsphere-datacenter
-	VSphereDatacenterString string `default:"" commented:"true" comment:"VSphere Datacenter"`
+	VSphereDatacenterString string `toml:"datacenterString" default:"" commented:"true" comment:"VSphere Datacenter"`
 
 	// DatastoreString vsphere-datastore
-	VSphereDatastoreString string `default:"" commented:"true" comment:"VSphere Datastore"`
+	VSphereDatastoreString string `toml:"datastoreString" default:"" commented:"true" comment:"VSphere Datastore"`
 
 	// NetworkString vsphere-network VM Network
-	VSphereNetworkString string `default:"" commented:"true" comment:"VShpere Network"`
+	VSphereNetworkString string `toml:"networkString" default:"" commented:"true" comment:"VShpere Network"`
 
 	// CardName vsphere-ethernet-card Name of the virtual ethernet card
-	VSphereCardName string `default:"e1000" commented:"true" comment:"Name of the virtual ethernet card"`
+	VSphereCardName string `toml:"cardName" default:"e1000" commented:"true" comment:"Name of the virtual ethernet card"`
 
 	// WorkerTTL Worker TTL (minutes)
-	WorkerTTL int `default:"30" commented:"true" comment:"Worker TTL (minutes)"`
+	WorkerTTL int `toml:"workerTTL" default:"30" commented:"true" comment:"Worker TTL (minutes)"`
 
 	// DisableCreateImage if true: hatchery does not create vsphere image when a worker model is updated
-	DisableCreateImage bool `default:"false" commented:"true" comment:"if true: hatchery does not create vsphere image when a worker model is updated"`
+	DisableCreateImage bool `toml:"disableCreateImage" default:"false" commented:"true" comment:"if true: hatchery does not create vsphere image when a worker model is updated"`
 
 	// CreateImageTimeout max wait for create a vsphere image (in seconds)
-	CreateImageTimeout int `default:"180" commented:"true" comment:"max wait for create a vsphere image (in seconds)"`
+	CreateImageTimeout int `toml:"createImageTimeout" default:"180" commented:"true" comment:"max wait for create a vsphere image (in seconds)"`
 }
 
 // HatcheryVSphere spawns vm
