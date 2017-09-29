c1ab4f91bdcd0c1b2edf4d6e46daae2bc42f2d4c
François Samin <francois.samin+github@gmail.com>
2017-09-29 13:25:48 +0200

    feat (api): Improve workflow join trigger condition (#1190)

--- engine/api/workflow_trigger.go
+++ engine/api/workflow_trigger.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ovh/cds/engine/api/project"
 	"github.com/ovh/cds/engine/api/workflow"
 	"github.com/ovh/cds/sdk"
+	"github.com/ovh/cds/sdk/log"
 )
 
 func (api *API) getWorkflowTriggerConditionHandler() Handler {
@@ -137,6 +138,7 @@ func (api *API) getWorkflowTriggerJoinConditionHandler() Handler {
 			Operators: sdk.WorkflowConditionsOperators,
 		}
 
+		//First we merge all build parameters from all source nodes
 		allparams := map[string]string{}
 		for _, i := range j.SourceNodeIDs {
 			params, errp := workflow.NodeBuildParameters(proj, wf, wr, i, getUser(ctx))
@@ -145,9 +147,42 @@ func (api *API) getWorkflowTriggerJoinConditionHandler() Handler {
 			}
 			allparams = sdk.ParametersMapMerge(allparams, sdk.ParametersToMap(params))
 		}
-
 		for k := range allparams {
 			data.ConditionNames = append(data.ConditionNames, k)
+		}
+
+		//Then we push all ancestors status if needed
+		for _, i := range j.SourceNodeIDs {
+			refNode := wf.GetNode(i)
+			if refNode == nil {
+				log.Error("getWorkflowTriggerJoinConditionHandler> Unable to get node %d", i)
+				continue
+			}
+			var found bool
+			for _, s := range data.ConditionNames {
+				if s == "workflow."+refNode.Name+".status" {
+					found = true
+				}
+			}
+			if !found {
+				data.ConditionNames = append(data.ConditionNames, "workflow."+refNode.Name+".status")
+			}
+			ancestorIds := refNode.Ancestors(wf)
+			for _, aID := range ancestorIds {
+				ancestor := wf.GetNode(aID)
+				if ancestor == nil {
+					continue
+				}
+				var found bool
+				for _, s := range data.ConditionNames {
+					if s == "workflow."+ancestor.Name+".status" {
+						found = true
+					}
+				}
+				if !found {
+					data.ConditionNames = append(data.ConditionNames, "workflow."+ancestor.Name+".status")
+				}
+			}
 		}
 
 		return WriteJSON(w, r, data, http.StatusOK)
