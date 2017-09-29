1a7db85cd715ea8fd3f93b0626bca37ead3f957b
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2017-09-29 10:46:45 +0200

    fix(ui): fix ui for loading and stop button on workflow node
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- engine/api/workflow/execute_node_run.go
+++ engine/api/workflow/execute_node_run.go
@@ -133,12 +133,6 @@ func execute(db gorp.SqlExecutor, store cache.Store, p *sdk.Project, n *sdk.Work
 		}
 	}
 
-	if n.Status == sdk.StatusFail.String() {
-		if err := updateWorkflowRunStatus(db, updatedWorkflowRun.ID, sdk.StatusFail.String()); err != nil {
-			return sdk.WrapError(err, "workflow.execute> Unable to update workflow run status with id %d for status %s ", updatedWorkflowRun.ID, sdk.StatusFail.String())
-		}
-	}
-
 	return nil
 }
 
