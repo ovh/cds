f11eb9f3d370c7b940faffc1c620bb1e3e9772f3
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2017-10-04 11:17:45 +0200

    feat(api): feat fork, improve sql request
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- engine/sql/047_pb_fork.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-ALTER TABLE pipeline_build ADD COLUMN vcs_remote_url TEXT DEFAULT '';
-ALTER TABLE pipeline_build ADD COLUMN vcs_remote TEXT DEFAULT '';
-
--- +migrate Down
-ALTER TABLE pipeline_build DROP COLUMN vcs_remote_url;
-ALTER TABLE pipeline_build DROP COLUMN vcs_remote;
