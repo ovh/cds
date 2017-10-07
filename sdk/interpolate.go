08ed5972e2b149b2f891b8ef4aa8a340de902b61
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-10-07 23:44:46 +0200

    fix (api): load variable containing secret from worker
    Keep value '{{.cds.foo}}' if foo is a app secret var to let worker use it
    
    This use http://masterminds.github.io/sprig/defaults.html for default value if var is not replace with an existing variable
    
    all funcs from http://masterminds.github.io/sprig/ are now available \o/
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- sdk/interpolate.go
+++ sdk/interpolate.go
@@ -6,39 +6,12 @@ import (
 	"html/template"
 	"regexp"
 	"strings"
+
+	"github.com/Masterminds/sprig"
 )
 
-// InterpolateFilterFunc is the type of a filter func
-type InterpolateFilterFunc func() (string, func(string) string)
-
-// InterpolateFilters provides some standers filters
-var InterpolateFilters = struct {
-	Title  InterpolateFilterFunc
-	Lower  InterpolateFilterFunc
-	Upper  InterpolateFilterFunc
-	Escape InterpolateFilterFunc
-}{
-	Title: func() (string, func(string) string) {
-		return "title", strings.Title
-	},
-	Lower: func() (string, func(string) string) {
-		return "lower", strings.ToLower
-	},
-	Upper: func() (string, func(string) string) {
-		return "upper", strings.ToUpper
-	},
-	Escape: func() (string, func(string) string) {
-		return "escape", func(s string) string {
-			s1 := strings.Replace(s, "_", "-", -1)
-			s1 = strings.Replace(s1, "/", "-", -1)
-			s1 = strings.Replace(s1, ".", "-", -1)
-			return s1
-		}
-	},
-}
-
 // Interpolate returns interpolated input with vars
-func Interpolate(input string, vars map[string]string, filters ...InterpolateFilterFunc) (string, error) {
+func Interpolate(input string, vars map[string]string) (string, error) {
 	data := map[string]string{}
 
 	for k, v := range vars {
@@ -55,13 +28,15 @@ func Interpolate(input string, vars map[string]string, filters ...InterpolateFil
 		}
 	}
 
-	funcMap := template.FuncMap{}
-	for i := range filters {
-		s, fun := filters[i]()
-		funcMap[s] = fun
+	// in input, replace {{.cds.foo.bar}} per {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
+	// t.Execute will call "default" function, documented on http://masterminds.github.io/sprig/defaults.html
+	re := regexp.MustCompile("{{\\.(.*)}}")
+	sm := re.FindStringSubmatch(input)
+	if len(sm) > 0 {
+		input = strings.Replace(input, sm[0], "{{ default \"{{."+sm[1]+"}}\" ."+sm[1]+"}}", -1)
 	}
 
-	t, err := template.New("input").Funcs(funcMap).Parse(input)
+	t, err := template.New("input").Funcs(sprig.FuncMap()).Parse(input)
 	if err != nil {
 		return "", fmt.Errorf("Invalid template format: %s", err.Error())
 	}
