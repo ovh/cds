a38dfc7cc835aadf6a112e8a540dd52cca79cc29
Sacha Froment <sfroment42@gmail.com>
2017-10-12 10:23:15 +0200

    Change file download for x86_64 route (#1286)

--- engine/api/api_routes.go
+++ engine/api/api_routes.go
@@ -41,7 +41,7 @@ func (api *API) InitRouter() {
 
 	// Download file
 	r.ServeAbsoluteFile("/download/cli/x86_64", path.Join(api.Config.Directories.Download, "cds-linux-amd64"), "cds")
-	r.ServeAbsoluteFile("/download/worker/x86_64", path.Join(api.Config.Directories.Download, "cds-worker-linux-386"), "worker")
+	r.ServeAbsoluteFile("/download/worker/x86_64", path.Join(api.Config.Directories.Download, "cds-worker-linux-amd64"), "worker")
 	r.ServeAbsoluteFile("/download/worker/windows_x86_64", path.Join(api.Config.Directories.Download, "cds-worker-windows-amd64"), "worker.exe")
 	r.ServeAbsoluteFile("/download/worker/i386", path.Join(api.Config.Directories.Download, "cds-worker-linux-386"), "worker")
 	r.ServeAbsoluteFile("/download/worker/i686", path.Join(api.Config.Directories.Download, "cds-worker-linux-386"), "worker")
