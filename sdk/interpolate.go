8a75d6ee4047dd1d05429f7c030c9a7d47f48e15
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-11-04 15:19:02 +0100

    cr
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- sdk/interpolate.go
+++ sdk/interpolate.go
@@ -36,7 +36,7 @@ func Interpolate(input string, vars map[string]string) (string, error) {
 		}
 	}
 
-	// in input, replace {{.cds.foo.bar}} per {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
+	// in input, replace {{.cds.foo.bar}} with {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
 	// t.Execute will call "default" function, documented on http://masterminds.github.io/sprig/defaults.html
 	sm := interpolateRegex.FindAllStringSubmatch(input, -1)
 	if len(sm) > 0 {
@@ -48,16 +48,16 @@ func Interpolate(input string, vars map[string]string) (string, error) {
 				// see test "two same unknown" on InterpolateTest
 				if _, ok := alreadyReplaced[sm[i][1]]; !ok {
 					if _, ok := defaults[e]; !ok {
-						// replace {{.cds.foo.bar}} per {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
+						// replace {{.cds.foo.bar}} with {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
 						// with cds.foo.bar unknown from vars
 						nameWithDot := strings.Replace(e, "__", ".", -1)
 						input = strings.Replace(input, sm[i][1], "{{ default \"{{"+nameWithDot+"}}\" "+e+" }}", -1)
 					} else if _, ok := empty[e]; !ok {
-						// replace {{.cds.foo.bar}} per {{ default "" .cds.foo.bar}}
+						// replace {{.cds.foo.bar}} with {{ default "" .cds.foo.bar}}
 						// with cds.foo.bar knowned, but with value empty string ""
 						input = strings.Replace(input, sm[i][1], "{{ default \"\" "+e+" }}", -1)
 					} else {
-						// replace {{.cds.foo.bar}} per {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
+						// replace {{.cds.foo.bar}} with {{ default "{{.cds.foo.bar}}" .cds.foo.bar}}
 						// with cds.foo.bar knowned from vars
 						input = strings.Replace(input, sm[i][1], "{{ default \"{{"+defaults[e[1:]]+"}}\" "+e+" }}", -1)
 					}
