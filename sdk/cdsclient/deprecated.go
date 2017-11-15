93e39573ff1899d3a1d5814620051878cce7cc91
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-11-15 15:53:34 +0100

    feat (sdk): deprecated routes
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

---
+++ sdk/cdsclient/deprecated.go
@@ -0,0 +1,61 @@
+package cdsclient
+
+import (
+	"fmt"
+
+	"github.com/ovh/cds/sdk"
+)
+
+func (c *client) ApplicationPipelinesAttach(projectKey string, appName string, pipelineNames ...string) error {
+	uri := fmt.Sprintf("/project/%s/application/%s/pipeline/attach", projectKey, appName)
+	code, err := c.PostJSON(uri, pipelineNames, nil)
+	if err != nil {
+		return err
+	}
+
+	if code >= 300 {
+		return fmt.Errorf("cds: api error (%d)", code)
+	}
+
+	return nil
+}
+
+func (c *client) ApplicationPipelineTriggerAdd(t *sdk.PipelineTrigger) error {
+	uri := fmt.Sprintf("/project/%s/application/%s/pipeline/%s/trigger", t.SrcProject.Key, t.SrcApplication.Name, t.SrcPipeline.Name)
+
+	if t.SrcEnvironment.Name != "" {
+		uri = fmt.Sprintf("%s?env=%s", uri, t.SrcEnvironment.Name)
+	}
+
+	app := &sdk.Application{}
+	code, err := c.PostJSON(uri, t, app)
+	if err != nil {
+		return err
+	}
+
+	if code >= 300 {
+		return fmt.Errorf("cds: api error (%d)", code)
+	}
+
+	return nil
+}
+
+func (c *client) ApplicationPipelineTriggersGet(projectKey string, appName string, pipelineName string, envName string) ([]sdk.PipelineTrigger, error) {
+	uri := fmt.Sprintf("/project/%s/application/%s/pipeline/%s/trigger/source", projectKey, appName, pipelineName)
+
+	if envName != "" {
+		uri = fmt.Sprintf("%s?env=%s", uri, envName)
+	}
+
+	var triggers []sdk.PipelineTrigger
+	code, err := c.GetJSON(uri, &triggers)
+	if err != nil {
+		return nil, err
+	}
+
+	if code >= 300 {
+		return nil, fmt.Errorf("cds: api error (%d)", code)
+	}
+
+	return triggers, nil
+}
