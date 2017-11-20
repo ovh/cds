74cb80e1306106b7fd978e536c4061f7ad2edacd
Yvonnick Esnault <yesnault@users.noreply.github.com>
2017-11-20 10:02:08 +0100

    feat (api, cdsctl): handler to get some DB Times (#1517)

--- cli/cdsctl/health.go
+++ cli/cdsctl/health.go
@@ -1,13 +1,46 @@
 package main
 
-import "github.com/ovh/cds/cli"
+import (
+	"fmt"
 
-var healthCmd = cli.Command{
-	Name:  "health",
-	Short: "Check CDS health",
+	"github.com/spf13/cobra"
+
+	"github.com/ovh/cds/cli"
+)
+
+var (
+	healthCmd = cli.Command{
+		Name:  "health",
+		Short: "Check CDS health",
+	}
+
+	health = cli.NewCommand(healthCmd, nil,
+		[]*cobra.Command{
+			cli.NewCommand(healthStatusCmd, healthStatusRun, nil),
+			cli.NewGetCommand(healthMonDBTimesCmd, healthMonDBTimesRun, nil),
+		})
+)
+
+var healthStatusCmd = cli.Command{
+	Name:  "status",
+	Short: "Show CDS Status",
 }
 
-func healthRun(v cli.Values) (interface{}, error) {
+func healthStatusRun(v cli.Values) error {
 	s, err := client.MonStatus()
-	return s, err
+	if err != nil {
+		return err
+	}
+	fmt.Println(s)
+	return nil
+}
+
+var healthMonDBTimesCmd = cli.Command{
+	Name:  "db",
+	Short: "Show some DB Times",
+}
+
+func healthMonDBTimesRun(v cli.Values) (interface{}, error) {
+	s, err := client.MonDBTimes()
+	return *s, err
 }
