8b77fbfdce2bcb031fa781b44d9be249668c9b2d
Coenen Benjamin <benjamin.coenen@hotmail.com>
2017-11-28 09:44:07 +0100

    fix(ui): all scheduler list params are now displayed (#1587)
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- ui/src/app/views/application/show/scheduler/form/scheduler.form.component.ts
+++ ui/src/app/views/application/show/scheduler/form/scheduler.form.component.ts
@@ -3,7 +3,7 @@ import {Scheduler} from '../../../../../model/scheduler.model';
 import {Project} from '../../../../../model/project.model';
 import {Application} from '../../../../../model/application.model';
 import {Pipeline} from '../../../../../model/pipeline.model';
-import {unionBy} from 'lodash';
+import {unionBy, differenceBy} from 'lodash';
 
 @Component({
     selector: 'app-application-scheduler-form',
@@ -26,6 +26,17 @@ export class ApplicationSchedulerFormComponent implements OnInit {
         if (this.scheduler && this.pipeline) {
             let filteredPipelineParams = this.pipeline.parameters ? this.pipeline.parameters.filter((p) => p.type !== 'list') : [];
             this.scheduler.args = unionBy(this.scheduler.args || [], filteredPipelineParams, 'id');
+
+            let missingParams = differenceBy(this.pipeline.parameters, this.scheduler.args, 'id');
+            missingParams = missingParams.map((param) => {
+                let paramsSplitted = param.value.split(';');
+                if (!paramsSplitted.length) {
+                    return param;
+                }
+
+                return Object.assign({}, param, {value: paramsSplitted[0]});
+            });
+            this.scheduler.args = this.scheduler.args.concat(missingParams);
         }
     }
 }
