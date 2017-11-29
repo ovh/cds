2cd6a5ce8210087b0fb38007c7718ef28a5d5257
François Samin <francois.samin@corp.ovh.com>
2017-11-29 15:46:24 +0100

    fix (api): remove migration code

--- engine/api/bootstrap/bootstrap.go
+++ engine/api/bootstrap/bootstrap.go
@@ -1,22 +1,15 @@
 package bootstrap
 
 import (
-	"database/sql"
 	"strings"
 
 	"github.com/go-gorp/gorp"
 
 	"github.com/ovh/cds/engine/api/action"
-	"github.com/ovh/cds/engine/api/cache"
-	"github.com/ovh/cds/engine/api/database/gorpmapping"
 	"github.com/ovh/cds/engine/api/environment"
 	"github.com/ovh/cds/engine/api/group"
-	"github.com/ovh/cds/engine/api/keys"
-	"github.com/ovh/cds/engine/api/project"
-	"github.com/ovh/cds/engine/api/repositoriesmanager"
 	"github.com/ovh/cds/engine/api/token"
 	"github.com/ovh/cds/sdk"
-	"github.com/ovh/cds/sdk/log"
 )
 
 //InitiliazeDB inits the database
@@ -51,154 +44,6 @@ func InitiliazeDB(defaultValues sdk.DefaultValues, DBFunc func() *gorp.DbMap) er
 
 	if err := environment.CreateBuiltinEnvironments(dbGorp); err != nil {
 		return sdk.WrapError(err, "InitiliazeDB> Cannot setup builtin environments")
-	}
-
-	return nil
-}
-
-// VCSMigrate migrate stuffs to other stuffs
-func VCSMigrate(db *gorp.DbMap, cache cache.Store) error {
-	log.Info("VCSMigrate> Begin")
-	defer log.Info("VCSMigrate> End")
-
-	query := `
-	select project.projectkey, repositories_manager_project.data, repositories_manager.name, repositories_manager.id
-	from project, repositories_manager_project, repositories_manager 
-	where project.id = repositories_manager_project.id_project
-	and repositories_manager_project.id_repositories_manager = repositories_manager.id
-	and project.vcs_servers is NULL
-	`
-
-	rows := []struct {
-		Key  string         `db:"projectkey"`
-		Name string         `db:"name"`
-		Data sql.NullString `db:"data"`
-		ID   int64          `db:"id"`
-	}{}
-
-	if _, err := db.Select(&rows, query); err != nil {
-		return err
-	}
-
-	for _, r := range rows {
-		log.Info("VCSMigrate> Migrating %s %s", r.Key, r.Name)
-		tx, err := db.Begin()
-		if err != nil {
-			log.Error("VCSMigrate> unable to start transaction %v", err)
-			continue
-		}
-
-		proj, err := project.Load(tx, cache, r.Key, nil, project.LoadOptions.WithApplications, project.LoadOptions.WithLockNoWait)
-		if err != nil {
-			log.Error("VCSMigrate> unable to load project %s: %v", r.Key, err)
-			_ = tx.Rollback()
-			continue
-		}
-
-		data := map[string]string{}
-		if err := gorpmapping.JSONNullString(r.Data, &data); err != nil {
-			log.Error("VCSMigrate> unable to unmarshall data %s: %v", r.Key, err)
-			_ = tx.Rollback()
-			continue
-		}
-
-		accessToken := data["access_token"]
-		accessTokenSecret := data["access_token_secret"]
-
-		//supportedName: see vcs configuration
-		vcsServerForProject := &sdk.ProjectVCSServer{
-			Name: strings.Replace(r.Name, ".", "_", -1),
-			Data: map[string]string{
-				"token":  accessToken,
-				"secret": accessTokenSecret,
-			},
-		}
-
-		//Insert data for the project
-		if err := repositoriesmanager.InsertForProject(tx, proj, vcsServerForProject); err != nil {
-			log.Error("VCSMigrate> unable to migrate project %s: %v", r.Key, err)
-			_ = tx.Rollback()
-			continue
-		}
-
-		//Compute application
-		for _, a := range proj.Applications {
-			if a.RepositoryFullname != "" {
-				appQuery := `update application set vcs_server = $3 where id = $1 and repositories_manager_id = $2`
-				if _, err := tx.Exec(appQuery, a.ID, r.ID, vcsServerForProject.Name); err != nil {
-					log.Error("VCSMigrate> unable to migrate application %s/%s: %v", r.Key, a.Name, err)
-					_ = tx.Rollback()
-					continue
-				}
-			}
-		}
-
-		if err := tx.Commit(); err != nil {
-			log.Error("VCSMigrate> unable to commit transaction %v", err)
-			_ = tx.Rollback()
-			continue
-		}
-	}
-
-	return nil
-}
-
-// ProjectBuiltinGPGKey create a builtin gpg key for all projects
-func ProjectBuiltinGPGKey(db *gorp.DbMap, cache cache.Store) error {
-	log.Info("ProjectBuiltinGPGKey> Begin")
-	defer log.Info("ProjectBuiltinGPGKey> End")
-
-	query := `select projectkey, id
-	from project
-	where id not in (
-		select project_id from project_key where builtin = true
-	)`
-
-	rows := []struct {
-		Key string `db:"projectkey"`
-		ID  int64  `db:"id"`
-	}{}
-
-	if _, err := db.Select(&rows, query); err != nil {
-		return err
-	}
-
-	for _, r := range rows {
-		log.Info("ProjectBuiltinGPGKey> Migrating %s", r.Key)
-		tx, err := db.Begin()
-		if err != nil {
-			log.Error("ProjectBuiltinGPGKey> unable to start transaction %v", err)
-			continue
-		}
-
-		keyID, publicKey, privateKey, err := keys.GeneratePGPKeyPair("builtin")
-		if err != nil {
-			tx.Rollback()
-			log.Error("ProjectBuiltinGPGKey> Unable to generate PGPKeyPair: %v", err)
-			continue
-		}
-
-		pk := sdk.ProjectKey{}
-		pk.Key.KeyID = keyID
-		pk.Key.Name = "builtin"
-		pk.Key.Private = privateKey
-		pk.Key.Public = publicKey
-		pk.Type = sdk.KeyTypePgp
-		pk.ProjectID = r.ID
-		pk.Builtin = true
-
-		if err := project.InsertKey(tx, &pk); err != nil {
-			tx.Rollback()
-			log.Error("ProjectBuiltinGPGKey> Unable to insert PGPKeyPair: %v", err)
-			continue
-		}
-
-		if err := tx.Commit(); err != nil {
-			tx.Rollback()
-			log.Error("ProjectBuiltinGPGKey> Unable to commit tx: %v", err)
-			continue
-		}
-
 	}
 
 	return nil
