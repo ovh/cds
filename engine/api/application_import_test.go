f105481c0701f542aad03cc603c5e33ccaa679b6
François Samin <francois.samin@corp.ovh.com>
2017-11-29 10:02:20 +0100

    test (api): application import unit test

--- engine/api/application_import_test.go
+++ engine/api/application_import_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ovh/cds/engine/api/application"
 	"github.com/ovh/cds/engine/api/test"
 	"github.com/ovh/cds/engine/api/test/assets"
 	"github.com/ovh/cds/sdk"
@@ -50,6 +51,44 @@ variables:
 
 	//Check result
 	t.Logf(">>%s", rec.Body.String())
+
+	app, err := application.LoadByName(db, api.Cache, proj.Key, "myNewApp", nil, application.LoadOptions.WithVariables, application.LoadOptions.WithGroups)
+	test.NoError(t, err)
+
+	assert.NotNil(t, app)
+	assert.Equal(t, "myNewApp", app.Name)
+
+	//Check default permission which should be set to the project ones
+	for _, perm := range proj.ProjectGroups {
+		var found bool
+		for _, aperm := range app.ApplicationGroups {
+			if aperm.Group.Name == perm.Group.Name && aperm.Permission == perm.Permission {
+				found = true
+			}
+		}
+		assert.True(t, found, "Group %s - %d not found", perm.Group.Name, perm.Permission)
+	}
+
+	//Check variables
+	for _, v := range app.Variable {
+		switch v.Name {
+		case "var1":
+			assert.True(t, v.Type == sdk.StringVariable, "var1.type should be type string")
+			assert.True(t, v.Value == "value 1", "var1.value is wrong")
+		case "var2":
+			assert.True(t, v.Type == sdk.TextVariable, "var2.type should be type text")
+			assert.True(t, v.Value == "value 2", "var2.value is wrong")
+		case "var3":
+			assert.True(t, v.Type == sdk.BooleanVariable, "var3.type should be type bool")
+			assert.True(t, v.Value == "true", "var3.value is wrong")
+		case "var4":
+			assert.True(t, v.Type == sdk.NumberParameter, "var4.type should be type number")
+			assert.True(t, v.Value == "42", "var4.value is wrong")
+		default:
+			t.Errorf("Unexpected variable %+v", v)
+		}
+	}
+
 }
 
 func Test_postApplicationImportHandler_NewAppFromYAMLWithSecrets(t *testing.T) {
