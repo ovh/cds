f4fbf472fc5951f6f86e879d0c1fce9ea6309f3f
François Samin <francois.samin+github@gmail.com>
2017-12-06 13:48:38 +0100

    fix (api): API Debug UI (#1658)

--- engine/api/api_routes.go
+++ engine/api/api_routes.go
@@ -34,7 +34,7 @@ func (api *API) InitRouter() {
 	// Admin
 	r.Handle("/admin/warning", r.DELETE(api.adminTruncateWarningsHandler, NeedAdmin(true)))
 	r.Handle("/admin/maintenance", r.POST(api.postAdminMaintenanceHandler, NeedAdmin(true)), r.GET(api.getAdminMaintenanceHandler, NeedAdmin(true)), r.DELETE(api.deleteAdminMaintenanceHandler, NeedAdmin(true)))
-	r.Handle("/admin/debug", r.GET(api.getProfileIndexHandler, NeedAdmin(true)))
+	r.Handle("/admin/debug", r.GET(api.getProfileIndexHandler, Auth(false)))
 	r.Handle("/admin/debug/trace", r.POST(api.getTraceHandler, NeedAdmin(true)))
 	r.Handle("/admin/debug/cpu", r.POST(api.getCPUProfileHandler, NeedAdmin(true)))
 	r.Handle("/admin/debug/{name}", r.POST(api.getProfileHandler, NeedAdmin(true)))
