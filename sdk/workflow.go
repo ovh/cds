01044167b14a68b45c349e8dbcdeb6ba0ca7bebb
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-12-18 11:36:44 +0100

    cr
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- sdk/workflow.go
+++ sdk/workflow.go
@@ -620,7 +620,7 @@ type WorkflowNodeConditions struct {
 	LuaScript       string                  `json:"lua_script,omitempty" yaml:"script,omitempty"`
 }
 
-//WorkflowTriggerCondition represents a condition to trigger ot not a pipeline in a workflow. Operator can be =, !=, regex
+//WorkflowNodeCondition represents a condition to trigger ot not a pipeline in a workflow. Operator can be =, !=, regex
 type WorkflowNodeCondition struct {
 	Variable string `json:"variable"`
 	Operator string `json:"operator"`
@@ -640,9 +640,14 @@ type WorkflowNodeContext struct {
 	Conditions                WorkflowNodeConditions `json:"conditions,omitempty" db:"-"`
 }
 
+//WorkflowNodeContextDefaultPayloadVCS represents a default payload when a workflow is attached to a repository Webhook
 type WorkflowNodeContextDefaultPayloadVCS struct {
-	GitBranch string `json:"git.branch" db:"-"`
-	GitHash   string `json:"git.hash" db:"-"`
+	GitBranch     string `json:"git.branch" db:"-"`
+	GitHash       string `json:"git.hash" db:"-"`
+	GitAuthor     string `json:"git.author" db:"-"`
+	GitHashBefore string `json:"git.hash.before" db:"-"`
+	GitRepository string `json:"git.repository" db:"-"`
+	GitMessage    string `json:"git.message" db:"-"`
 }
 
 //WorkflowList return the list of the workflows for a project
