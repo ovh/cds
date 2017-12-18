a7fd72cc46ec2024e3bad2b20421ccc27183aea7
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2017-12-18 10:45:22 +0100

    feat(ui): add dblclick on node run on each stage, job and step
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- ui/src/app/views/workflow/sidebar/run/node/stage/job/job.summary.component.ts
+++ ui/src/app/views/workflow/sidebar/run/node/stage/job/job.summary.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 import {PipelineStatus} from '../../../../../../../model/pipeline.model';
-import {WorkflowNodeJobRun} from '../../../../../../../model/workflow.run.model';
+import {WorkflowRun, WorkflowNodeJobRun} from '../../../../../../../model/workflow.run.model';
+import {Stage} from '../../../../../../../model/stage.model';
 
 @Component({
     selector: 'app-job-step-summary',
@@ -10,13 +12,28 @@ import {WorkflowNodeJobRun} from '../../../../../../../model/workflow.run.model'
 export class JobStepSummaryComponent implements OnInit {
 
     @Input() job: WorkflowNodeJobRun;
+    @Input() workflowRun: WorkflowRun;
+    @Input() stage: Stage;
 
     open = false;
-    constructor() {
+    constructor(private _router: Router, private _route: ActivatedRoute) {
 
     }
 
     ngOnInit() {
       this.open = this.job.status === PipelineStatus.FAIL || PipelineStatus.isActive(this.job.status);
+    }
+
+    goToJobLogs() {
+      this._router.navigate([
+          'project',
+          this._route.snapshot.params['key'],
+          'workflow',
+          this._route.snapshot.params['workflowName'],
+          'run',
+          this.workflowRun.num,
+          'node',
+          this.job.workflow_node_run_id
+      ], {queryParams: {stageId: this.stage.id, actionId: this.job.job.pipeline_action_id}});
     }
 }
