5f9aa8f00e9768ffa0cb8284335aedabaaddbbbf
François Samin <francois.samin@corp.ovh.com>
2017-12-29 15:40:29 +0100

    fix (sdk/lua): perform script

---
+++ sdk/luascript/luascript_test.go
@@ -0,0 +1,19 @@
+package luascript
+
+import (
+	"testing"
+
+	"github.com/ovh/cds/engine/api/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLuaCheck(t *testing.T) {
+	l, err := NewCheck()
+	test.NoError(t, err)
+	l.SetVariables(map[string]string{
+		"cds.application": "mon-appli",
+	})
+	test.NoError(t, l.Perform("return cds_application == \"mon-appli\""))
+	assert.False(t, l.IsError)
+	assert.True(t, l.Result)
+}
