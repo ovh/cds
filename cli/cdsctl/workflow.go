6c8acd08c41d14da6fbdd9f74b3ff3e9a95f69dc
François Samin <francois.samin+github@gmail.com>
2018-01-09 10:59:55 +0100

    feat (cdsctl): workflow run (#1911)

--- cli/cdsctl/workflow.go
+++ cli/cdsctl/workflow.go
@@ -12,10 +12,9 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"time"
 
-	tm "github.com/buger/goterm"
 	"github.com/fsamin/go-dump"
+	"github.com/pkg/browser"
 	"github.com/spf13/cobra"
 
 	"github.com/ovh/cds/cli"
@@ -232,6 +231,18 @@ var workflowRunManualCmd = cli.Command{
 			Usage: "Node Name to relaunch; Flag run-number is mandatory",
 			Kind:  reflect.String,
 		},
+		{
+			Name:      "interactive",
+			ShortHand: "i",
+			Usage:     "Follow the workflow run in an interactive terminal user interface",
+			Kind:      reflect.Bool,
+		},
+		{
+			Name:      "open-web-browser",
+			ShortHand: "o",
+			Usage:     "Open web browser on the workflow run",
+			Kind:      reflect.Bool,
+		},
 	},
 }
 
@@ -275,117 +286,35 @@ func workflowRunManualRun(v cli.Values) error {
 		return err
 	}
 
-	var wo *sdk.WorkflowRun
-	var failedOn, output string
+	fmt.Printf("Workflow %s #%d has been lauched\n", v["workflow-name"], w.Number)
 
-	for {
-		var errrg error
-		wo, errrg = client.WorkflowRunGet(v["project-key"], v["workflow-name"], w.Number)
-		if errrg != nil {
-			return errrg
-		}
-
-		var newOutput string
-
-		failedOn = ""
-		for _, wnrs := range wo.WorkflowNodeRuns {
-			for _, wnr := range wnrs {
-				wn := w.Workflow.GetNode(wnr.WorkflowNodeID)
-				for _, stage := range wnr.Stages {
-					for _, job := range stage.RunJobs {
-						status, _ := statusShort(job.Status)
-						var start, end string
-						if job.Status != sdk.StatusWaiting.String() {
-							start = fmt.Sprintf("start:%s", job.Start)
-						}
-						if job.Done.After(job.Start) {
-							end = fmt.Sprintf(" end:%s", job.Done)
-						}
-
-						jobLine := fmt.Sprintf("%s  %s/%s/%s/%s %s %s \n", status, v["workflow-name"], wn.Name, stage.Name, job.Job.Action.Name, start, end)
-						if job.Status == sdk.StatusFail.String() {
-							newOutput += fmt.Sprintf(tm.Color(tm.Bold(jobLine), tm.RED))
-						} else {
-							newOutput += fmt.Sprintf(tm.Bold(jobLine))
-						}
-
-						for _, info := range job.SpawnInfos {
-							newOutput += fmt.Sprintf("\nInformations: %s - %s", info.APITime, info.UserMessage)
-						}
-						newOutput += fmt.Sprintf("\n")
-
-						for _, step := range job.Job.StepStatus {
-							buildState, errb := client.WorkflowNodeRunJobStep(v["project-key"], v["workflow-name"], wo.Number, wnr.ID, job.ID, step.StepOrder)
-							if errb != nil {
-								return errb
-							}
-
-							vSplitted := strings.Split(buildState.StepLogs.Val, "\n")
-							failedOnStepKnowned := false
-							for _, line := range vSplitted {
-								line = strings.Trim(line, " ")
-								titleStep := fmt.Sprintf("%s / step %d", job.Job.Action.Name, step.StepOrder)
-								// RED color on step failed
-								if step.Status == sdk.StatusFail.String() {
-									if !failedOnStepKnowned {
-										// hide "Starting" text on resume
-										failedOn = fmt.Sprintf("%s%s / %s / %s / %s %s \n", failedOn, v["workflow-name"], wn.Name, stage.Name, titleStep, strings.Replace(line, "Starting", "", 1))
-									}
-									failedOnStepKnowned = true
-									titleStep = fmt.Sprintf(tm.Color(titleStep, tm.RED))
-								}
-
-								if line != "" {
-									newOutput += fmt.Sprintf("%s\t\t %s\n", titleStep, line)
-								}
-							}
-						}
-						if job.Done.After(job.Start) {
-							newOutput += fmt.Sprintf("\n")
-						}
-
-						if newOutput != output {
-							tm.Clear() // Clear current screen
-							tm.MoveCursor(1, 1)
-							output = newOutput
-							tm.Printf(output)
-							tm.Flush()
-						}
-					}
-				}
-			}
-		}
-
-		if wo.Status == sdk.StatusFail.String() || wo.Status == sdk.StatusSuccess.String() {
-			break
-		}
-		time.Sleep(2 * time.Second)
+	var baseURL string
+	configUser, err := client.ConfigUser()
+	if err != nil {
+		return err
 	}
 
-	if wo != nil {
-		iconStatus, _ := statusShort(wo.Status)
-		tm.Printf("Workflow: %s - RUN %d.%d - %s %s \n", v["workflow-name"], wo.Number, wo.LastSubNumber, wo.Status, iconStatus)
-		tm.Printf("Start: %s - End %s\n", wo.Start, wo.LastModified)
-		tm.Printf("Duration: %s\n", sdk.Round(wo.LastModified.Sub(wo.Start), time.Second).String())
-		if wo.Status == sdk.StatusFail.String() {
-			tm.Println(tm.Color(fmt.Sprintf("Failed on: %s", failedOn), tm.RED))
-		}
-
-		var baseURL string
-		configUser, err := client.ConfigUser()
-		if err != nil {
-			return err
-		}
-
-		if b, ok := configUser[sdk.ConfigURLUIKey]; ok {
-			baseURL = b
-		}
-
-		u := fmt.Sprintf("%s/project/%s/workflow/%s/run/%d", baseURL, v["project-key"], v["workflow-name"], wo.Number)
-		tm.Printf("View on web UI: %s\n", u)
+	if b, ok := configUser[sdk.ConfigURLUIKey]; ok {
+		baseURL = b
 	}
-	tm.Flush()
-	return nil
+
+	if baseURL == "" {
+		fmt.Println("Unable to retrieve workflow URI")
+		return nil
+	}
+
+	if !v.GetBool("interactive") {
+		url := fmt.Sprintf("%s/project/%s/workflow/%s/run/%d", baseURL, v["project-key"], v["workflow-name"], w.Number)
+		fmt.Println(url)
+
+		if v.GetBool("open-web-browser") {
+			return browser.OpenURL(url)
+		}
+
+		return nil
+	}
+
+	return workflowRunInteractive(v, w, baseURL)
 }
 
 var workflowExportCmd = cli.Command{
