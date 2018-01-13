9050deb72f0f8d3056f479137a7174125505b22b
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2018-01-13 17:39:04 +0100

    feat (api,ui): improve download page.
    Display if binary is available or not on UI
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- sdk/download.go
+++ sdk/download.go
@@ -3,6 +3,8 @@ package sdk
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"path"
 )
 
 // URLGithubIssues contains a link to CDS Issues
@@ -19,14 +21,20 @@ type Download struct {
 
 // OSArch contains a association OS / Arch
 type OSArch struct {
-	OS    string   `json:"os"`
-	Archs []string `json:"archs"`
+	OS    string `json:"os"`
+	Archs []Arch `json:"archs"`
+}
+
+// Arch contains a association Arch / available
+type Arch struct {
+	Arch      string `json:"arch"`
+	Available bool   `json:"available"`
 }
 
 // IsBinaryOSArchValid returns err if name (worker, engine, cdsctl..) is not
 // valid with os and arch. Returns "fixed Arch" 386 / amd64 or arm
 // example: if arch == i386 or i686, return 386
-func IsBinaryOSArchValid(name, os, arch string) (string, error) {
+func IsBinaryOSArchValid(directoriesDownload, name, osBinary, arch string) (string, error) {
 	v := GetStaticDownloads()
 	var fixedArch = getArchName(arch)
 
@@ -45,10 +53,10 @@ func IsBinaryOSArchValid(name, os, arch string) (string, error) {
 
 	var osFound bool
 	for _, o := range download.OSArchs {
-		if os == o.OS {
+		if osBinary == o.OS {
 			osFound = true
 			for _, a := range o.Archs {
-				if a == fixedArch {
+				if a.Arch == fixedArch {
 					// name, os, arch found, it's valid
 					return fixedArch, nil
 				}
@@ -57,6 +65,10 @@ func IsBinaryOSArchValid(name, os, arch string) (string, error) {
 	}
 	if !osFound {
 		return fixedArch, ErrDownloadInvalidOS
+	}
+
+	if _, err := os.Stat(path.Join(directoriesDownload, fmt.Sprintf("cds-%s-%s-%s", name, osBinary, fixedArch))); err != nil {
+		return fixedArch, ErrDownloadDoesNotExist
 	}
 
 	return fixedArch, ErrDownloadInvalidArch
@@ -80,34 +92,52 @@ func GetArtifactFilename(name, os, arch string) string {
 	return fmt.Sprintf("cds-%s-%s-%s", name, os, getArchName(arch))
 }
 
+func getDefaultArch() []OSArch {
+	return []OSArch{
+		{OS: "windows", Archs: []Arch{{Arch: "386"}, {Arch: "amd64"}}},
+		{OS: "linux", Archs: []Arch{{Arch: "386"}, {Arch: "amd64"}, {Arch: "arm"}}},
+		{OS: "darwin", Archs: []Arch{{Arch: "amd64"}}},
+		{OS: "freebsd", Archs: []Arch{{Arch: "386"}, {Arch: "amd64"}}},
+	}
+}
+
 // GetStaticDownloads returns default builded CDS Binaries
 func GetStaticDownloads() []Download {
-	defaultArch := []OSArch{
-		{OS: "windows", Archs: []string{"386", "amd64"}},
-		{OS: "linux", Archs: []string{"386", "amd64", "arm"}},
-		{OS: "darwin", Archs: []string{"amd64"}},
-		{OS: "freebsd", Archs: []string{"386", "amd64"}},
-	}
-
 	downloads := []Download{
 		{
 			Name:    "worker",
-			OSArchs: defaultArch,
+			OSArchs: getDefaultArch(),
 		},
 		{
 			Name:    "engine",
-			OSArchs: defaultArch,
+			OSArchs: getDefaultArch(),
 		},
 		{
 			Name:    "cdsctl",
-			OSArchs: []OSArch{{OS: "linux", Archs: []string{"amd64"}}},
+			OSArchs: []OSArch{{OS: "linux", Archs: []Arch{{Arch: "amd64"}}}},
 		},
 		{
 			Name:    "cds",
-			OSArchs: defaultArch,
+			OSArchs: getDefaultArch(),
 		},
 	}
 
+	return downloads
+}
+
+// GetStaticDownloadsWithAvailability set flag Available on downloads list
+func GetStaticDownloadsWithAvailability(directoriesDownload string) []Download {
+	downloads := GetStaticDownloads()
+	// for each download, check if binary exists in directoriesDownload
+	for k, d := range downloads {
+		for ks, o := range downloads[k].OSArchs {
+			for ka, a := range downloads[k].OSArchs[ks].Archs {
+				if _, err := os.Stat(path.Join(directoriesDownload, fmt.Sprintf("cds-%s-%s-%s", d.Name, o.OS, a.Arch))); err == nil {
+					downloads[k].OSArchs[ks].Archs[ka].Available = true
+				}
+			}
+		}
+	}
 	return downloads
 }
 
