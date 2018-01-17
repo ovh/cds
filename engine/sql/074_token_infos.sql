36ea9d3d5d50d288ecc0b58f52063f252a174539
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2018-01-17 15:43:17 +0100

    feat(api, cli, ui): add token handling
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- engine/sql/074_token_infos.sql
+++ engine/sql/074_token_infos.sql
@@ -2,7 +2,9 @@
 ALTER TABLE token ADD COLUMN description TEXT;
 ALTER TABLE token ADD COLUMN creator TEXT;
 ALTER TABLE token ADD COLUMN id BIGSERIAL PRIMARY KEY;
+SELECT create_foreign_key_idx_cascade('FK_TOKEN_GROUP', 'token', 'group', 'group_id', 'id');
 -- +migrate Down
+ALTER TABLE token DROP CONSTRAINT FK_TOKEN_GROUP;
 ALTER TABLE token DROP COLUMN description;
 ALTER TABLE token DROP COLUMN creator;
 ALTER TABLE token DROP COLUMN id;
