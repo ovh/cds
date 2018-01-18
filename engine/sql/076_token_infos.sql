b53668a78fddcfe4c09ae1efe68f001be5ff8944
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2018-01-18 15:27:39 +0100

    fix(sql): fix create index
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- engine/sql/076_token_infos.sql
+++ engine/sql/076_token_infos.sql
@@ -2,7 +2,7 @@
 ALTER TABLE token ADD COLUMN description TEXT;
 ALTER TABLE token ADD COLUMN creator TEXT;
 ALTER TABLE token ADD COLUMN id BIGSERIAL PRIMARY KEY;
-SELECT create_foreign_key_idx_cascade('FK_TOKEN_GROUP', 'token', 'group', 'group_id', 'id');
+-- SELECT create_foreign_key_idx_cascade('FK_TOKEN_GROUP', 'token', 'group', 'group_id', 'id');
 -- +migrate Down
 ALTER TABLE token DROP CONSTRAINT FK_TOKEN_GROUP;
 ALTER TABLE token DROP COLUMN description;
