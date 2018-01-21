a505dadb434f4393c2652ce4f89e5b6471c8f952
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2018-01-21 12:01:52 +0100

    fix: gorp dep
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- vendor/github.com/go-gorp/gorp/dialect_postgres_test.go
+++ vendor/github.com/go-gorp/gorp/dialect_postgres_test.go
@@ -24,16 +24,15 @@ import (
 	"github.com/go-gorp/gorp"
 )
 
-var _ = Describe("MySQLDialect", func() {
+var _ = Describe("PostgresDialect", func() {
 	var (
-		engine, encoding string
-		dialect          gorp.MySQLDialect
+		lowercasefields bool
+		dialect         gorp.PostgresDialect
 	)
 
 	JustBeforeEach(func() {
-		dialect = gorp.MySQLDialect{
-			Engine:   engine,
-			Encoding: encoding,
+		dialect = gorp.PostgresDialect{
+			LowercaseFields: lowercasefields,
 		}
 	})
 
@@ -44,79 +43,52 @@ var _ = Describe("MySQLDialect", func() {
 			Expect(sqlType).To(Equal(expected))
 		},
 		Entry("bool", true, 0, false, "boolean"),
-		Entry("int8", int8(1), 0, false, "tinyint"),
-		Entry("uint8", uint8(1), 0, false, "tinyint unsigned"),
-		Entry("int16", int16(1), 0, false, "smallint"),
-		Entry("uint16", uint16(1), 0, false, "smallint unsigned"),
-		Entry("int32", int32(1), 0, false, "int"),
-		Entry("int (treated as int32)", int(1), 0, false, "int"),
-		Entry("uint32", uint32(1), 0, false, "int unsigned"),
-		Entry("uint (treated as uint32)", uint(1), 0, false, "int unsigned"),
+		Entry("int8", int8(1), 0, false, "integer"),
+		Entry("uint8", uint8(1), 0, false, "integer"),
+		Entry("int16", int16(1), 0, false, "integer"),
+		Entry("uint16", uint16(1), 0, false, "integer"),
+		Entry("int32", int32(1), 0, false, "integer"),
+		Entry("int (treated as int32)", int(1), 0, false, "integer"),
+		Entry("uint32", uint32(1), 0, false, "integer"),
+		Entry("uint (treated as uint32)", uint(1), 0, false, "integer"),
 		Entry("int64", int64(1), 0, false, "bigint"),
-		Entry("uint64", uint64(1), 0, false, "bigint unsigned"),
-		Entry("float32", float32(1), 0, false, "double"),
-		Entry("float64", float64(1), 0, false, "double"),
-		Entry("[]uint8", []uint8{1}, 0, false, "mediumblob"),
+		Entry("uint64", uint64(1), 0, false, "bigint"),
+		Entry("float32", float32(1), 0, false, "real"),
+		Entry("float64", float64(1), 0, false, "double precision"),
+		Entry("[]uint8", []uint8{1}, 0, false, "bytea"),
 		Entry("NullInt64", sql.NullInt64{}, 0, false, "bigint"),
-		Entry("NullFloat64", sql.NullFloat64{}, 0, false, "double"),
-		Entry("NullBool", sql.NullBool{}, 0, false, "tinyint"),
-		Entry("Time", time.Time{}, 0, false, "datetime"),
-		Entry("default-size string", "", 0, false, "varchar(255)"),
+		Entry("NullFloat64", sql.NullFloat64{}, 0, false, "double precision"),
+		Entry("NullBool", sql.NullBool{}, 0, false, "boolean"),
+		Entry("Time", time.Time{}, 0, false, "timestamp with time zone"),
+		Entry("default-size string", "", 0, false, "text"),
 		Entry("sized string", "", 50, false, "varchar(50)"),
-		Entry("large string", "", 1024, false, "text"),
+		Entry("large string", "", 1024, false, "varchar(1024)"),
 	)
 
 	Describe("AutoIncrStr", func() {
 		It("returns the auto increment string", func() {
-			Expect(dialect.AutoIncrStr()).To(Equal("auto_increment"))
+			Expect(dialect.AutoIncrStr()).To(Equal(""))
 		})
 	})
 
 	Describe("AutoIncrBindValue", func() {
 		It("returns the value used to bind the auto-increment value", func() {
-			Expect(dialect.AutoIncrBindValue()).To(Equal("null"))
+			Expect(dialect.AutoIncrBindValue()).To(Equal("default"))
 		})
 	})
 
 	Describe("AutoIncrInsertSuffix", func() {
 		It("returns the suffix needed for auto-incrementing", func() {
-			Expect(dialect.AutoIncrInsertSuffix(nil)).To(BeEmpty())
+			cm := gorp.ColumnMap{
+				ColumnName: "foo",
+			}
+			Expect(dialect.AutoIncrInsertSuffix(&cm)).To(Equal(` returning "foo"`))
 		})
 	})
 
 	Describe("CreateTableSuffix", func() {
-		Context("with an empty engine", func() {
-			BeforeEach(func() {
-				engine = ""
-				encoding = "foo"
-			})
-			It("panics", func() {
-				Expect(func() {
-					dialect.CreateTableSuffix()
-				}).To(Panic())
-			})
-		})
-
-		Context("with an empty encoding", func() {
-			BeforeEach(func() {
-				engine = "foo"
-				encoding = ""
-			})
-			It("panics", func() {
-				Expect(func() {
-					dialect.CreateTableSuffix()
-				}).To(Panic())
-			})
-		})
-
-		Context("with an engine and an encoding", func() {
-			BeforeEach(func() {
-				engine = "foo"
-				encoding = "bar"
-			})
-			It("returns a valid suffix", func() {
-				Expect(dialect.CreateTableSuffix()).To(Equal(" engine=foo charset=bar"))
-			})
+		It("returns an empty suffix", func() {
+			Expect(dialect.CreateTableSuffix()).To(Equal(""))
 		})
 	})
 
@@ -128,7 +100,7 @@ var _ = Describe("MySQLDialect", func() {
 
 	Describe("DropIndexSuffix", func() {
 		It("returns the suffix for deleting indexes", func() {
-			Expect(dialect.DropIndexSuffix()).To(Equal("on"))
+			Expect(dialect.DropIndexSuffix()).To(Equal(""))
 		})
 	})
 
@@ -140,22 +112,32 @@ var _ = Describe("MySQLDialect", func() {
 
 	Describe("SleepClause", func() {
 		It("returns the clause for sleeping", func() {
-			Expect(dialect.SleepClause(1 * time.Second)).To(Equal("sleep(1.000000)"))
-			Expect(dialect.SleepClause(100 * time.Millisecond)).To(Equal("sleep(0.100000)"))
+			Expect(dialect.SleepClause(1 * time.Second)).To(Equal("pg_sleep(1.000000)"))
+			Expect(dialect.SleepClause(100 * time.Millisecond)).To(Equal("pg_sleep(0.100000)"))
 		})
 	})
 
 	Describe("BindVar", func() {
 		It("returns the variable binding sequence", func() {
-			Expect(dialect.BindVar(0)).To(Equal("?"))
+			Expect(dialect.BindVar(0)).To(Equal("$1"))
+			Expect(dialect.BindVar(4)).To(Equal("$5"))
 		})
 	})
 
 	PDescribe("InsertAutoIncr", func() {})
 
 	Describe("QuoteField", func() {
-		It("returns the argument quoted as a field", func() {
-			Expect(dialect.QuoteField("foo")).To(Equal("`foo`"))
+		It("returns the argument quoted as a field (mixed case by default)", func() {
+			Expect(dialect.QuoteField("Foo")).To(Equal(`"Foo"`))
+			Expect(dialect.QuoteField("bar")).To(Equal(`"bar"`))
+		})
+		Context("with lowercase fields", func() {
+			BeforeEach(func() {
+				lowercasefields = true
+			})
+			It("returns the argument quoted as a field", func() {
+				Expect(dialect.QuoteField("Foo")).To(Equal(`"foo"`))
+			})
 		})
 	})
 
@@ -176,7 +158,7 @@ var _ = Describe("MySQLDialect", func() {
 				table = "foo"
 			})
 			It("returns just the table", func() {
-				Expect(quotedTable).To(Equal("`foo`"))
+				Expect(quotedTable).To(Equal(`"foo"`))
 			})
 		})
 
@@ -186,7 +168,7 @@ var _ = Describe("MySQLDialect", func() {
 				table = "bar"
 			})
 			It("returns the schema and table", func() {
-				Expect(quotedTable).To(Equal("foo.`bar`"))
+				Expect(quotedTable).To(Equal(`foo."bar"`))
 			})
 		})
 	})
