420611ea0d150a34e0b14e15d3d4a20c7d7f5f4b
CDS <yesnault@users.noreply.github.com>
2018-01-23 11:59:17 +0000

    doc: generate v.1861

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,7 @@
 // Stick the top to the top of the screen when  scrolling
 $("#top-bar").stick_in_parent({spacer: false});
 
-function GetLatestReleaseInfo() {
-    $.getJSON("https://api.github.com/repos/ovh/cds/releases/latest").done(function(release) {
-      for (var i = 0; i < release.assets.length; i++) {
-        $("."+release.assets[i].name).text(release.assets[i].name);
-        $("."+release.assets[i].name).fadeIn("slow");
-        $(".download-"+release.assets[i].name).attr("href", release.assets[i].browser_download_url);
-      }
-    });
-  }
-
 jQuery(document).ready(function () {
     var sidebarActiveli = $('#sidebar').find('li.active').get(0);
     sidebarActiveli && sidebarActiveli.scrollIntoView();
-    GetLatestReleaseInfo();
 });
