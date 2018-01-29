054d5a2c1f5c90d0fccc85a9295315e54364573a
Yvonnick Esnault <yesnault@users.noreply.github.com>
2018-01-29 09:55:33 +0100

    fix (hooks): cds.triggered_by.* (#2084)

--- engine/hooks/tasks.go
+++ engine/hooks/tasks.go
@@ -292,7 +292,7 @@ func (s *Service) doScheduledTaskExecution(t *TaskExecution) (*sdk.WorkflowNodeR
 	}
 
 	//Prepare the payload
-	//Anything can be pushed in the configuration, juste avoid sending
+	//Anything can be pushed in the configuration, just avoid sending
 	payloadValues := map[string]string{}
 	for k, v := range t.Config {
 		switch k {
@@ -301,6 +301,8 @@ func (s *Service) doScheduledTaskExecution(t *TaskExecution) (*sdk.WorkflowNodeR
 			payloadValues[k] = v.Value
 		}
 	}
+	payloadValues["cds.triggered_by.username"] = "cds.scheduler"
+	payloadValues["cds.triggered_by.fullname"] = "CDS Scheduler"
 	h.Payload = payloadValues
 
 	return &h, nil
@@ -348,6 +350,10 @@ func executeRepositoryWebHook(t *TaskExecution) (*sdk.WorkflowNodeRunHookEvent, 
 		payload["git.hash.before"] = pushEvent.Before
 		payload["git.hash"] = pushEvent.After
 		payload["git.repository"] = pushEvent.Repository.FullName
+		payload["cds.triggered_by.username"] = pushEvent.HeadCommit.Author.Username
+		payload["cds.triggered_by.fullname"] = pushEvent.HeadCommit.Author.Name
+		payload["cds.triggered_by.email"] = pushEvent.HeadCommit.Author.Email
+
 		if strings.HasPrefix(pushEvent.Ref, "refs/tags/") {
 			payload["git.tag"] = strings.TrimPrefix(pushEvent.Ref, "refs/tags/")
 		}
@@ -369,6 +375,11 @@ func executeRepositoryWebHook(t *TaskExecution) (*sdk.WorkflowNodeRunHookEvent, 
 		payload["git.hash.before"] = pushEvent.Before
 		payload["git.hash"] = pushEvent.After
 		payload["git.repository"] = pushEvent.Repository.Name
+
+		payload["cds.triggered_by.username"] = pushEvent.UserUsername
+		payload["cds.triggered_by.fullname"] = pushEvent.UserName
+		payload["cds.triggered_by.email"] = pushEvent.UserEmail
+
 		if strings.HasPrefix(pushEvent.Ref, "refs/tags/") {
 			payload["git.tag"] = strings.TrimPrefix(pushEvent.Ref, "refs/tags/")
 		}
@@ -391,6 +402,11 @@ func executeRepositoryWebHook(t *TaskExecution) (*sdk.WorkflowNodeRunHookEvent, 
 		payload["git.hash.before"] = pushEvent.Changes[0].FromHash
 		payload["git.hash"] = pushEvent.Changes[0].ToHash
 		payload["git.repository"] = fmt.Sprintf("%s/%s", pushEvent.Repository.Project.Key, pushEvent.Repository.Name)
+
+		payload["cds.triggered_by.username"] = pushEvent.Actor.Name
+		payload["cds.triggered_by.fullname"] = pushEvent.Actor.DisplayName
+		payload["cds.triggered_by.email"] = pushEvent.Actor.EmailAddress
+
 		if strings.HasPrefix(pushEvent.Changes[0].RefID, "refs/tags/") {
 			payload["git.tag"] = strings.TrimPrefix(pushEvent.Changes[0].RefID, "refs/tags/")
 		}
@@ -490,6 +506,9 @@ func executeWebHook(t *TaskExecution) (*sdk.WorkflowNodeRunHookEvent, error) {
 			h.Payload[k] = v.Value
 		}
 	}
+
+	h.Payload["cds.triggered_by.username"] = "cds.webhook"
+	h.Payload["cds.triggered_by.fullname"] = "CDS Webhook"
 
 	//try to find some specific values
 	for k := range values {
