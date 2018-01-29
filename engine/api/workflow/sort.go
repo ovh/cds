1d68a497a4b3011d96e133fd02086e582d782c4a
François Samin <francois.samin@corp.ovh.com>
2018-01-29 09:51:21 +0100

    fix (api): panic on workflow load

--- engine/api/workflow/sort.go
+++ engine/api/workflow/sort.go
@@ -9,22 +9,34 @@ import (
 
 // Sort sorts all the workflow tree
 func Sort(w *sdk.Workflow) {
+	if w == nil {
+		return
+	}
 	SortNode(w.Root)
 }
 
 // SortNode sort the content of a node
 func SortNode(n *sdk.WorkflowNode) {
+	if n == nil {
+		return
+	}
 	sortNodeHooks(&n.Hooks)
 	sortNodeTriggers(&n.Triggers)
 }
 
 func sortNodeHooks(hooks *[]sdk.WorkflowNodeHook) {
+	if hooks == nil {
+		return
+	}
 	sort.Slice(*hooks, func(i, j int) bool {
 		return (*hooks)[i].UUID < (*hooks)[j].UUID
 	})
 }
 
 func sortNodeTriggers(triggers *[]sdk.WorkflowNodeTrigger) {
+	if triggers == nil {
+		return
+	}
 	for i := range *triggers {
 		sortNodeHooks(&(*triggers)[i].WorkflowDestNode.Hooks)
 	}
