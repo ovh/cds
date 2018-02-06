03ee1dc1ae210637ed41eef2ef2fc0f01a2d9119
François Samin <francois.samin+github@gmail.com>
2018-02-06 08:55:49 +0100

    feat (api): get hook details (#2142)

---
+++ sdk/hooks.go
@@ -0,0 +1,37 @@
+package sdk
+
+// Task is a generic hook tasks such as webhook, scheduler,... which will be started and wait for execution
+type Task struct {
+	UUID       string
+	Type       string
+	Config     WorkflowNodeHookConfig
+	Stopped    bool
+	Executions []TaskExecution
+}
+
+// TaskExecution represents an execution instance of a task. It the task is a webhook; this represents the call of the webhook
+type TaskExecution struct {
+	UUID                string
+	Type                string
+	Timestamp           int64
+	NbErrors            int64
+	LastError           string
+	ProcessingTimestamp int64
+	WorkflowRun         int64
+	Config              WorkflowNodeHookConfig
+	WebHook             *WebHookExecution
+	ScheduledTask       *ScheduledTaskExecution
+	Status              string
+}
+
+// WebHookExecution contains specific data for a webhook execution
+type WebHookExecution struct {
+	RequestURL    string
+	RequestBody   []byte
+	RequestHeader map[string][]string
+}
+
+// ScheduledTaskExecution contains specific data for a scheduled task execution
+type ScheduledTaskExecution struct {
+	DateScheduledExecution string
+}
