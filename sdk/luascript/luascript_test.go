bc30f35d566af82d33efbddb177a3e900176a38e
François SAMIN <francois.samin@gmail.com>
2018-02-12 19:55:06 +0100

    feat (api): support lua regex
    close #2192

--- sdk/luascript/luascript_test.go
+++ sdk/luascript/luascript_test.go
@@ -28,3 +28,27 @@ func TestLuaCheckStrings(t *testing.T) {
 	assert.False(t, l.IsError)
 	assert.True(t, l.Result)
 }
+
+func TestLuaCheckRegularExpression(t *testing.T) {
+	l, err := NewCheck()
+	test.NoError(t, err)
+	l.SetVariables(map[string]string{
+		"cds.application": "mon-appli",
+	})
+	test.NoError(t, l.Perform(`
+		local re = require("re")
+
+		return re.match("abcdefg", "abc.*") == "abcdefg"
+	`))
+	assert.False(t, l.IsError)
+	assert.True(t, l.Result)
+
+	test.NoError(t, l.Perform(`
+		local re = require("re")
+
+		return re.match("abcdefg", "zzz.*") == ""
+	`))
+	assert.False(t, l.IsError)
+	assert.False(t, l.Result)
+
+}
