afb76cbb3f108ea2c69105a0111d130c1b183b52
steven.guiheux <steven.guiheux@corp.ovh.com>
2018-03-05 15:24:37 +0100

    add missing file

---
+++ engine/api/workflow/repo.go
@@ -0,0 +1,210 @@
+package workflow
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/fsamin/go-dump"
+	"github.com/go-gorp/gorp"
+
+	"github.com/ovh/cds/engine/api/cache"
+	"github.com/ovh/cds/engine/api/keys"
+	"github.com/ovh/cds/engine/api/services"
+	"github.com/ovh/cds/sdk"
+	"github.com/ovh/cds/sdk/log"
+)
+
+// WorkflowAsCodePattern is the default code pattern to find cds files
+const WorkflowAsCodePattern = ".cds/**/*.yml"
+
+type WorkflowPushOption struct {
+	VCSServer          string
+	FromRepository     string
+	Branch             string
+	IsDefaultBranch    bool
+	RepositoryName     string
+	RepositoryStrategy sdk.RepositoryStrategy
+	DryRun             bool
+	IsExecution        bool
+}
+
+// CreateFromRepository a workflow from a repository
+func CreateFromRepository(c context.Context, db *gorp.DbMap, store cache.Store, p *sdk.Project, w *sdk.Workflow, opts sdk.WorkflowRunPostHandlerOption, u *sdk.User, decryptFunc keys.DecryptFunc) error {
+	ope, err := createOperationRequest(*w, opts)
+	if err != nil {
+		return sdk.WrapError(err, "CreateFromRepository> Unable to create operation request")
+	}
+
+	if err := PostRepositoryOperation(db, store, &ope); err != nil {
+		return sdk.WrapError(err, "CreateFromRepository> Unable to post repository operation")
+	}
+
+	if err := pollRepositoryOperation(c, db, store, &ope); err != nil {
+		return sdk.WrapError(err, "CreateFromRepository> Cannot analyse repository")
+	}
+
+	if err := extractWorkflow(db, store, p, w, ope, u, decryptFunc); err != nil {
+		return sdk.WrapError(err, "CreateFromRepository> Unable to extract workflow")
+	}
+
+	return nil
+}
+
+func extractWorkflow(db *gorp.DbMap, store cache.Store, p *sdk.Project, w *sdk.Workflow, ope sdk.Operation, u *sdk.User, decryptFunc keys.DecryptFunc) error {
+	// Read files
+	tr, err := ReadCDSFiles(ope.LoadFiles.Results)
+	if err != nil {
+		return sdk.WrapError(err, "extractWorkflow> Unable to read cds files")
+	}
+	opt := &WorkflowPushOption{
+		VCSServer:          ope.VCSServer,
+		RepositoryName:     ope.RepositoryInfo.Name,
+		RepositoryStrategy: ope.RepositoryStrategy,
+		Branch:             ope.Setup.Checkout.Branch,
+		FromRepository:     ope.RepositoryInfo.FetchURL,
+		IsDefaultBranch:    ope.Setup.Checkout.Branch == ope.RepositoryInfo.DefaultBranch,
+		DryRun:             true,
+		IsExecution:        true,
+	}
+	_, workflowPushed, errP := Push(db, store, p, tr, opt, u, decryptFunc)
+	if errP != nil {
+		return sdk.WrapError(errP, "extractWorkflow> Unable to get workflow from file")
+	}
+	w = workflowPushed
+	return nil
+}
+
+func ReadCDSFiles(files map[string][]byte) (*tar.Reader, error) {
+	// Create a buffer to write our archive to.
+	buf := new(bytes.Buffer)
+	// Create a new tar archive.
+	tw := tar.NewWriter(buf)
+	// Add some files to the archive.
+	for fname, fcontent := range files {
+		log.Debug("ReadCDSFiles> Reading %s", fname)
+		hdr := &tar.Header{
+			Name: filepath.Base(fname),
+			Mode: 0600,
+			Size: int64(len(fcontent)),
+		}
+		if err := tw.WriteHeader(hdr); err != nil {
+			return nil, sdk.WrapError(err, "ReadCDSFiles> Cannot write header")
+		}
+		if n, err := tw.Write(fcontent); err != nil {
+			return nil, sdk.WrapError(err, "ReadCDSFiles> Cannot write content")
+		} else if n == 0 {
+			return nil, fmt.Errorf("nothing to write")
+		}
+	}
+	// Make sure to check the error on Close.
+	if err := tw.Close(); err != nil {
+		return nil, err
+	}
+
+	return tar.NewReader(buf), nil
+}
+
+func pollRepositoryOperation(c context.Context, db gorp.SqlExecutor, store cache.Store, ope *sdk.Operation) error {
+	tickTimeout := time.NewTicker(10 * time.Minute)
+	tickPoll := time.NewTicker(2 * time.Second)
+	defer tickTimeout.Stop()
+	for {
+		select {
+		case <-c.Done():
+			if c.Err() != nil {
+				return sdk.WrapError(c.Err(), "pollRepositoryOperation> Exiting")
+			}
+		case <-tickTimeout.C:
+			return sdk.WrapError(sdk.ErrRepoOperationTimeout, "pollRepositoryOperation> Timeout analyzing repository")
+		case <-tickPoll.C:
+			if err := GetRepositoryOperation(db, store, ope); err != nil {
+				return sdk.WrapError(err, "pollRepositoryOperation> Cannot get repository operation status")
+			}
+			switch ope.Status {
+			case sdk.OperationStatusError:
+				return sdk.WrapError(fmt.Errorf("%s", ope.Error), "getImportAsCodeHandler> Operation in error. %+v", ope)
+			case sdk.OperationStatusDone:
+				return nil
+			}
+			continue
+		}
+	}
+}
+
+func createOperationRequest(w sdk.Workflow, opts sdk.WorkflowRunPostHandlerOption) (sdk.Operation, error) {
+	ope := sdk.Operation{}
+	if w.Root.Context.Application == nil {
+		return ope, sdk.WrapError(sdk.ErrApplicationNotFound, "CreateFromRepository> Workflow node root does not have a application context")
+	}
+	ope = sdk.Operation{
+		VCSServer:          w.Root.Context.Application.VCSServer,
+		RepoFullName:       w.Root.Context.Application.RepositoryFullname,
+		URL:                w.FromRepository,
+		RepositoryStrategy: w.Root.Context.Application.RepositoryStrategy,
+		Setup: sdk.OperationSetup{
+			Checkout: sdk.OperationCheckout{
+				Branch: "",
+				Commit: "",
+			},
+		},
+		LoadFiles: sdk.OperationLoadFiles{
+			Pattern: WorkflowAsCodePattern,
+		},
+	}
+
+	var branch, commit string
+	if opts.Hook != nil {
+		branch = opts.Hook.Payload[tagGitBranch]
+		commit = opts.Hook.Payload[tagGitHash]
+	}
+	if opts.Manual != nil {
+		e := dump.NewDefaultEncoder(new(bytes.Buffer))
+		e.Formatters = []dump.KeyFormatterFunc{dump.WithDefaultLowerCaseFormatter()}
+		e.ExtraFields.DetailedMap = false
+		e.ExtraFields.DetailedStruct = false
+		e.ExtraFields.Len = false
+		e.ExtraFields.Type = false
+		m1, errm1 := e.ToStringMap(opts.Manual.Payload)
+		if errm1 != nil {
+			return ope, sdk.WrapError(errm1, "CreateFromRepository> Unable to compute payload")
+		}
+		branch = m1[tagGitBranch]
+		commit = m1[tagGitHash]
+	}
+	ope.Setup.Checkout.Commit = commit
+	ope.Setup.Checkout.Branch = branch
+
+	return ope, nil
+}
+
+// PostRepositoryOperation creates a new repository operation
+func PostRepositoryOperation(db gorp.SqlExecutor, cache cache.Store, ope *sdk.Operation) error {
+	querier := services.Querier(db, cache)
+	srvs, err := querier.FindByType(services.TypeRepositories)
+	if err != nil {
+		return sdk.WrapError(err, "PostRepositoryOperation> Unable to found repositories service")
+	}
+	if _, err := services.DoJSONRequest(srvs, http.MethodPost, "/operations", ope, ope); err != nil {
+		return sdk.WrapError(err, "PostRepositoryOperation> Unable to perform operation")
+	}
+	return nil
+}
+
+// GetRepositoryOperation get repository operation status
+func GetRepositoryOperation(db gorp.SqlExecutor, store cache.Store, ope *sdk.Operation) error {
+	querier := services.Querier(db, store)
+	srvs, err := querier.FindByType(services.TypeRepositories)
+	if err != nil {
+		return sdk.WrapError(err, "GetRepositoryOperation> Unable to found repositories service")
+	}
+
+	if _, err := services.DoJSONRequest(srvs, http.MethodGet, "/operations/"+ope.UUID, nil, ope); err != nil {
+		return sdk.WrapError(err, "GetRepositoryOperation> Unable to get operation")
+	}
+	return nil
+}
