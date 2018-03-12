cfd20ceca378fa1e20a76316d288de2b3eb266a5
Guiheux Steven <steven.guiheux+github@gmail.com>
2018-03-12 09:52:27 +0100

    fix(api): return error when branch is deleted from repository during … (#2360)

--- sdk/messages.go
+++ sdk/messages.go
@@ -75,6 +75,7 @@ var (
 	MsgWorkflowImportedUpdated             = &Message{"MsgWorkflowImportedUpdated", trad{FR: "Le workflow %s a été mis à jour", EN: "Workflow %s has been updated"}, nil}
 	MsgWorkflowImportedInserted            = &Message{"MsgWorkflowImportedInserted", trad{FR: "Le workflow %s a été créé", EN: "Workflow %s has been created"}, nil}
 	MsgSpawnInfoHatcheryCannotStartJob     = &Message{"MsgSpawnInfoHatcheryCannotStart", trad{FR: "Aucune hatchery n'a pu démarrer de worker respectant vos pré-requis de job, merci de les vérifier.", EN: "No hatchery can spawn a worker corresponding your job's requirements. Please check your job's requirements."}, nil}
+	MsgWorkflowRunBranchDeleted            = &Message{"MsgWorkflowRunBranchDeleted", trad{FR: "La branche %s  a été supprimée", EN: "Branch %s has been deleted"}, nil}
 )
 
 // Messages contains all sdk Messages
@@ -132,6 +133,7 @@ var Messages = map[string]*Message{
 	MsgWorkflowNodeMutex.ID:                   MsgWorkflowNodeMutex,
 	MsgWorkflowNodeMutexRelease.ID:            MsgWorkflowNodeMutexRelease,
 	MsgSpawnInfoHatcheryCannotStartJob.ID:     MsgSpawnInfoHatcheryCannotStartJob,
+	MsgWorkflowRunBranchDeleted.ID:            MsgWorkflowRunBranchDeleted,
 }
 
 //Message represent a struc format translated messages
