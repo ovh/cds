4a6f10d33f7e15bab90e08538995894bc9520744
François Samin <francois.samin@corp.ovh.com>
2018-03-13 21:04:30 +0100

    fix

--- engine/hatchery/swarm/swarm_test.go
+++ engine/hatchery/swarm/swarm_test.go
@@ -4,9 +4,15 @@ import (
 	"testing"
 
 	docker "github.com/docker/docker/client"
+	"github.com/ovh/cds/sdk/log"
 )
 
+func init() {
+	log.Initialize(&log.Conf{Level: "debug"})
+}
+
 func testSwarmHatchery(t *testing.T) *HatcherySwarm {
+	log.SetLogger(t)
 	dockerClient, err := docker.NewEnvClient()
 	if err != nil {
 		t.Logf("unable to get docker client: %v. Skipping this test", err)
