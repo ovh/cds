1132d1986dee80cf87172d34ff218ee6909e7f98
Yvonnick Esnault <yesnault@users.noreply.github.com>
2018-03-14 16:33:13 +0100

    doc (worker): doc about cache (#2385)

--- engine/worker/cmd_cache.go
+++ engine/worker/cmd_cache.go
@@ -24,11 +24,38 @@ func cmdCache(w *currentWorker) *cobra.Command {
 	cmdCacheRoot := &cobra.Command{
 		Use: "cache",
 		Long: `
-Inside a project, you can create or retrieve a cache from your worker with a tag (useful for vendors for example)
+Inside a project, you can create or retrieve a cache from your worker with a tag (useful for vendors for example).
 
 You can access to this cache from any workflow inside a project. You just have to choose a tag that fits with your needs.
 
 For example if you need a different cache for each workflow so choose a tag scoped with your workflow name and workflow version (example of tag value: {{.cds.workflow}}-{{.cds.version}})
+
+## Use Case
+Java Developers often use maven to manage dependencies. The mvn install command could be long because all the maven dependencies have to be downloaded on a fresh CDS Job workspace.
+With the worker cache feature, you don't have to download the dependencies if they haven't been updated since the last run of the job.
+
+
+- cache push: take the current .m2/ directory and set it as a cache
+- cache pull: download a cache of .m2 directory
+
+Here, an example of a script inside a CDS Job using the cache feature:
+
+	#!/bin/bash
+
+	tag=($(md5sum pom.xml))
+
+	# download the cache of .m2/
+	worker cache pull $tag 
+
+	# update the directory .m2/
+	# as there is a cache, mvn does not need to download all dependencies
+	# if they are not updated on upstream
+	mvn install 
+
+	# put in cache the updated .m2/ directory
+	worker cache push $tag .m2/
+
+
     `,
 		Short: "Inside a project, you can create or retrieve a cache from your worker with a tag",
 	}
@@ -179,7 +206,19 @@ func cmdCachePull(w *currentWorker) *cobra.Command {
 		Short:   "worker cache pull tagValue",
 		Long: `
 Inside a project, you can fetch a cache from your worker with a tag
+
 	worker pull <tagValue>
+
+If you push a cache with:
+
+	worker cache push latest {{.cds.workspace}}/pathToUpload
+
+The command:
+
+	worker cache pull latest
+
+will create the directory {{.cds.workspace}}/pathToUpload with the content of the cache
+
 		`,
 		Run: cachePullCmd(w),
 	}
