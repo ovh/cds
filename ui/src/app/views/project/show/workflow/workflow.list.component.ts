695f6f1fd0d6f2f2c2d5af1062c90302e70b37bc
Coenen Benjamin <benjamin.coenen@hotmail.com>
2018-03-26 09:34:03 +0200

    feat(ui): add search on list (applications, pipelines, workflows) (#2464)
    Signed-off-by: Benjamin Coenen <benjamin.coenen@hotmail.com>

--- ui/src/app/views/project/show/workflow/workflow.list.component.ts
+++ ui/src/app/views/project/show/workflow/workflow.list.component.ts
@@ -8,7 +8,33 @@ import {Project} from '../../../../model/project.model';
 })
 export class ProjectWorkflowListComponent {
 
-    @Input() project: Project;
+  @Input()
+  set project(project: Project) {
+    this._project = project;
+    if (project.workflow_names) {
+      let filter = this.filter.toLowerCase();
+      this.filteredWorkflows = project.workflow_names.filter((wf) => wf.toLowerCase().indexOf(filter) !== -1);
+    }
+  }
+  get project(): Project {
+    return this._project;
+  }
+
+  set filter(filter: string) {
+    this._filter = filter;
+    if (this.project.workflow_names) {
+      let filterLower = filter.toLowerCase();
+      this.filteredWorkflows = this.project.workflow_names.filter((wf) => wf.toLowerCase().indexOf(filterLower) !== -1);
+    }
+  }
+  get filter(): string {
+    return this._filter;
+  }
+
+  _project: Project;
+  _filter = '';
+
+  filteredWorkflows: Array<string> = [];
 
     constructor() { }
 }
