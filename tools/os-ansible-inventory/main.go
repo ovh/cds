697704fef612608f0deb8fed577896fb34632d71
François Samin <francois.samin@corp.ovh.com>
2018-03-30 17:15:33 +0200

    feat(tools): os-ansible-inventory

---
+++ tools/os-ansible-inventory/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
+	"github.com/urfave/cli"
+)
+
+var (
+	osRegionName string
+)
+
+func init() {
+	osRegionName = os.Getenv("OS_REGION_NAME")
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "os-ansible-inventory"
+	app.Usage = "Openstack Ansible Inventory base on metadata"
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.StringSliceFlag{
+			Name: "filter",
+		},
+		cli.StringFlag{
+			Name: "group-by",
+		},
+		cli.StringFlag{
+			Name: "net",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		filters := c.StringSlice("filter")
+
+		fList := []Filter{}
+		for _, f := range filters {
+			tuple := strings.Split(f, "=")
+			if len(tuple) == 2 {
+				fList = append(fList, Filter{
+					Key:      tuple[0],
+					Value:    tuple[1],
+					Operator: "=",
+				})
+			}
+
+			tuple = strings.Split(f, "~")
+			if len(tuple) == 2 {
+				fList = append(fList, Filter{
+					Key:      tuple[0],
+					Value:    tuple[1],
+					Operator: "~",
+				})
+			}
+		}
+
+		groupBy := c.String("group-by")
+		net := c.String("net")
+
+		return do(fList, groupBy, net)
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func do(filters []Filter, groupBy, net string) error {
+	auth, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		return err
+	}
+
+	provider, err := openstack.AuthenticatedClient(auth)
+	if err != nil {
+		return err
+	}
+
+	nova, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{Region: osRegionName})
+	if err != nil {
+		return err
+	}
+
+	all, err := servers.List(nova, nil).AllPages()
+	if err != nil {
+		return err
+	}
+	serverList, err := servers.ExtractServers(all)
+	if err != nil {
+		return err
+	}
+
+	list := &ServerList{
+		l: serverList,
+	}
+
+	filter(list, filters)
+	if list.err != nil {
+		return list.err
+	}
+
+	return render(list, groupBy, net)
+}
+
+func filter(l *ServerList, filters []Filter) {
+	for _, f := range filters {
+		if l.err != nil {
+			return
+		}
+		l.Filter(f)
+	}
+}
+
+func getIPAdress(s *servers.Server, net string, netVersion int) string {
+	if netVersion == 0 {
+		netVersion = 4
+	}
+
+	address := map[string]string{}
+
+	for k, v := range s.Addresses {
+		switch v.(type) {
+		case []interface{}:
+			for _, z := range v.([]interface{}) {
+				var addr string
+				var version int
+				for x, y := range z.(map[string]interface{}) {
+					if x == "addr" {
+						addr = y.(string)
+					}
+					if x == "version" {
+						version = int(y.(float64))
+					}
+				}
+				//we only support IPV4
+				if addr != "" && version == netVersion {
+					address[k] = addr
+				}
+			}
+		}
+	}
+
+	if net != "" {
+		return address[net]
+	}
+
+	return ""
+}
+
+func render(l *ServerList, groupBy string, net string) error {
+	i := &Inventory{
+		Groups: map[string]Group{},
+	}
+
+	if groupBy == "" {
+		g := Group{}
+		for _, s := range l.l {
+			g.Hosts = append(g.Hosts, Host{
+				Address: getIPAdress(&s, net, 4),
+				Name:    s.Name,
+				Port:    "22",
+				User:    "ubuntu",
+			})
+		}
+		i.Groups["all"] = g
+	}
+
+	t := template.New("t")
+	t, err := t.Parse(iniFile)
+	if err != nil {
+		return err
+	}
+
+	err = t.Execute(os.Stdout, i)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var iniFile = `
+{{- range $key, $value := .Groups }}
+[{{$key}}]
+{{ range $value.Hosts}}{{ .Name }}	ansible_connection=ssh	ansible_host={{ .Address }}	ansible_port={{ .Port }}	ansible_user={{ .User }}{{ end }}
+{{ end -}}
+`
---
+++ tools/os-ansible-inventory/types.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
+)
+
+// Host is a map of HostVar, keyed by a hostname.
+type Host struct {
+	Name    string
+	Address string
+	Port    string
+	User    string
+}
+
+// Group is struct that contains a list of hosts.
+type Group struct {
+	Hosts []Host `yaml:"hosts,omitempty"`
+}
+
+// Groups is a map of groups, keyed by a group name.
+type Groups map[string]Group
+
+// Inventory represents an Ansible Inventory consists of host variables and groups.
+type Inventory struct {
+	Groups Groups `json:"groups,omitempty"`
+}
+
+// Filter represent a filter on server matadata
+type Filter struct {
+	Key, Operator, Value string
+}
+
+// Match checks if a key, valur pair matches with the filter
+func (f Filter) Match(k, v string) (bool, error) {
+	switch f.Operator {
+	case "=":
+		if strings.ToUpper(k) == strings.ToUpper(f.Key) && strings.ToUpper(v) == strings.ToUpper(f.Value) {
+			return true, nil
+		}
+	case "~":
+		if strings.ToUpper(k) == strings.ToUpper(f.Key) {
+			r, err := regexp.Compile(f.Value)
+			if err != nil {
+				return false, err
+			}
+			return r.Match([]byte(v)), nil
+		}
+	default:
+		return false, fmt.Errorf("unsupported operator %s", f.Operator)
+	}
+	return false, nil
+}
+
+// ServerList is a list of server
+type ServerList struct {
+	l   []servers.Server
+	err error
+}
+
+// Filter  a list of server
+func (l *ServerList) Filter(f Filter) {
+	if l.err != nil {
+		fmt.Println(l.err)
+		return
+	}
+
+	list := []servers.Server{}
+	for _, s := range l.l {
+		for k, v := range s.Metadata {
+			ok, err := f.Match(k, v)
+			if err != nil {
+				l.err = err
+				return
+			}
+			if ok {
+				list = append(list, s)
+			}
+		}
+	}
+	l.l = list
+}
