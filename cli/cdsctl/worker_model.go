c94f280295541b189a68f6eccc0b66084913fa3c
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2018-04-04 11:31:20 +0200

    fix (cdsctl): check err on worker model delete
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- cli/cdsctl/worker_model.go
+++ cli/cdsctl/worker_model.go
@@ -189,7 +189,7 @@ var workerModelDeleteCmd = cli.Command{
 
 func workerModelDeleteRun(v cli.Values) error {
 	if err := client.WorkerModelDelete(v.GetString("name")); err != nil {
-		if err.Error() == "worker model not found" && v.GetBool("force") {
+		if err.Error() == "worker model does not exist" && v.GetBool("force") {
 			return nil
 		}
 		return err
