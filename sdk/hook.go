b5adcf18b234a76a257f3541a00210bdf361e340
Steven GUIHEUX <steven.guiheux@corp.ovh.com>
2018-05-17 17:53:34 +0200

    revert hook

--- sdk/hook.go
+++ sdk/hook.go
@@ -138,6 +138,35 @@ type Hook struct {
 	Link          string   `json:"link"`
 }
 
+// AddHook creates a new hook between a pipeline and a repository
+func AddHook(a *Application, p *Pipeline, host string, project string, repository string) (*Hook, error) {
+	h := Hook{
+		Pipeline:      *p,
+		ApplicationID: a.ID,
+		Kind:          "stash",
+		Host:          host,
+		Project:       project,
+		Repository:    repository,
+	}
+
+	data, err := json.Marshal(h)
+	if err != nil {
+		return nil, err
+	}
+
+	uri := fmt.Sprintf("/project/%s/application/%s/pipeline/%s/hook", p.ProjectKey, a.Name, p.Name)
+	data, _, err = Request("POST", uri, data)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(data, &h); err != nil {
+		return nil, err
+	}
+
+	return &h, nil
+}
+
 // GetHooks lists all hooks related to a pipeline
 func GetHooks(project, application, pipeline string) ([]Hook, error) {
 	var hooks []Hook
@@ -159,6 +188,22 @@ func GetHooks(project, application, pipeline string) ([]Hook, error) {
 	}
 
 	return hooks, nil
+}
+
+// DeleteHook remove a hook previously created
+func DeleteHook(project, application, pipeline string, id int64) error {
+	uri := fmt.Sprintf("/project/%s/application/%s/pipeline/%s/hook/%d", project, application, pipeline, id)
+
+	_, code, err := Request("DELETE", uri, nil)
+	if err != nil {
+		return err
+	}
+
+	if code >= 300 {
+		return fmt.Errorf("HTTP %d", code)
+	}
+
+	return nil
 }
 
 // GetDefaultHookModel return the workflow hook model by its name
