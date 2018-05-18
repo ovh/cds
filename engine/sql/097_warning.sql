9eb6c471db160a8baea11282b47a18202aa328f3
Steven GUIHEUX <steven.guiheux@corp.ovh.com>
2018-05-18 10:58:07 +0200

    fix migrate

--- engine/sql/097_warning.sql
+++ engine/sql/097_warning.sql
@@ -4,3 +4,4 @@ DROP INDEX IDX_WARNING;
 select create_unique_index('warning', 'IDX_WARNING', 'project_key, application_name, pipeline_name, environment_name, workflow_name, type, element');
 
 -- +migrate Down
+SELECT 1;
