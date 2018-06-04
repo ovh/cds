17194d2fd4a9bd89ca57ac1f956f43bb65c49b02
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2018-06-04 07:34:44 +0200

    feat(ui): add queue view in UI admin section
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- ui/src/app/views/admin/admin.module.ts
+++ ui/src/app/views/admin/admin.module.ts
@@ -8,8 +8,6 @@ import {MigrationProjectComponent} from './migration/project/migration.project.c
 import {WorkerModelPatternComponent} from './worker-model-pattern/worker-model-pattern.component';
 import {WorkerModelPatternAddComponent} from './worker-model-pattern/add/worker-model-pattern.add.component';
 import {WorkerModelPatternEditComponent} from './worker-model-pattern/edit/worker-model-pattern.edit.component';
-import {StatusComponent} from './status/status.component';
-import {QueueComponent} from './queue/queue.component';
 import {BroadcastAddComponent} from './broadcast/add/broadcast.add.component';
 import {BroadcastEditComponent} from './broadcast/edit/broadcast.edit.component';
 import {BroadcastListComponent} from './broadcast/list/broadcast.list.component';
@@ -19,8 +17,6 @@ import {BroadcastListComponent} from './broadcast/list/broadcast.list.component'
         AdminComponent,
         MigrationOverviewComponent,
         MigrationProjectComponent,
-        StatusComponent,
-        QueueComponent,
         WorkerModelPatternComponent,
         WorkerModelPatternAddComponent,
         WorkerModelPatternEditComponent,
