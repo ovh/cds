81b474313ae1d13980ae52356c4cd53d9ee06e79
François Samin <francois.samin+github@gmail.com>
2018-06-19 11:21:05 +0200

    feat(api): set database role (#2920)

--- engine/api/api.go
+++ engine/api/api.go
@@ -455,6 +455,7 @@ func (a *API) Serve(ctx context.Context) error {
 	var errDB error
 	a.DBConnectionFactory, errDB = database.Init(
 		a.Config.Database.User,
+		a.Config.Database.Role,
 		a.Config.Database.Password,
 		a.Config.Database.Name,
 		a.Config.Database.Host,
