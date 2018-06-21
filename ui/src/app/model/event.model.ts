6981d7f20b9ec96b17d5d310b9039c942dd1c157
Steven GUIHEUX <steven.guiheux@corp.ovh.com>
2018-06-21 11:09:08 +0200

    fix event

--- ui/src/app/model/event.model.ts
+++ ui/src/app/model/event.model.ts
@@ -3,6 +3,7 @@ export class Event {
     hostname: string;
     cdsname: string;
     type_event: string;
+    payload: {};
     attempt: number;
     username: string;
     user_mail: string;
@@ -14,4 +15,39 @@ export class Event {
     status: string;
     workflow_run_num: number;
     workflow_run_num_sub: number;
+}
+
+export class EventType {
+    static PROJECT_PREFIX = 'sdk.EventProject';
+    static PROJECT_DELETE = 'sdk.EventProjectDelete';
+    static PROJECT_VARIABLE_PREFIX = 'sdk.EventProjectVariable';
+    static PROJECT_PERMISSION_PREFIX = 'sdk.EventProjectPermission';
+    static PROJECT_KEY_PREFIX = 'sdk.EventProjectKey';
+    static PROJECT_PLATFORM_PREFIX = 'sdk.EventProjectPlatform';
+
+    static ENVIRONMENT_PREFIX = 'sdk.EventEnvironment';
+
+    static APPLICATION_PREFIX = 'sdk.EventApplication';
+    static APPLICATION_ADD = 'sdk.EventApplicationAdd';
+    static APPLICATION_UPDATE = 'sdk.EventApplicationUpdate';
+    static APPLICATION_DELETE = 'sdk.EventApplicationDelete';
+
+    static PIPELINE_PREFIX = 'sdk.EventPipeline';
+    static PIPELINE_ADD = 'sdk.EventPipelineAdd';
+    static PIPELINE_UPDATE = 'sdk.EventPipelineUpdate';
+    static PIPELINE_DELETE = 'sdk.EventPipelineDelete';
+    static PIPELINE_PARAMETER_PREFIX = 'sdk.EventPipelineParameter';
+
+    static WORKFLOW_PREFIX = 'sdk.EventWorkflow';
+    static WORKFLOW_ADD = 'sdk.EventWorkflowAdd';
+    static WORKFLOW_UPDATE = 'sdk.EventWorkflowUpdate';
+    static WORKFLOW_DELETE = 'sdk.EventWorkflowDelete';
+
+    static RUN_WORKFLOW_PREFIX = 'sdk.EventRunWorkflow';
+    static RUN_WORKFLOW_NODE = 'sdk.EventRunWorkflowNode';
+
+    static BROADCAST_PREFIX = 'sdk.EventBroadcast';
+    static BROADCAST_ADD = 'sdk.EventBroadcastAdd';
+    static BROADCAST_UPDATE = 'sdk.EventBroadcastUpdate';
+    static BROADCAST_DELETE = 'sdk.EventBroadcastDelete';
 }
