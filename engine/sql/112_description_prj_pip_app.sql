2047b88263dd9968090e859f9fc783f4dc443822
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2018-07-06 17:37:29 +0200

    feat(api,ui): add description and icon to application
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- engine/sql/112_description_prj_pip_app.sql
+++ engine/sql/112_description_prj_pip_app.sql
@@ -5,6 +5,7 @@ ALTER TABLE application ADD COLUMN icon TEXT DEFAULT '';
 ALTER TABLE application DROP COLUMN description;
 ALTER TABLE application ADD COLUMN description TEXT DEFAULT '';
 ALTER TABLE pipeline ADD COLUMN description TEXT DEFAULT '';
+ALTER TABLE workflow ADD COLUMN icon TEXT DEFAULT '';
 
 -- +migrate Down
 ALTER TABLE project DROP COLUMN description;
@@ -13,3 +14,4 @@ ALTER TABLE application DROP COLUMN description;
 ALTER TABLE application ADD COLUMN description TEXT;
 ALTER TABLE application DROP COLUMN icon;
 ALTER TABLE pipeline DROP COLUMN description;
+ALTER TABLE workflow DROP COLUMN icon;
