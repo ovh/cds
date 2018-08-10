e8d36301d8c10c0dca337d08cbd20c9fc5eaf647
Yvonnick Esnault <yesnault@users.noreply.github.com>
2018-08-10 14:21:33 +0200

    feat(action/GitClone): set default value from git.url to empty (#3178)
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/sql/120_git_clone.sql
+++ engine/sql/120_git_clone.sql
@@ -1,9 +1,13 @@
 -- +migrate Up
 
-INSERT into action_parameter (action_id, name, description, type, value, advanced) values((select id from action where name = 'GitClone'), 'depth', 'gitClone use a depth of 50 by default. You can remove --depth with the value ''false''', 'string', '', true);
-INSERT into action_parameter (action_id, name, description, type, value, advanced) values((select id from action where name = 'GitClone'), 'submodules', 'gitClone clones submodules by default, you can set ''false'' to avoid this', 'boolean', 'true', true);
+INSERT into action_parameter (action_id, name, description, type, value, advanced) values((select id from action where name = 'GitClone' and type = 'Builtin'), 'depth', 'gitClone use a depth of 50 by default. You can remove --depth with the value ''false''', 'string', '', true);
+INSERT into action_parameter (action_id, name, description, type, value, advanced) values((select id from action where name = 'GitClone' and type = 'Builtin'), 'submodules', 'gitClone clones submodules by default, you can set ''false'' to avoid this', 'boolean', 'true', true);
+
+UPDATE action_parameter set value = '' where (name='url' or name = 'privateKey') and action_id=(select id from action where name = 'GitClone' and type = 'Builtin');
 
 -- +migrate Down
 
-DELETE from action_parameter where name = 'depth' and action_id = (select id from action where name = 'GitClone');
-DELETE from action_parameter where name = 'submodules' and action_id = (select id from action where name = 'GitClone');
+DELETE from action_parameter where name = 'depth' and action_id = (select id from action where name = 'GitClone' and type = 'Builtin');
+DELETE from action_parameter where name = 'submodules' and action_id = (select id from action where name = 'GitClone' and type = 'Builtin');
+
+UPDATE action_parameter set value = '{{.git.url}}' where name='url' and action_id=(select id from action where name = 'GitClone' and type = 'Builtin');
