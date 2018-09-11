33151ba84f27799515f27c5f87742350f1277b56
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2018-09-11 10:44:15 +0200

    fix(ui): let user put empty value in run condition
    close #fix_3311
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- ui/src/app/model/workflow.model.ts
+++ ui/src/app/model/workflow.model.ts
@@ -740,6 +740,10 @@ export class WorkflowNodeCondition {
     variable: string;
     operator: string;
     value: string;
+
+    constructor() {
+      this.value = '';
+    }
 }
 
 export class WorkflowTriggerConditionCache {
