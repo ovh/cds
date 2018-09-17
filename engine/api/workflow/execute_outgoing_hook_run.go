ee9a68d57b58293cad9e2828972e4c571188b3a9
François Samin <francois.samin@corp.ovh.com>
2018-09-17 15:45:51 +0200

    fix

--- engine/api/workflow/execute_outgoing_hook_run.go
+++ engine/api/workflow/execute_outgoing_hook_run.go
@@ -42,8 +42,8 @@ loop:
 	report.Add(hookRun)
 	log.Debug("UpdateOutgoingHookRunStatus> hook run updated: %v", hookRun)
 
-	report1, _, err := processWorkflowRun(ctx, db, store, proj, wr, nil, nil, nil) //nolint
-	report.Merge(report1, err)
+	report1, _, err := processWorkflowRun(ctx, db, store, proj, wr, nil, nil, nil)
+	report.Merge(report1, err) //nolint
 	if err != nil {
 		return nil, err
 	}
