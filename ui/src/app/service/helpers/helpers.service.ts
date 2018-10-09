42bfddba001ff8440a2ff67bceb8987c036956ca
Coenen Benjamin <benjamin.coenen@hotmail.com>
2018-10-09 18:26:19 +0200

    fix(ui): adjust font color for brightness with opacity (#3424)
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- ui/src/app/service/helpers/helpers.service.ts
+++ ui/src/app/service/helpers/helpers.service.ts
@@ -6,16 +6,22 @@ export class HelpersService {
 
   }
 
-  getBrightness(rgb) {
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(rgb);
+  getBrightness(rgb): number {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(rgb);
+    if (result && result.length > 4 && result[4] != null) {
+        return 255 - parseInt(result[4], 16);
+    }
+
     return result ?
       0.2126 * parseInt(result[1], 16) +
       0.7152 * parseInt(result[2], 16) +
       0.0722 * parseInt(result[3], 16) : 0;
   }
 
-  getBrightnessColor(rgb) {
-    if (this.getBrightness(rgb) > 130) {
+  getBrightnessColor(rgb): string {
+    let brightness = this.getBrightness(rgb);
+
+    if (brightness > 130) {
       return '#000000';
     }
     return '#ffffff';
