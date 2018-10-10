52abd38149353bdd2fdaa283f8b98b35538729b6
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2018-10-10 10:38:27 +0200

    fix(sdk): disable keepAlive
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- sdk/cdsclient/client.go
+++ sdk/cdsclient/client.go
@@ -29,10 +29,10 @@ func NewHTTPClient(timeout time.Duration, insecureSkipVerifyTLS bool) *http.Clie
 			Proxy: http.ProxyFromEnvironment,
 			DialContext: (&net.Dialer{
 				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
+				KeepAlive: 0 * time.Second,
 				DualStack: true,
 			}).DialContext,
-			MaxIdleConns:          50,
+			MaxIdleConns:          100,
 			IdleConnTimeout:       30 * time.Second,
 			TLSHandshakeTimeout:   10 * time.Second,
 			ExpectContinueTimeout: 1 * time.Second,
