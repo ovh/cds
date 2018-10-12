cae489a65bc7e541099160720f7888b1d68c3e2d
François Samin <francois.samin@corp.ovh.com>
2018-10-12 17:10:51 +0200

    fix

--- engine/api/events.go
+++ engine/api/events.go
@@ -128,6 +128,7 @@ func (b *eventsBroker) Start(ctx context.Context) {
 							log.Debug("send data to %s", c.UUID)
 							if err := c.Send(receivedEvent); err != nil {
 								log.Error("eventsBroker> unable to send event to %s: %v", c.UUID, err)
+								b.chanRemoveClient <- c.UUID
 							}
 						}
 					},
