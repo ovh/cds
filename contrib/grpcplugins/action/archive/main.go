efbf645fcffbc974a574dbdb9d7f8756fa860653
mdouchement <marc.douchement@gmail.com>
2018-10-18 10:19:11 +0200

    plugin-archive contribution

--- contrib/grpcplugins/action/archive/main.go
+++ contrib/grpcplugins/action/archive/main.go
@@ -24,7 +24,7 @@ func (actPlugin *archiveActionPlugin) Manifest(ctx context.Context, _ *empty.Emp
 	return &actionplugin.ActionPluginManifest{
 		Name:        "plugin-archive",
 		Author:      "mdouchement <https://github.com/mdouchement>",
-		Description: `This is a plugin to compress or uncompress an archive`,
+		Description: `This is a plugin to compress or uncompress an archive. Supported formats: .zip, .tar, .tar.gz, .tar.bz2, .tar.xz, .tar.lz4, .tar.sz, and .rar (extract-only)`,
 		Version:     sdk.VERSION,
 	}, nil
 }
---
+++ docs/content/workflows/pipelines/actions/plugins/plugin-archive.md
@@ -0,0 +1,28 @@
++++
+title = "plugin-archive"
+
++++
+
+This is a plugin to compress or uncompress an archive.
+
+## Supported formats
+
+* `.zip`
+* `.tar`
+* `.tar.gz`
+* `.tar.bz2`
+* `.tar.xz`
+* `.tar.lz4`
+* `.tar.sz`
+* `.rar` (extract-only)
+
+## Parameters
+
+* **source**: The source path
+* **destination**: The destination path
+* **action**: `compress` or `uncompress` an archive
+
+
+## More
+
+More documentation on [Github](https://github.com/ovh/cds/tree/master/contrib/grpcplugins/action/archive/README.md)
