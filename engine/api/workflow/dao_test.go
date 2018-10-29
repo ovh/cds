ae5ccb04a53b1171e7547d12e8c1d907c4223013
François Samin <francois.samin@corp.ovh.com>
2018-10-29 16:22:08 +0100

    fix unit tests

--- engine/api/workflow/dao_test.go
+++ engine/api/workflow/dao_test.go
@@ -1019,7 +1019,7 @@ func TestInsertComplexeWorkflowWithComplexeJoins(t *testing.T) {
 			sdk.WorkflowNotification{
 				Type:           "jabber",
 				SourceNodeRefs: []string{"pip6", "pip5"},
-				Settings: &sdk.JabberEmailUserNotificationSettings{
+				Settings: sdk.UserNotificationSettings{
 					OnFailure:    sdk.UserNotificationAlways,
 					OnStart:      true,
 					OnSuccess:    sdk.UserNotificationAlways,
