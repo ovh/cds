1c9ec23ad69c5938d1e3913a11a7bfd74e588646
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2018-11-04 22:11:21 +0100

    cr
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/worker/run_plugin.go
+++ engine/worker/run_plugin.go
@@ -66,8 +66,10 @@ func startGRPCPlugin(ctx context.Context, pluginName string, w *currentWorker, p
 	if binary == nil {
 		var errBi error
 		binary, errBi = w.client.PluginGetBinaryInfos(pluginName, currentOS, currentARCH)
-		if errBi != nil || binary == nil {
-			return nil, fmt.Errorf("plugin:%s Unable to get plugin binary infos... Aborting (%v)", pluginName, errBi)
+		if errBi != nil {
+			return nil, sdk.WrapError(errBi, "plugin:%s Unable to get plugin binary infos... Aborting", pluginName)
+		} else if binary == nil {
+			return nil, fmt.Errorf("plugin:%s Unable to get plugin binary infos - binary is nil... Aborting", pluginName)
 		}
 	}
 
@@ -90,17 +92,17 @@ func startGRPCPlugin(ctx context.Context, pluginName string, w *currentWorker, p
 	log.Info("Starting GRPC Plugin %s in dir %s", binary.Name, dir)
 	fileContent, err := ioutil.ReadFile(path.Join(w.basedir, binary.GetName()))
 	if err != nil {
-		return nil, fmt.Errorf("plugin:%s unable to get plugin binary file... Aborting (%v)", pluginName, err)
+		return nil, sdk.WrapError(err, "plugin:%s unable to get plugin binary file... Aborting", pluginName)
 	}
 
 	switch {
 	case sdk.IsTar(fileContent):
 		if err := sdk.Untar(w.basedir, bytes.NewReader(fileContent)); err != nil {
-			return nil, fmt.Errorf("plugin:%s unable to untar binary file (%v)", pluginName, err)
+			return nil, sdk.WrapError(err, "plugin:%s unable to untar binary file", pluginName)
 		}
 	case sdk.IsGz(fileContent):
 		if err := sdk.UntarGz(w.basedir, bytes.NewReader(fileContent)); err != nil {
-			return nil, fmt.Errorf("plugin:%s unable to untarGz binary file (%v)", pluginName, err)
+			return nil, sdk.WrapError(err, "plugin:%s unable to untarGz binary file", pluginName)
 		}
 	}
 
@@ -112,13 +114,13 @@ func startGRPCPlugin(ctx context.Context, pluginName string, w *currentWorker, p
 	if _, err := exec.LookPath(cmd); err != nil {
 		cmd = path.Join(w.basedir, cmd)
 		if _, err := exec.LookPath(cmd); err != nil {
-			return nil, fmt.Errorf("plugin:%s unable to start GRPC plugin, binary command not found (%v)", pluginName, err)
+			return nil, sdk.WrapError(err, "plugin:%s unable to start GRPC plugin, binary command not found.", pluginName)
 		}
 	}
 	args := append(binary.Entrypoints, binary.Args...)
 	var errstart error
 	if c.StdoutPipe, c.StderrPipe, c.Socket, errstart = grpcplugin.StartPlugin(ctx, pluginName, dir, cmd, args, envs); errstart != nil {
-		return nil, fmt.Errorf("plugin:%s unable to start GRPC plugin... Aborting. err:%v", pluginName, errstart)
+		return nil, sdk.WrapError(errstart, "plugin:%s unable to start GRPC plugin... Aborting", pluginName)
 	}
 
 	registerPluginClient(w, pluginName, &c)
