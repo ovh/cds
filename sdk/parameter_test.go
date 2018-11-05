1b22bdd539551ca0d947280b6b646d2c8c787ab2
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2018-11-05 16:40:49 +0100

    fix(api): merge multiple payload (close #3550)
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

---
+++ sdk/parameter_test.go
@@ -0,0 +1,45 @@
+package sdk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParameterMapMerge(t *testing.T) {
+	original := map[string]string{
+		"test":     "value1",
+		"default":  "ok",
+		"git.hash": "XXX",
+	}
+	override := map[string]string{
+		"test":     "override",
+		"git.hash": "XXXBIS",
+	}
+
+	res := ParametersMapMerge(original, override)
+	assert.Equal(t, "override", res["test"])
+	assert.Equal(t, "ok", res["default"])
+	assert.Equal(t, "XXX", res["git.hash"])
+}
+
+func TestParametersMerge(t *testing.T) {
+	original := []Parameter{
+		Parameter{Name: "test", Value: "value1"},
+		Parameter{Name: "default", Value: "ok"},
+	}
+	override := []Parameter{
+		Parameter{Name: "test", Value: "override"},
+	}
+
+	res := ParametersMerge(original, override)
+	for _, param := range res {
+		if param.Name == "test" {
+			assert.Equal(t, "override", param.Value)
+			continue
+		}
+		if param.Name == "default" {
+			assert.Equal(t, "ok", param.Value)
+		}
+	}
+}
