c834d7fed31c66a6e35add8760dc0a1688f020bb
Steven GUIHEUX <steven.guiheux@corp.ovh.com>
2018-11-30 09:08:13 +0100

    fix language awareness

--- ui/src/app/service/auth.interceptor.service.ts
+++ ui/src/app/service/auth.interceptor.service.ts
@@ -3,27 +3,37 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {AuthentificationStore} from './auth/authentification.store';
+import {LanguageStore} from './language/language.store';
 
 @Injectable()
 export class AuthentificationInterceptor implements  HttpInterceptor {
 
-    constructor(private _authStore: AuthentificationStore) {}
+    languageHeader = 'en-US';
+    constructor(private _authStore: AuthentificationStore, private _language: LanguageStore) {
+        _language.get().subscribe( l => {
+            if (l) {
+                this.languageHeader = l;
+            }
+        });
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (req.url.indexOf('assets/i18n') !== -1) {
             const clone = req.clone();
             return next.handle(clone);
         } else {
-            const clone = req.clone( { setHeaders: this.addAuthHeader(), url: environment.apiURL + req.url });
+            const clone = req.clone( { setHeaders: this.addHeader(), url: environment.apiURL + req.url });
             return next.handle(clone);
         }
     }
 
-    addAuthHeader(): any {
+    addHeader(): any {
+        let headers = {};
         if (this._authStore.isConnected()) {
             let sessionToken = this._authStore.getSessionToken();
-            return { 'Session-Token': sessionToken };
+            headers['Session-Token'] = sessionToken;
         }
-        return {}
+        headers['Accept-Language'] = this.languageHeader;
+        return headers
     }
 }
