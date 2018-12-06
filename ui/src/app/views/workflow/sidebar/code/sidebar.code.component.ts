f6fb80bbd32b59f91afa0266910c72e3d5f6db18
Coenen Benjamin <benjamin.coenen@hotmail.com>
2018-12-06 13:18:33 +0100

    fix(ui): unselect all node when update via edit as code (#3690) (#3700)
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- ui/src/app/views/workflow/sidebar/code/sidebar.code.component.ts
+++ ui/src/app/views/workflow/sidebar/code/sidebar.code.component.ts
@@ -1,16 +1,18 @@
-import {Component, Input, ViewChild} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
-import {CodemirrorComponent} from 'ng2-codemirror-typescript/Codemirror';
-import {Subscription} from 'rxjs';
-import {finalize} from 'rxjs/operators';
-import {PermissionValue} from '../../../../model/permission.model';
-import {Project} from '../../../../model/project.model';
-import {Workflow} from '../../../../model/workflow.model';
-import {WorkflowCoreService} from '../../../../service/workflow/workflow.core.service';
-import {WorkflowService} from '../../../../service/workflow/workflow.service';
-import {WorkflowStore} from '../../../../service/workflow/workflow.store';
-import {AutoUnsubscribe} from '../../../../shared/decorator/autoUnsubscribe';
-import {ToastService} from '../../../../shared/toast/ToastService';
+import { Component, Input, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { CodemirrorComponent } from 'ng2-codemirror-typescript/Codemirror';
+import { Subscription } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { PermissionValue } from '../../../../model/permission.model';
+import { Project } from '../../../../model/project.model';
+import { Workflow } from '../../../../model/workflow.model';
+import { WorkflowCoreService } from '../../../../service/workflow/workflow.core.service';
+import { WorkflowEventStore } from '../../../../service/workflow/workflow.event.store';
+import { WorkflowService } from '../../../../service/workflow/workflow.service';
+import { WorkflowStore } from '../../../../service/workflow/workflow.store';
+import { AutoUnsubscribe } from '../../../../shared/decorator/autoUnsubscribe';
+import { ToastService } from '../../../../shared/toast/ToastService';
 
 @Component({
     selector: 'app-workflow-sidebar-code',
@@ -52,7 +54,10 @@ export class WorkflowSidebarCodeComponent {
     permissionEnum = PermissionValue;
 
     constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _router: Router,
         private _workflowCore: WorkflowCoreService,
+        private _workflowEventStore: WorkflowEventStore,
         private _workflowService: WorkflowService,
         private _workflowStore: WorkflowStore,
         private _toast: ToastService,
@@ -86,7 +91,17 @@ export class WorkflowSidebarCodeComponent {
         this._workflowCore.toggleAsCodeEditor({open: false, save: false});
     }
 
+    unselectAll() {
+        this._workflowEventStore.unselectAll();
+        let url = this._router.createUrlTree(['./'], {
+            relativeTo: this._activatedRoute,
+            queryParams: {}
+        });
+        this._router.navigateByUrl(url.toString());
+    }
+
     preview() {
+        this.unselectAll();
         this.loading = true;
         this._workflowService.previewWorkflowImport(this.project.key, this.exportedWf)
             .pipe(finalize(() => this.loading = false))
@@ -94,6 +109,7 @@ export class WorkflowSidebarCodeComponent {
     }
 
     save() {
+        this.unselectAll();
         this.loading = true;
         this._workflowStore.importWorkflow(this.project.key, this.workflow.name, this.exportedWf)
             .pipe(finalize(() => this.loading = false))
