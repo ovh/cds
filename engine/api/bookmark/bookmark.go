12775b230d5be427d70fa00073f7bbf7d717b347
Coenen Benjamin <benjamin.coenen@hotmail.com>
2018-12-31 11:06:18 +0100

    fix(api): order bookmarks alphabetically (#3783) (#3789)
    Signed-off-by: Benjamin Coenen <benjamin.coenen@corp.ovh.com>

--- engine/api/bookmark/bookmark.go
+++ engine/api/bookmark/bookmark.go
@@ -11,27 +11,28 @@ import (
 // LoadAll returns all bookmarks with icons and their description
 func LoadAll(db gorp.SqlExecutor, u *sdk.User) ([]sdk.Bookmark, error) {
 	var data []sdk.Bookmark
-	query := `
-	(
-		SELECT DISTINCT
-			project.projectkey AS key, project.name AS project_name, project.description, '' AS workflow_name, project.description, project.icon,
-			true AS favorite,
-			'project' AS type
-		FROM project
-		JOIN project_favorite ON project.id = project_favorite.project_id AND project_favorite.user_id = $1
-		ORDER BY project.name
-	)
-	UNION
-	(
-		SELECT DISTINCT
-			project.projectkey AS key, project.name AS project_name, workflow.description, workflow.name AS workflow_name, workflow.description, workflow.icon,
-			true AS favorite,
-			'workflow' AS type
-		FROM project
-		JOIN workflow ON workflow.project_id = project.id
-		JOIN workflow_favorite ON workflow.id = workflow_favorite.workflow_id AND workflow_favorite.user_id = $1
-		ORDER BY project.name
-	)
+	query := `SELECT * FROM (
+		(
+			SELECT DISTINCT
+				project.projectkey AS key, project.name AS project_name, project.description, '' AS workflow_name, project.description, project.icon,
+				true AS favorite,
+				'project' AS type
+			FROM project
+			JOIN project_favorite ON project.id = project_favorite.project_id AND project_favorite.user_id = $1
+			ORDER BY project.name
+		)
+		UNION
+		(
+			SELECT DISTINCT
+				project.projectkey AS key, project.name AS project_name, workflow.description, workflow.name AS workflow_name, workflow.description, workflow.icon,
+				true AS favorite,
+				'workflow' AS type
+			FROM project
+			JOIN workflow ON workflow.project_id = project.id
+			JOIN workflow_favorite ON workflow.id = workflow_favorite.workflow_id AND workflow_favorite.user_id = $1
+			ORDER BY workflow_name
+		)
+	) AS sub ORDER BY sub.workflow_name
 	`
 	if u == nil {
 		u = &sdk.User{}
