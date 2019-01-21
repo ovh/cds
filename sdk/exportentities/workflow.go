123fa49458756ae114d63f06619f6182cc794047
richardlt <richard.le.terrier@gmail.com>
2019-01-21 18:09:23 +0100

    feat(api): allow to return a pulled workflow as json

--- sdk/exportentities/workflow.go
+++ sdk/exportentities/workflow.go
@@ -1,14 +1,27 @@
 package exportentities
 
 import (
+	"archive/tar"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"math/rand"
 	"sort"
 	"strings"
 	"time"
 
 	"github.com/fsamin/go-dump"
+
 	"github.com/ovh/cds/sdk"
+	"github.com/ovh/cds/sdk/log"
+)
+
+// Name pattern for pull files.
+const (
+	PullWorkflowName    = "%s.yml"
+	PullPipelineName    = "%s.pip.yml"
+	PullApplicationName = "%s.app.yml"
+	PullEnvironmentName = "%s.env.yml"
 )
 
 // Workflow is the "as code" representation of a sdk.Workflow
@@ -37,6 +50,98 @@ type Workflow struct {
 	HistoryLength       *int64                         `json:"history_length,omitempty" yaml:"history_length,omitempty"`
 	Notifications       []NotificationEntry            `json:"notify,omitempty" yaml:"notify,omitempty"`               // This is used when the workflow have only one pipeline
 	MapNotifications    map[string][]NotificationEntry `json:"notifications,omitempty" yaml:"notifications,omitempty"` // This is used when the workflow have more than one pipeline
+}
+
+// WorkflowPulled contains all the yaml base64 that are needed to generate a workflow tar file.
+type WorkflowPulled struct {
+	Workflow     WorkflowPulledItem   `json:"workflow"`
+	Pipelines    []WorkflowPulledItem `json:"pipelines"`
+	Applications []WorkflowPulledItem `json:"applications"`
+	Environments []WorkflowPulledItem `json:"environments"`
+}
+
+// WorkflowPulledItem contains data for a workflow item.
+type WorkflowPulledItem struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// Tar returns a tar containing all files for a pulled workflow.
+func (w WorkflowPulled) Tar(writer io.Writer) error {
+	tw := tar.NewWriter(writer)
+	defer func() {
+		if err := tw.Close(); err != nil {
+			log.Error("%v", sdk.WrapError(err, "unable to close tar writer"))
+		}
+	}()
+
+	bs, err := base64.StdEncoding.DecodeString(w.Workflow.Value)
+	if err != nil {
+		return sdk.WithStack(err)
+	}
+	if err := tw.WriteHeader(&tar.Header{
+		Name: fmt.Sprintf(PullWorkflowName, w.Workflow.Name),
+		Mode: 0644,
+		Size: int64(len(bs)),
+	}); err != nil {
+		return sdk.WrapError(err, "unable to write workflow header for %s", w.Workflow.Name)
+	}
+	if _, err := tw.Write(bs); err != nil {
+		return sdk.WrapError(err, "unable to write workflow value")
+	}
+
+	for _, a := range w.Applications {
+		bs, err := base64.StdEncoding.DecodeString(a.Value)
+		if err != nil {
+			return sdk.WithStack(err)
+		}
+		if err := tw.WriteHeader(&tar.Header{
+			Name: fmt.Sprintf(PullApplicationName, a.Name),
+			Mode: 0644,
+			Size: int64(len(bs)),
+		}); err != nil {
+			return sdk.WrapError(err, "unable to write application header for %s", a.Name)
+		}
+		if _, err := tw.Write(bs); err != nil {
+			return sdk.WrapError(err, "unable to write application value")
+		}
+	}
+
+	for _, e := range w.Environments {
+		bs, err := base64.StdEncoding.DecodeString(e.Value)
+		if err != nil {
+			return sdk.WithStack(err)
+		}
+		if err := tw.WriteHeader(&tar.Header{
+			Name: fmt.Sprintf(PullEnvironmentName, e.Name),
+			Mode: 0644,
+			Size: int64(len(bs)),
+		}); err != nil {
+			return sdk.WrapError(err, "unable to write env header for %s", e.Name)
+		}
+		if _, err := tw.Write(bs); err != nil {
+			return sdk.WrapError(err, "unable to copy env buffer")
+		}
+	}
+
+	for _, p := range w.Pipelines {
+		bs, err := base64.StdEncoding.DecodeString(p.Value)
+		if err != nil {
+			return sdk.WithStack(err)
+		}
+		if err := tw.WriteHeader(&tar.Header{
+			Name: fmt.Sprintf(PullPipelineName, p.Name),
+			Mode: 0644,
+			Size: int64(len(bs)),
+		}); err != nil {
+			return sdk.WrapError(err, "unable to write pipeline header for %s", p.Name)
+		}
+		if _, err := tw.Write(bs); err != nil {
+			return sdk.WrapError(err, "unable to write pipeline value")
+		}
+	}
+
+	return nil
 }
 
 // NodeEntry represents a node as code
