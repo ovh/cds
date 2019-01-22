a436619c106e1c22bd0c2773747daa206657caf1
François Samin <francois.samin@corp.ovh.com>
2019-01-22 15:25:48 +0100

    fix(api): decrypt vcs strategies while doing update as-code
    closes #3876

--- engine/api/workflow/as_code.go
+++ engine/api/workflow/as_code.go
@@ -47,8 +47,19 @@ func UpdateAsCode(ctx context.Context, db *gorp.DbMap, store cache.Store, proj *
 		return nil, sdk.WrapError(err, "cannot pull workflow")
 	}
 
-	if err := application.DecryptVCSStrategyPassword(&app); err != nil {
-		return nil, sdk.WrapError(err, "unable to decrypt vcs strategy")
+	var vcsStrategy = app.RepositoryStrategy
+
+	if vcsStrategy.SSHKey != "" {
+		key := proj.GetSSHKey(vcsStrategy.SSHKey)
+		if key == nil {
+			return nil, fmt.Errorf("unable to find key %s on project %s", vcsStrategy.SSHKey, proj.Key)
+		}
+		vcsStrategy.SSHKeyContent = key.Private
+	} else {
+		if err := application.DecryptVCSStrategyPassword(&app); err != nil {
+			return nil, sdk.WrapError(err, "unable to decrypt vcs strategy")
+		}
+		vcsStrategy = app.RepositoryStrategy
 	}
 
 	// Create VCS Operation
@@ -56,7 +67,7 @@ func UpdateAsCode(ctx context.Context, db *gorp.DbMap, store cache.Store, proj *
 		VCSServer:          app.VCSServer,
 		RepoFullName:       app.RepositoryFullname,
 		URL:                "",
-		RepositoryStrategy: app.RepositoryStrategy,
+		RepositoryStrategy: vcsStrategy,
 		Setup: sdk.OperationSetup{
 			Push: sdk.OperationPush{
 				FromBranch: fmt.Sprintf("cdsAsCode-%d", time.Now().Unix()),
