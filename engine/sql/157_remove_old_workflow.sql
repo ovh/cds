185e3cd458932578832d8e862f49f13fc10c44a7
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2019-01-28 18:29:35 +0100

    add drop table plugin
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/sql/157_remove_old_workflow.sql
+++ engine/sql/157_remove_old_workflow.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS build_log;
 DROP TABLE IF EXISTS artifact;
 DROP TABLE IF EXISTS pipeline_build_log;
 DROP TABLE IF EXISTS pipeline_build_test;
+DROP TABLE IF EXISTS plugin;
 
 -- +migrate Down
 SELECT 1;
