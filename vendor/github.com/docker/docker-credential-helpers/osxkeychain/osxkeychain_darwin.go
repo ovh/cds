21149ff3310ce804d4dd2eb8461d5c3375c29281
François Samin <francois.samin+github@gmail.com>
2019-03-26 15:06:37 +0100

    fix(cli): cdsctl cross compilation (#4100)

--- vendor/github.com/docker/docker-credential-helpers/osxkeychain/osxkeychain_darwin.go
+++ vendor/github.com/docker/docker-credential-helpers/osxkeychain/osxkeychain_darwin.go
@@ -1,8 +1,8 @@
 package osxkeychain
 
 /*
-#cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Security -framework Foundation
+#cgo CFLAGS: -x objective-c -mmacosx-version-min=10.10
+#cgo LDFLAGS: -framework Security -framework Foundation -mmacosx-version-min=10.10
 
 #include "osxkeychain_darwin.h"
 #include <stdlib.h>
