03af9fc1612c23bde9f55ba2bbb3a5f6608ff483
richardlt <richard.le.terrier@gmail.com>
2019-05-03 18:19:28 +0200

    wip

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (true) {
+    document.body.className += ' ' + 'night';
+}
+
 if (environment.production) {
     enableProdMode();
 }
