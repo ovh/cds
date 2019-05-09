bd9ecc7a76d47e621a379b9e0f3763337d2edd26
richardlt <richard.le.terrier@gmail.com>
2019-05-09 13:54:22 +0200

    feat: detect theme before loading angular

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (true) {
-    document.body.className += ' ' + 'night';
-}
-
 if (environment.production) {
     enableProdMode();
 }
