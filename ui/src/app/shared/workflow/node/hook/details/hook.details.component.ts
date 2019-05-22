1d1618b3a763cc20318d862758304d3d31ed5d2b
richardlt <richard.le.terrier@gmail.com>
2019-05-22 17:57:23 +0200

    fix(ui): read-only input style, some fixes

--- ui/src/app/shared/workflow/node/hook/details/hook.details.component.ts
+++ ui/src/app/shared/workflow/node/hook/details/hook.details.component.ts
@@ -1,48 +1,63 @@
-import {Component, ViewChild} from '@angular/core';
-import {cloneDeep} from 'lodash';
-import {ModalTemplate, SuiModalService, TemplateModalConfig} from 'ng2-semantic-ui';
-import {ActiveModal} from 'ng2-semantic-ui/dist';
-import {TaskExecution} from '../../../../../model/workflow.hook.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TaskExecution } from 'app/model/workflow.hook.model';
+import { ThemeStore } from 'app/service/services.module';
+import { AutoUnsubscribe } from 'app/shared/decorator/autoUnsubscribe';
+import { cloneDeep } from 'lodash';
+import { ModalTemplate, SuiModalService, TemplateModalConfig } from 'ng2-semantic-ui';
+import { ActiveModal } from 'ng2-semantic-ui/dist';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-workflow-node-hook-details',
-    templateUrl: './hook.details.component.html',
-    styleUrls: ['./hook.details.component.scss']
+  selector: 'app-workflow-node-hook-details',
+  templateUrl: './hook.details.component.html',
+  styleUrls: ['./hook.details.component.scss']
 })
-export class WorkflowNodeHookDetailsComponent {
-    codeMirrorConfig: any;
+@AutoUnsubscribe()
+export class WorkflowNodeHookDetailsComponent implements OnInit {
+  @ViewChild('code') codemirror: any;
 
-    // Ng semantic modal
-    @ViewChild('nodeHookDetailsModal')
-    public nodeHookDetailsModal: ModalTemplate<boolean, boolean, void>;
-    modal: ActiveModal<boolean, boolean, void>;
-    modalConfig: TemplateModalConfig<boolean, boolean, void>;
+  @ViewChild('nodeHookDetailsModal') nodeHookDetailsModal: ModalTemplate<boolean, boolean, void>;
+  modal: ActiveModal<boolean, boolean, void>;
+  modalConfig: TemplateModalConfig<boolean, boolean, void>;
 
-    task: TaskExecution;
+  task: TaskExecution;
+  codeMirrorConfig: any;
+  themeSubscription: Subscription;
 
-    constructor(private _modalService: SuiModalService) {
-        this.codeMirrorConfig = {
-            matchBrackets: true,
-            autoCloseBrackets: true,
-            mode: 'application/json',
-            lineWrapping: true,
-            autoRefresh: true,
-            readOnly: true
-        };
+  constructor(
+    private _modalService: SuiModalService,
+    private _theme: ThemeStore
+  ) {
+    this.codeMirrorConfig = {
+      matchBrackets: true,
+      autoCloseBrackets: true,
+      mode: 'application/json',
+      lineWrapping: true,
+      autoRefresh: true,
+      readOnly: true
+    };
+  }
+
+  ngOnInit(): void {
+    this.themeSubscription = this._theme.get().subscribe(t => {
+      this.codeMirrorConfig.theme = t === 'night' ? 'darcula' : 'default';
+      if (this.codemirror && this.codemirror.instance) {
+          this.codemirror.instance.setOption('theme', this.codeMirrorConfig.theme);
+      }
+  });
+  }
+
+  show(taskExec: TaskExecution): void {
+    this.task = cloneDeep(taskExec);
+    if (this.task.webhook && this.task.webhook.request_body) {
+      let body = atob(this.task.webhook.request_body);
+      try {
+        this.task.webhook.request_body = JSON.stringify(JSON.parse(body), null, 4);
+      } catch (e) {
+        this.task.webhook.request_body = body;
+      }
     }
-
-    show(taskExec: TaskExecution): void {
-        this.task = cloneDeep(taskExec);
-        if (this.task.webhook && this.task.webhook.request_body) {
-          let body = atob(this.task.webhook.request_body);
-          try {
-            this.task.webhook.request_body = JSON.stringify(JSON.parse(body), null, 4);
-          } catch (e) {
-            this.task.webhook.request_body = body;
-          }
-
-        }
-        this.modalConfig = new TemplateModalConfig<boolean, boolean, void>(this.nodeHookDetailsModal);
-        this.modal = this._modalService.open(this.modalConfig);
-    }
+    this.modalConfig = new TemplateModalConfig<boolean, boolean, void>(this.nodeHookDetailsModal);
+    this.modal = this._modalService.open(this.modalConfig);
+  }
 }
