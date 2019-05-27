3d015a5ac45b93f929d22b71e581a5ba4be64e07
richardlt <richard.le.terrier@gmail.com>
2019-05-27 10:49:27 +0200

    doc: add example for coverage action

--- sdk/action/coverage.go
+++ sdk/action/coverage.go
@@ -1,13 +1,19 @@
 package action
 
-import "github.com/ovh/cds/sdk"
+import (
+	"github.com/ovh/cds/sdk"
+	"github.com/ovh/cds/sdk/exportentities"
+)
 
 // Coverage action definition.
 var Coverage = Manifest{
 	Action: sdk.Action{
 		Name: sdk.CoverageAction,
 		Description: `CDS Builtin Action.
-Parse given file to extract coverage results.`,
+Parse given file to extract coverage results.
+
+Coverage report will be linked to the application from the pipeline context.
+You will be able to see the coverage history in the application home page.`,
 		Parameters: []sdk.Parameter{
 			{
 				Name:        "format",
@@ -27,5 +33,23 @@ Parse given file to extract coverage results.`,
 				Advanced:    true,
 			},
 		},
+	},
+	Example: exportentities.PipelineV1{
+		Version: exportentities.PipelineVersion1,
+		Name:    "Pipeline1",
+		Stages:  []string{"Stage1"},
+		Jobs: []exportentities.Job{{
+			Name:  "Job1",
+			Stage: "Stage1",
+			Steps: []exportentities.Step{
+				{
+					Coverage: &exportentities.StepCoverage{
+						Format:  "cobertura",
+						Minimum: "./coverage.xml",
+						Path:    "60",
+					},
+				},
+			},
+		}},
 	},
 }
