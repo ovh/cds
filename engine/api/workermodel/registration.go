dc4769a43859b93ba978632add9de3e802e8163e
Yvonnick Esnault <yvonnick@esnau.lt>
2019-06-07 22:43:13 +0200

    fix(api): 0x00/0xbf in spawnError
    Theses two sequences can happen.
    
    This fix allows to have more info about spawn register, as:
    
    ```
    l2019-06-07T20:39:09.266904200Z  fetch http://dl-cdn.alpinelinux.org/alpine/v3.9/main/x86_64/APKINDEX.tar.gz
    q2019-06-07T20:39:09.418793300Z  fetch http://dl-cdn.alpinelinux.org/alpine/v3.9/community/x86_64/APKINDEX.tar.gz
    J2019-06-07T20:39:09.510410800Z  (1/4) Installing nghttp2-libs (1.35.1-r0)
    D2019-06-07T20:39:09.523838000Z  (2/4) Installing libssh2 (1.8.2-r0)
    E2019-06-07T20:39:09.538660200Z  (3/4) Installing libcurl (7.64.0-r2)
    B2019-06-07T20:39:09.568090800Z  (4/4) Installing curl (7.64.0-r2)
    E2019-06-07T20:39:09.594085800Z  Executing busybox-1.29.3-r10.trigger
    92019-06-07T20:39:09.609865100Z  OK: 7 MiB in 19 packages
    p2019-06-07T20:39:09.633641200Z    % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
    n2019-06-07T20:39:09.635767500Z                                   Dload  Upload   Total   Spent    Left  Speed
    2019-06-07T20:39:09.643114000Z
      0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
    100    19  100    19    0     0   2111      0 --:--:-- --:--:-- --:--:--  2111
    A2019-06-07T20:39:09.645015700Z  ./worker: line 1: 404: not found
    ```
    
    Signed-off-by: Yvonnick Esnault <yvonnick@esnau.lt>

--- engine/api/workermodel/registration.go
+++ engine/api/workermodel/registration.go
@@ -1,8 +1,10 @@
 package workermodel
 
 import (
+	"bytes"
 	"errors"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-gorp/gorp"
@@ -79,9 +81,10 @@ func updateAllToCheckRegistration(db gorp.SqlExecutor) error {
 // UpdateSpawnErrorWorkerModel updates worker model error registration
 func UpdateSpawnErrorWorkerModel(db gorp.SqlExecutor, modelID int64, spawnError sdk.SpawnErrorForm) error {
 	// some times when the docker container fails to start, the docker logs is not empty but only contains utf8 null char
-	if spawnError.Error == string([]byte{0x00}) {
-		spawnError.Error = ""
-	}
+	spawnError.Error = strings.ReplaceAll(spawnError.Error, string([]byte{0x00}), "")
+	spawnError.Error = strings.ReplaceAll(spawnError.Error, string([]byte{0xbf}), "")
+	spawnError.Logs = bytes.ReplaceAll(spawnError.Logs, []byte{0x00}, []byte(""))
+	spawnError.Logs = bytes.ReplaceAll(spawnError.Logs, []byte{0xbf}, []byte(""))
 
 	query := `UPDATE worker_model SET nb_spawn_err=nb_spawn_err+1, last_spawn_err=$3, last_spawn_err_log=$4, date_last_spawn_err=$2 WHERE id = $1`
 	res, err := db.Exec(query, modelID, time.Now(), spawnError.Error, spawnError.Logs)
