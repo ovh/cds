dc438bfdcc9c544a76b22c7aee4b1e030f987c97
Guiheux Steven <steven.guiheux+github@gmail.com>
2019-06-27 17:48:00 +0200

    fix(ui): css in sidebar and logs (#4398)

--- engine/api/workflow/dao.go
+++ engine/api/workflow/dao.go
@@ -524,13 +524,14 @@ func load(ctx context.Context, db gorp.SqlExecutor, store cache.Store, proj *sdk
 	dbRes := Workflow{}
 
 	_, next := observability.Span(ctx, "workflow.load.selectOne")
-	if err := db.SelectOne(&dbRes, query, args...); err != nil {
+	err := db.SelectOne(&dbRes, query, args...)
+	next()
+	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, sdk.ErrWorkflowNotFound
 		}
 		return nil, sdk.WrapError(err, "Unable to load workflow")
 	}
-	next()
 
 	res := sdk.Workflow(dbRes)
 	if proj.Key == "" {
@@ -546,11 +547,11 @@ func load(ctx context.Context, db gorp.SqlExecutor, store cache.Store, proj *sdk
 	// Load groups
 	_, next = observability.Span(ctx, "workflow.load.loadWorkflowGroups")
 	gps, err := group.LoadWorkflowGroups(db, res.ID)
+	next()
 	if err != nil {
 		return nil, sdk.WrapError(err, "Unable to load workflow groups")
 	}
 	res.Groups = gps
-	next()
 
 	res.Pipelines = map[int64]sdk.Pipeline{}
 	res.Applications = map[int64]sdk.Application{}
