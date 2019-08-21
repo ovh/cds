55cc3adada7cd793db2ce8b6cc7bab9ba0d3e39c
Yvonnick Esnault <yvonnick@esnau.lt>
2019-08-21 23:34:17 +0200

    cr
    Signed-off-by: Yvonnick Esnault <yvonnick@esnau.lt>

--- engine/ui/types.go
+++ engine/ui/types.go
@@ -15,7 +15,7 @@ type Service struct {
 // Configuration is the hooks configuration structure
 type Configuration struct {
 	Name      string `toml:"name" comment:"Name of this CDS UI Service\n Enter a name to enable this service" json:"name"`
-	Staticdir string `toml:"staticdir" comment:"This directory must contains index.html file and other ui files (css, js...) from ui.tar.gz artifact." json:"staticdir"`
+	Staticdir string `toml:"staticdir" default:"./" comment:"This directory must contains index.html file and other ui files (css, js...) from ui.tar.gz artifact." json:"staticdir"`
 	HTTP      struct {
 		Addr string `toml:"addr" default:"" commented:"true" comment:"Listen address without port, example: 127.0.0.1" json:"addr"`
 		Port int    `toml:"port" default:"8080" json:"port"`
