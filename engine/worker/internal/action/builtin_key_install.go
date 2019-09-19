874a2dae23512457307996e81649ffd960a1da7d
François Samin <francois.samin@corp.ovh.com>
2019-09-19 14:45:29 +0200

    lint

--- engine/worker/internal/action/builtin_key_install.go
+++ engine/worker/internal/action/builtin_key_install.go
@@ -7,11 +7,11 @@ import (
 	"runtime"
 	"strings"
 
+	"github.com/spf13/afero"
+
 	"github.com/ovh/cds/engine/worker/pkg/workerruntime"
 	"github.com/ovh/cds/sdk"
 	"github.com/ovh/cds/sdk/log"
-
-	"github.com/spf13/afero"
 )
 
 func RunInstallKey(ctx context.Context, wk workerruntime.Runtime, a sdk.Action, params []sdk.Parameter, secrets []sdk.Variable) (sdk.Result, error) {
