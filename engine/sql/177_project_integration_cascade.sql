de56ec4cc8e664156bc618c8209938b9428d0f58
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2019-09-20 23:08:51 +0200

    old table
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/sql/177_project_integration_cascade.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-
-ALTER TABLE workflow_node_context DROP CONSTRAINT "fk_workflow_node_project_integration";
-SELECT create_foreign_key_idx_cascade('FK_WORKFLOW_NODE_PROJECT_INTEGRATION', 'workflow_node_context', 'project_integration', 'project_integration_id', 'id');
-
--- +migrate Down
-SELECT 1;
