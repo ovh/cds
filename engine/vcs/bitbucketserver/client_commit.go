5cb087a60ba0c3e1fa7dac6f442f1144c92fb118
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2019-09-26 16:26:14 +0200

    fix(vcs/bitbucketserver): slug & display available on bitbucket response (#4624)

--- engine/vcs/bitbucketserver/client_commit.go
+++ engine/vcs/bitbucketserver/client_commit.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/url"
-	"strings"
 
 	"github.com/ovh/cds/engine/api/cache"
 	"github.com/ovh/cds/sdk"
-	"github.com/ovh/cds/sdk/log"
 )
 
 func (b *bitbucketClient) Commits(ctx context.Context, repo, branch, since, until string) ([]sdk.VCSCommit, error) {
@@ -60,43 +58,18 @@ func (b *bitbucketClient) Commits(ctx context.Context, repo, branch, since, unti
 			Timestamp: sc.Timestamp,
 			Message:   sc.Message,
 			Author: sdk.VCSAuthor{
-				Name:  sc.Author.Name,
-				Email: sc.Author.Email,
+				Name:        sc.Author.Name,
+				Email:       sc.Author.Email,
+				DisplayName: sc.Author.DisplayName,
 			},
 			URL: urlCommit + sc.Hash,
 		}
-		stashUser := b.findUser(ctx, sc.Author.Email)
-		if stashUser == nil {
-			newStashUserUnknown := newUnknownStashUser(*sc.Author)
-			var stashUserKey = cache.Key("vcs", "bitbucket", b.consumer.URL, sc.Author.Email)
-			b.consumer.cache.SetWithTTL(stashUserKey, newStashUserUnknown, 86400) // 1 day
-			stashUser = newUnknownStashUser(*sc.Author)
-		}
-
-		c.Author.DisplayName = stashUser.DisplayName
-		if stashUser.Slug != "" && stashUser.Slug != "unknownSlug" {
-			c.Author.Avatar = fmt.Sprintf("%s/users/%s/avatar.png", b.consumer.URL, stashUser.Slug)
+		if sc.Author.Slug != "" && sc.Author.Slug != "unknownSlug" {
+			c.Author.Avatar = fmt.Sprintf("%s/users/%s/avatar.png", b.consumer.URL, sc.Author.Slug)
 		}
 		commits = append(commits, c)
 	}
 	return commits, nil
-}
-
-func (b *bitbucketClient) findUser(ctx context.Context, email string) *User {
-	var stashUser = &User{}
-	var stashUserKey = cache.Key("reposmanager", "stash", b.consumer.URL, email)
-	if !b.consumer.cache.Get(stashUserKey, &stashUser) && email != "" {
-		newStashUser, err := b.findByEmail(ctx, email)
-		if err != nil {
-			if !strings.Contains(err.Error(), "User not found") {
-				log.Warning("Unable to get stash user %s : %s", email, err)
-			}
-			return nil
-		}
-		b.consumer.cache.Set(stashUserKey, newStashUser)
-		stashUser = newStashUser
-	}
-	return stashUser
 }
 
 func (b *bitbucketClient) Commit(ctx context.Context, repo, hash string) (sdk.VCSCommit, error) {
@@ -119,33 +92,16 @@ func (b *bitbucketClient) Commit(ctx context.Context, repo, hash string) (sdk.VC
 		Timestamp: sc.Timestamp,
 		Message:   sc.Message,
 		Author: sdk.VCSAuthor{
-			Name:  sc.Author.Name,
-			Email: sc.Author.Email,
+			Name:        sc.Author.Name,
+			Email:       sc.Author.Email,
+			DisplayName: sc.Author.DisplayName,
 		},
 		URL: urlCommit,
 	}
-
-	stashUser := b.findUser(ctx, sc.Author.Email)
-	if stashUser == nil {
-		newStashUserUnknown := newUnknownStashUser(*sc.Author)
-		var stashUserKey = cache.Key("vcs", "bitbucket", b.consumer.URL, sc.Author.Email)
-		b.consumer.cache.SetWithTTL(stashUserKey, newStashUserUnknown, 86400) // 1 day
-		stashUser = newUnknownStashUser(*sc.Author)
-	}
-	commit.Author.DisplayName = stashUser.DisplayName
-	if stashUser.Slug != "" && stashUser.Slug != "unknownSlug" {
-		commit.Author.Avatar = fmt.Sprintf("%s/users/%s/avatar.png", b.consumer.URL, stashUser.Slug)
+	if sc.Author.Slug != "" && sc.Author.Slug != "unknownSlug" {
+		commit.Author.Avatar = fmt.Sprintf("%s/users/%s/avatar.png", b.consumer.URL, sc.Author.Slug)
 	}
 	return commit, nil
-}
-
-func newUnknownStashUser(author Author) *User {
-	return &User{
-		Username:     author.Name,
-		EmailAddress: author.Email,
-		DisplayName:  author.Name,
-		Slug:         "unknownSlug",
-	}
 }
 
 func (b *bitbucketClient) CommitsBetweenRefs(ctx context.Context, repo, base, head string) ([]sdk.VCSCommit, error) {
@@ -197,22 +153,15 @@ func (b *bitbucketClient) CommitsBetweenRefs(ctx context.Context, repo, base, he
 			Timestamp: sc.Timestamp,
 			Message:   sc.Message,
 			Author: sdk.VCSAuthor{
-				Name:  sc.Author.Name,
-				Email: sc.Author.Email,
+				Name:        sc.Author.Name,
+				Email:       sc.Author.Email,
+				DisplayName: sc.Author.DisplayName,
 			},
 			URL: urlCommit + sc.Hash,
 		}
-		stashUser := b.findUser(ctx, sc.Author.Email)
-		if stashUser == nil {
-			newStashUserUnknown := newUnknownStashUser(*sc.Author)
-			var stashUserKey = cache.Key("vcs", "bitbucket", b.consumer.URL, sc.Author.Email)
-			b.consumer.cache.SetWithTTL(stashUserKey, newStashUserUnknown, 86400) // 1 day
-			stashUser = newUnknownStashUser(*sc.Author)
-		}
 
-		c.Author.DisplayName = stashUser.DisplayName
-		if stashUser.Slug != "" && stashUser.Slug != "unknownSlug" {
-			c.Author.Avatar = fmt.Sprintf("%s/users/%s/avatar.png", b.consumer.URL, stashUser.Slug)
+		if sc.Author.Slug != "" && sc.Author.Slug != "unknownSlug" {
+			c.Author.Avatar = fmt.Sprintf("%s/users/%s/avatar.png", b.consumer.URL, sc.Author.Slug)
 		}
 		commits = append(commits, c)
 	}
