c390f947633836eb8ff7e8db17ccb9c37ebe4e55
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2019-10-03 16:04:31 +0200

    fix(ui): refresh junit results (#4647)
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- ui/src/app/views/workflow/run/node/test/tests.component.ts
+++ ui/src/app/views/workflow/run/node/test/tests.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { Coverage } from '../../../../../model/coverage.model';
 import { Tests } from '../../../../../model/pipeline.model';
 
@@ -13,6 +13,23 @@ export class WorkflowRunTestsResultComponent {
     @Input() tests: Tests;
 
     _coverage: Coverage;
+    percentLines: number;
+    currentBranchTrendLines: number;
+    defaultBranchTrendLines: number;
+    percentFunctions: number;
+    currentBranchTrendFunctions: number;
+    defaultBranchTrendFunctions: number;
+    percentBranches: number;
+    currentBranchTrendBranches: number;
+    defaultBranchTrendBranches: number;
+
+    filter = 'error';
+
+    constructor(
+        private _cd: ChangeDetectorRef
+    ) {
+    }
+
     @Input('coverage')
     set coverage(data: Coverage) {
         if (data && data.workflow_id) {
@@ -62,22 +79,9 @@ export class WorkflowRunTestsResultComponent {
                 }
             }
         }
+        this._cd.detectChanges();
     };
     get coverage(): Coverage {
         return this._coverage;
     }
-    percentLines: number;
-    currentBranchTrendLines: number;
-    defaultBranchTrendLines: number;
-    percentFunctions: number;
-    currentBranchTrendFunctions: number;
-    defaultBranchTrendFunctions: number;
-    percentBranches: number;
-    currentBranchTrendBranches: number;
-    defaultBranchTrendBranches: number;
-
-
-    filter = 'error';
-
-    constructor() { }
 }
