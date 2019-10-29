048810ce093e0add0e5afbb3a193b015b580afc6
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2019-10-29 09:10:01 +0100

    docs: worker on windows
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/worker/cmd_main.go
+++ engine/worker/cmd_main.go
@@ -8,8 +8,13 @@ func cmdMain(w *currentWorker) *cobra.Command {
 	var mainCmd = &cobra.Command{
 		Use:   "worker",
 		Short: "CDS Worker",
-		Long:  "A pipeline is structured in sequential stages containing one or multiple concurrent jobs. A Job will be executed by a worker.",
-		Run:   runCmd(w),
+		Long: `A pipeline is structured in sequential stages containing
+one or multiple concurrent jobs. A Job will be executed by a worker.
+
+The worker provides some useful commands that can be used in a step, as ` + "`worker upload...`" + `, ` + "`worker download...`" + "`worker cache...`" + `
+
+On Windows OS, theses commands can be accessed with ` + "`worker.exe [cmd]` syntax.",
+		Run: runCmd(w),
 	}
 
 	initFlagsRun(mainCmd)
