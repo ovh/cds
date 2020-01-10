654e428279b42e1a3438e9ca604ccb5014ac2fa8
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-01-10 09:47:08 +0100

    chore(docker-compose): make test-docker-compose (#4876)
    * chore(docker-compose): make test-docker-compose
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- ui/e2e/config/index.ts
+++ ui/e2e/config/index.ts
@@ -1,6 +1,6 @@
 export default {
     environment: process.env.NODE_ENV || 'development',
-    baseUrl: process.env.BASE_URL ||  'http://localhost:4200',
+    baseUrl: process.env.BASE_URL ||  'http://localhost:8080',
     username: process.env.CDS_USERNAME,
     password: process.env.CDS_PASSWORD
 }
