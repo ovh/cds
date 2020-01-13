271333b7a80143e9eaa472f515a6be6468eadf5d
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-01-13 18:08:59 +0100

    fix: cr
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- cli/cdsctl/version.go
+++ cli/cdsctl/version.go
@@ -30,31 +30,53 @@ func version() *cobra.Command {
 
 func versionRun(v cli.Values) error {
 	format := v.GetString("format")
-	if format == "" {
-		fmt.Println(sdk.VersionString())
-		fmt.Printf("keychain support: %t\n", internal.IsKeychainEnabled())
-		return nil
+	type versionCDSCTL struct {
+		Version      string `json:"version" yaml:"version"`
+		Architecture string `json:"architecture" yaml:"architecture"`
+		OS           string `json:"os" yaml:"os"`
+		GitHash      string `json:"git_hash" yaml:"git_hash"`
+		BuildTime    string `json:"build_time" yaml:"build_time"`
+		Keychain     bool   `json:"keychain"`
 	}
 
-	type versionWithKeychain struct {
-		sdk.Version
-		Keychain bool `json:"keychain"`
+	vers := sdk.VersionCurrent()
+	cdsctlVers := versionCDSCTL{
+		Version:      vers.Version,
+		Architecture: vers.Architecture,
+		OS:           vers.OS,
+		GitHash:      vers.GitHash,
+		BuildTime:    vers.BuildTime,
+		Keychain:     internal.IsKeychainEnabled(),
 	}
 
-	version := versionWithKeychain{
-		Version:  sdk.VersionCurrent(),
-		Keychain: internal.IsKeychainEnabled(),
+	var versionAPI *sdk.Version
+	if cfg.Host != "" {
+		var err error
+		versionAPI, err = client.Version()
+		if err != nil {
+			return fmt.Errorf("error while getting API version: %v", err)
+		}
+	}
+
+	type allVersion struct {
+		CDSCTLVersion versionCDSCTL `json:"cdsctl,omitempty" yaml:"cdsctl,omitempty"`
+		APIVersion    *sdk.Version  `json:"api,omitempty" yaml:"api,omitempty"`
+	}
+
+	versions := allVersion{
+		CDSCTLVersion: cdsctlVers,
+		APIVersion:    versionAPI,
 	}
 
 	var buf []byte
 	var err error
 	switch format {
 	case "json":
-		buf, err = json.Marshal(version)
+		buf, err = json.Marshal(versions)
 	case "yaml":
-		buf, err = yaml.Marshal(version)
+		buf, err = yaml.Marshal(versions)
 	default:
-		return fmt.Errorf("invalid given format")
+		buf, err = yaml.Marshal(versions)
 	}
 	if err != nil {
 		return err
