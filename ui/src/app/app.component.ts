8a823911992d4da0f61a93bd037a7029cd42abad
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-01-23 14:33:47 +0100

    fix(ui): display refresh banner on new ui deployed
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -41,7 +41,6 @@ export class AppComponent implements OnInit {
     sseWorker: CDSWorker;
     heartbeatToken: number;
     zone: NgZone;
-    currentVersion = 0;
     showUIUpdatedBanner: boolean;
     languageSubscriber: Subscription;
     themeSubscriber: Subscription;
@@ -262,11 +261,7 @@ export class AppComponent implements OnInit {
         this.versionWorker.response().subscribe(msg => {
             if (msg) {
                 this.zone.run(() => {
-                    let versionJSON = Number(JSON.parse(msg).version);
-                    if (this.currentVersion === 0) {
-                        this.currentVersion = versionJSON;
-                    }
-                    if (this.currentVersion < versionJSON) {
+                    if ((<any>window).cds_version !== '' && (<any>window).cds_version !== JSON.parse(msg).version) {
                         this.showUIUpdatedBanner = true;
                     }
                 });
