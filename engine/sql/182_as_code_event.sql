e58fe7064e999b655c090b3deb3ccc0523df84a4
Steven Guiheux <steven.guiheux@corp.ovh.com>
2020-01-27 08:25:16 +0100

    fix(sql): code review

--- engine/sql/182_as_code_event.sql
+++ engine/sql/182_as_code_event.sql
@@ -1,15 +1,19 @@
 -- +migrate Up
-ALTER TABLE "workflow_as_code_events" ADD COLUMN data JSONB;
-ALTER TABLE "workflow_as_code_events" ADD COLUMN migrate BOOL DEFAULT false;
-ALTER TABLE "workflow_as_code_events" ADD COLUMN from_repository VARCHAR(300);
-ALTER TABLE "workflow_as_code_events" DROP COLUMN workflow_id;
-ALTER TABLE "workflow_as_code_events" RENAME TO "as_code_events";
+CREATE TABLE IF NOT EXISTS as_code_events
+(
+    id BIGSERIAL PRIMARY KEY,
+    pullrequest_id BIGINT,
+    pullrequest_url VARCHAR(500),
+    username VARCHAR(255),
+    creation_date TIMESTAMP WITH TIME ZONE DEFAULT LOCALTIMESTAMP,
+    from_repository VARCHAR(300),
+    migrate BOOL DEFAULT false,
+    data JSONB
+);
 CREATE INDEX idx_as_code_events_data ON as_code_events USING gin (data);
+ALTER TABLE workflow_as_code_events DROP CONSTRAINT FK_AS_CODE_EVENT;
 
 -- +migrate Down
-ALTER TABLE "as_code_events" RENAME TO "workflow_as_code_events";
-ALTER TABLE "workflow_as_code_events" DROP COLUMN data;
-ALTER TABLE "workflow_as_code_events" DROP COLUMN migrate;
-ALTER TABLE "workflow_as_code_events" DROP COLUMN from_repository;
-ALTER TABLE "workflow_as_code_events" ADD COLUMN workflow_id BIGINT;
+DROP TABLE IF EXISTS as_code_events;
 SELECT create_foreign_key_idx_cascade('FK_AS_CODE_EVENT', 'workflow_as_code_events', 'workflow', 'workflow_id', 'id');
+
