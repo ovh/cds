2aeb50563c823f53036466f83a37b730e632388c
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-01-31 10:40:31 +0100

    feat: download ui / sql
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/cmd_download.go
+++ engine/cmd_download.go
@@ -18,22 +18,32 @@ import (
 
 func init() {
 	downloadCmd.AddCommand(downloadWorkersCmd)
+	downloadCmd.AddCommand(downloadUICmd)
+	downloadCmd.AddCommand(downloadSQLCmd)
 
-	downloadWorkersCmd.Flags().StringVar(&flagDownloadWorkersURLAPI, "api", "", "Update binary from a CDS Engine API")
-	downloadWorkersCmd.Flags().StringVarP(&flagDownloadWorkersOS, "os", "", "", "Download only for this os")
-	downloadWorkersCmd.Flags().StringVarP(&flagDownloadWorkersArch, "arch", "", "", "Download only for this arch")
-	downloadWorkersCmd.Flags().StringVar(&flagDownloadWorkersConfigFile, "config", "", "config file")
-	downloadWorkersCmd.Flags().StringVar(&flagDownloadWorkersRemoteConfig, "remote-config", "", "(optional) consul configuration store")
-	downloadWorkersCmd.Flags().StringVar(&flagDownloadWorkersRemoteConfigKey, "remote-config-key", "cds/config.api.toml", "(optional) consul configuration store key")
+	downloadWorkersCmd.Flags().StringVar(&flagDownloadURLAPI, "api", "", "Update binary from a CDS Engine API")
+	downloadWorkersCmd.Flags().StringVarP(&flagDownloadOS, "os", "", "", "Download only for this os")
+	downloadWorkersCmd.Flags().StringVarP(&flagDownloadArch, "arch", "", "", "Download only for this arch")
+	downloadWorkersCmd.Flags().StringVar(&flagDownloadConfigFile, "config", "", "config file")
+	downloadWorkersCmd.Flags().StringVar(&flagDownloadRemoteConfig, "remote-config", "", "(optional) consul configuration store")
+	downloadWorkersCmd.Flags().StringVar(&flagDownloadRemoteConfigKey, "remote-config-key", "cds/config.api.toml", "(optional) consul configuration store key")
+
+	downloadUICmd.Flags().StringVar(&flagDownloadConfigFile, "config", "", "config file")
+	downloadUICmd.Flags().StringVar(&flagDownloadRemoteConfig, "remote-config", "", "(optional) consul configuration store")
+	downloadUICmd.Flags().StringVar(&flagDownloadRemoteConfigKey, "remote-config-key", "cds/config.api.toml", "(optional) consul configuration store key")
+
+	downloadSQLCmd.Flags().StringVar(&flagDownloadConfigFile, "config", "", "config file")
+	downloadSQLCmd.Flags().StringVar(&flagDownloadRemoteConfig, "remote-config", "", "(optional) consul configuration store")
+	downloadSQLCmd.Flags().StringVar(&flagDownloadRemoteConfigKey, "remote-config-key", "cds/config.api.toml", "(optional) consul configuration store key")
 }
 
 var (
-	flagDownloadWorkersURLAPI          string
-	flagDownloadWorkersOS              string
-	flagDownloadWorkersArch            string
-	flagDownloadWorkersConfigFile      string
-	flagDownloadWorkersRemoteConfig    string
-	flagDownloadWorkersRemoteConfigKey string
+	flagDownloadURLAPI          string
+	flagDownloadOS              string
+	flagDownloadArch            string
+	flagDownloadConfigFile      string
+	flagDownloadRemoteConfig    string
+	flagDownloadRemoteConfigKey string
 )
 
 var downloadCmd = &cobra.Command{
@@ -50,66 +60,94 @@ var downloadWorkersCmd = &cobra.Command{
 You can also indicate a specific os or architecture to not download all binaries available with flag --os and --arch`,
 	Example: "engine download workers",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Initialize config
-		conf := configImport(nil, flagDownloadWorkersConfigFile, flagDownloadWorkersRemoteConfig, flagDownloadWorkersRemoteConfigKey, "", "", false)
-
-		config := cdsclient.Config{Host: flagDownloadWorkersURLAPI}
-		client := cdsclient.New(config)
-
+		conf := configImport(nil, flagDownloadConfigFile, flagDownloadRemoteConfig, flagDownloadRemoteConfigKey, "", "", false)
 		if conf.API == nil {
 			sdk.Exit("Invalid configuration file")
 		}
-
-		if ok, err := sdk.DirectoryExists(conf.API.Directories.Download); !ok {
-			if err := os.MkdirAll(conf.API.Directories.Download, os.FileMode(0700)); err != nil {
-				sdk.Exit("Unable to create directory %s: %v", conf.API.Directories.Download, err)
-			}
-			log.Info(context.Background(), "Directory %s has been created", conf.API.Directories.Download)
-		} else if err != nil {
-			sdk.Exit("Invalid download directory %s: %v", conf.API.Directories.Download, err)
-		}
-
-		filename := "cds-worker-all.tar.gz"
-		urlBinary, err := client.DownloadURLFromGithub(filename)
-		if err != nil {
-			sdk.Exit("Error while getting %s from err:%s\n", filename, urlBinary, err)
-		}
-
-		fmt.Printf("Downloading workers into %s...\n", conf.API.Directories.Download)
-		resp, err := http.Get(urlBinary)
-		if err != nil {
-			sdk.Exit("Error while getting binary from: %s err:%s\n", urlBinary, err)
-		}
-		defer resp.Body.Close()
-
-		if err := sdk.CheckContentTypeBinary(resp); err != nil {
-			sdk.Exit(err.Error())
-		}
-
-		if resp.StatusCode != 200 {
-			sdk.Exit("Error http code: %d, url called: %s\n", resp.StatusCode, urlBinary)
-		}
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			sdk.Exit("Error while reading file content for %s", filename)
-		}
-
-		fullpath := path.Join(conf.API.Directories.Download, filename)
-		if err := ioutil.WriteFile(fullpath, body, 0755); err != nil {
-			sdk.Exit("Error while write file content for %s in %s", filename, conf.API.Directories.Download)
-		}
-
-		if err := archiver.DefaultTarGz.Unarchive(fullpath, conf.API.Directories.Download); err != nil {
-			sdk.Exit("Unarchive %s failed: %v", filename, err)
-		}
-		fmt.Printf("Unarchive to %s\n", conf.API.Directories.Download)
-
-		// delete file cds-worker-all.tar.gz
-		if err := os.Remove(fullpath); err != nil {
-			sdk.Exit("Error while deleting file %s: %v", fullpath, err)
-		}
-
-		fmt.Println("Download workers binaries done.")
+		downloadTarGzFromGithub(conf.API.Directories.Download, "cds-worker-all.tar.gz")
 	},
+}
+
+var downloadUICmd = &cobra.Command{
+	Use:     "ui",
+	Short:   "Download ui files from the latest release on GitHub",
+	Long:    `Download ui files from the latest release on GitHub`,
+	Example: "engine download ui",
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := configImport(nil, flagDownloadConfigFile, flagDownloadRemoteConfig, flagDownloadRemoteConfigKey, "", "", false)
+		if conf.API == nil {
+			sdk.Exit("Invalid configuration file")
+		}
+		downloadTarGzFromGithub(conf.UI.Staticdir, "ui.tar.gz")
+	},
+}
+
+var downloadSQLCmd = &cobra.Command{
+	Use:     "sql",
+	Short:   "Download sql files from the latest release on GitHub",
+	Long:    `Download sql files from the latest release on GitHub`,
+	Example: "engine download sql",
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := configImport(nil, flagDownloadConfigFile, flagDownloadRemoteConfig, flagDownloadRemoteConfigKey, "", "", false)
+		if conf.API == nil {
+			sdk.Exit("Invalid configuration file")
+		}
+		downloadTarGzFromGithub(conf.DatabaseMigrate.Directory, "sql.tar.gz")
+	},
+}
+
+func downloadTarGzFromGithub(confPath, filename string) {
+	config := cdsclient.Config{Host: flagDownloadURLAPI}
+	client := cdsclient.New(config)
+
+	if ok, err := sdk.DirectoryExists(confPath); !ok {
+		if err := os.MkdirAll(confPath, os.FileMode(0700)); err != nil {
+			sdk.Exit("Unable to create directory %s: %v", confPath, err)
+		}
+		log.Info(context.Background(), "Directory %s has been created", confPath)
+	} else if err != nil {
+		sdk.Exit("Invalid download directory %s: %v", confPath, err)
+	}
+
+	urlBinary, err := client.DownloadURLFromGithub(filename)
+	if err != nil {
+		sdk.Exit("Error while getting %s from err:%s\n", filename, urlBinary, err)
+	}
+
+	fmt.Printf("Downloading into %s...\n", confPath)
+	resp, err := http.Get(urlBinary)
+	if err != nil {
+		sdk.Exit("Error while getting binary from: %s err:%s\n", urlBinary, err)
+	}
+	defer resp.Body.Close()
+
+	if err := sdk.CheckContentTypeBinary(resp); err != nil {
+		sdk.Exit(err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		sdk.Exit("Error http code: %d, url called: %s\n", resp.StatusCode, urlBinary)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		sdk.Exit("Error while reading file content for %s", filename)
+	}
+
+	fullpath := path.Join(confPath, filename)
+	if err := ioutil.WriteFile(fullpath, body, 0755); err != nil {
+		sdk.Exit("Error while write file content for %s in %s", filename, confPath)
+	}
+
+	if err := archiver.DefaultTarGz.Unarchive(fullpath, confPath); err != nil {
+		sdk.Exit("Unarchive %s failed: %v", filename, err)
+	}
+	fmt.Printf("Unarchive to %s\n", confPath)
+
+	// delete file cds-worker-all.tar.gz
+	if err := os.Remove(fullpath); err != nil {
+		sdk.Exit("Error while deleting file %s: %v", fullpath, err)
+	}
+
+	fmt.Println("Download done.")
 }
