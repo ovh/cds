01b4d991d2672e57523c68cc76c0020397979a5a
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-02-14 09:43:40 +0100

    fix(api): ut for usernames selected to send notifications
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

---
+++ engine/api/notification/permission_test.go
@@ -0,0 +1,63 @@
+package notification
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alecthomas/assert"
+	"github.com/ovh/cds/engine/api/bootstrap"
+	"github.com/ovh/cds/engine/api/group"
+	"github.com/ovh/cds/engine/api/project"
+	"github.com/ovh/cds/engine/api/test"
+	"github.com/ovh/cds/engine/api/test/assets"
+	"github.com/ovh/cds/sdk"
+	"github.com/stretchr/testify/require"
+)
+
+// Test_projectPermissionUserIDs test the usernames selected to send notifications
+func Test_projectPermissionUserIDs(t *testing.T) {
+	db, cache, end := test.SetupPG(t, bootstrap.InitiliazeDB)
+	defer end()
+
+	g1 := assets.InsertTestGroup(t, db, sdk.RandomString(10))
+	g2 := assets.InsertTestGroup(t, db, sdk.RandomString(10))
+	g3 := assets.InsertTestGroup(t, db, sdk.RandomString(10))
+
+	u1, _ := assets.InsertLambdaUser(t, db, g1)
+	u2, _ := assets.InsertLambdaUser(t, db, g2)
+
+	pkey := sdk.RandomString(10)
+	proj := assets.InsertTestProject(t, db, cache, pkey, pkey)
+
+	require.NoError(t, group.InsertLinkGroupProject(context.TODO(), db, &group.LinkGroupProject{
+		GroupID:   g2.ID,
+		ProjectID: proj.ID,
+		Role:      sdk.PermissionReadWriteExecute,
+	}))
+
+	require.NoError(t, group.InsertLinkGroupProject(context.TODO(), db, &group.LinkGroupProject{
+		GroupID:   g3.ID,
+		ProjectID: proj.ID,
+		Role:      sdk.PermissionReadWriteExecute,
+	}))
+
+	require.NoError(t, group.InsertLinkGroupUser(context.TODO(), db, &group.LinkGroupUser{
+		GroupID:            g3.ID,
+		AuthentifiedUserID: u2.ID,
+		Admin:              false,
+	}), "unable to insert user in group")
+
+	assert.NoError(t, project.Update(db, cache, proj))
+
+	group.DefaultGroup = g1
+
+	userList, err := projectPermissionUserIDs(context.Background(), db, cache, proj.ID, sdk.PermissionRead)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, userList)
+	assert.Equal(t, 1, len(userList))
+
+	for _, u := range userList {
+		assert.NotEqual(t, u, u1.Username, "User in default group should not be here")
+	}
+
+}
