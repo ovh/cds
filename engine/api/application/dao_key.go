cea0d22667d1f0d9a1295e761d91bf1f374655b3
francois  samin <francois.samin@corp.ovh.com>
2020-02-17 10:09:26 +0100

    refactor(api): application keys migration
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- engine/api/application/dao_key.go
+++ engine/api/application/dao_key.go
@@ -13,7 +13,18 @@ import (
 // InsertKey a new application key in database
 func InsertKey(db gorp.SqlExecutor, key *sdk.ApplicationKey) error {
 	var dbAppKey = dbApplicationKey{ApplicationKey: *key}
-	if err := gorpmapping.InsertAndSign(context.TODO(), db, &dbAppKey); err != nil {
+	if err := gorpmapping.InsertAndSign(context.Background(), db, &dbAppKey); err != nil {
+		return err
+	}
+	*key = dbAppKey.ApplicationKey
+	return nil
+}
+
+// UpdateKey a new application key in database.
+// This function should be use only for migration purpose and should be removed
+func UpdateKey(ctx context.Context, db gorp.SqlExecutor, key *sdk.ApplicationKey) error {
+	var dbAppKey = dbApplicationKey{ApplicationKey: *key}
+	if err := gorpmapping.UpdateAndSign(ctx, db, &dbAppKey); err != nil {
 		return err
 	}
 	*key = dbAppKey.ApplicationKey
