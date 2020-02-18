428e3238d9a69ccd40cd5814be5fa3b09a88e159
francois  samin <francois.samin@corp.ovh.com>
2020-02-18 09:24:23 +0100

    feat(sql): improve sql migration

--- engine/sql/186_application_key.sql
+++ engine/sql/186_application_key.sql
@@ -1,4 +1,7 @@
 -- +migrate Up
+
+CREATE TABLE application_key_tmp AS SELECT * FROM application_key;
+
 ALTER TABLE "application_key" ADD COLUMN IF NOT EXISTS sig BYTEA;
 ALTER TABLE "application_key" ADD COLUMN IF NOT EXISTS signer TEXT;
 
@@ -6,3 +9,15 @@ ALTER TABLE "application_key" ADD COLUMN IF NOT EXISTS signer TEXT;
 
 ALTER TABLE "application_key" DROP COLUMN sig;
 ALTER TABLE "application_key" DROP COLUMN signer;
+
+UPDATE application_key 
+SET application_id      = application_key_tmp.application_id,
+    name                = application_key_tmp.name,
+    type                = application_key_tmp.type,
+    public              = application_key_tmp.public,
+    key_id              = application_key_tmp.key_id,
+    private             =  application_key_tmp.private
+FROM application_key_tmp
+WHERE application_key_tmp.id = application_key.id;
+
+DROP TABLE application_key_tmp;
