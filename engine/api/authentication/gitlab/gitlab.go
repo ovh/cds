1b431684bce2ca5bffae8390864e65ccd531c6c3
Louis GOUNOT <louis-gounot@users.noreply.github.com>
2020-02-25 14:55:16 +0100

    ovh/cds#5005 Fix gitlab user auth with custom endpoint
    Signed-off-by: Louis GOUNOT <louis-gounot@users.noreply.github.com>

--- engine/api/authentication/gitlab/gitlab.go
+++ engine/api/authentication/gitlab/gitlab.go
@@ -100,6 +100,10 @@ func (d authDriver) GetUserInfo(ctx context.Context, req sdk.AuthConsumerSigninR
 	}
 
 	c := gitlab.NewOAuthClient(http.DefaultClient, t.AccessToken)
+	err = c.SetBaseURL(d.url)
+	if err != nil {
+		return info, sdk.WrapError(err, "invalid gitlab url")
+	}
 	me, res, err := c.Users.CurrentUser()
 	if err != nil {
 		return info, sdk.WrapError(err, "cannot get current user from gitlab")
