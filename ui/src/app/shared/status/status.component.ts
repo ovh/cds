58893430e07a0e6545cf006838ebacfba58ab563
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-03-02 17:33:39 +0100

    fix: cr
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- ui/src/app/shared/status/status.component.ts
+++ ui/src/app/shared/status/status.component.ts
@@ -9,7 +9,6 @@ import { PipelineStatus } from 'app/model/pipeline.model';
 })
 export class StatusIconComponent {
     @Input() status: string;
-    @Input() value: string;
     @Input() optional: boolean;
     @Input() loader = true;
     pipelineStatusEnum = PipelineStatus;
