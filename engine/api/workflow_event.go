5f8c2cf2b9e7c0d2fa2acc3d2f937682afe5910b
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-03-25 11:50:32 +0100

    fix: cr
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/api/workflow_event.go
+++ engine/api/workflow_event.go
@@ -47,12 +47,6 @@ func WorkflowSendEvent(ctx context.Context, db gorp.SqlExecutor, store cache.Sto
 			log.Warning(ctx, "workflowSendEvent > Cannot load workflow node run: %v", err)
 			continue
 		}
-		// if report contains a node run with waiting status, this status
-		// could be overrided in the same process for empty pipeline, with the Success value.
-		// So, we keep the initial status from the report to avoid sending duplicate notification
-		if len(wnr.Stages) == 0 {
-			nr.Status = wnr.Status
-		}
 
 		event.PublishWorkflowNodeRun(ctx, db, store, *nr, wr.Workflow, &previousNodeRun)
 		e := &workflow.VCSEventMessenger{}
