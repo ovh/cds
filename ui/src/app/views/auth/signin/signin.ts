b4dd4db50d5dba2673c2c3b376415d9e0a9224ed
Brandon Nason <brandon.nason@gmail.com>
2020-04-28 15:05:50 -0500

    refactor(api): generalize ldap
    Signed-off-by: Brandon Nason <brandon.nason@gmail.com>

--- ui/src/app/views/auth/signin/signin.ts
+++ ui/src/app/views/auth/signin/signin.ts
@@ -129,7 +129,7 @@ export class SigninComponent implements OnInit {
     }
 
     ldapSignin(f: NgForm) {
-        this._authenticationService.ldapSignin(f.value.bind, f.value.password, f.value.init_token).subscribe(() => {
+        this._authenticationService.ldapSignin(f.value.user, f.value.password, f.value.init_token).subscribe(() => {
             if (this.redirect) {
                 this._router.navigateByUrl(decodeURIComponent(this.redirect));
             } else {
