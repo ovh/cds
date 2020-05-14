45b0cfadb7356e03e608dd27a94e1f77533f23f3
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-05-14 18:16:06 +0200

    fix(api): rename worker model
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/api/action/dao.go
+++ engine/api/action/dao.go
@@ -200,8 +200,8 @@ func GetRequirementsTypeModelAndValueStartBy(ctx context.Context, db gorp.SqlExe
 	query := gorpmapping.NewQuery(`
     SELECT *
     FROM action_requirement
-    WHERE type = 'model' AND value LIKE $1
-  `).Args(value + "%")
+    WHERE type = 'model' AND (value LIKE $1 OR value = $2)
+  `).Args(value+" %", value)
 
 	if err := gorpmapping.GetAll(ctx, db, query, &rs); err != nil {
 		return nil, sdk.WrapError(err, "cannot get requirements")
