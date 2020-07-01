ed90b643476ecb5172a7a95ab7bf7ab8d7b85586
Steven Guiheux <steven.guiheux@corp.ovh.com>
2020-07-01 10:09:30 +0200

    fix: update docker compose to use cdn

--- engine/cdn/types.go
+++ engine/cdn/types.go
@@ -29,8 +29,8 @@ type Configuration struct {
 		Port int    `toml:"port" default:"8089" json:"port"`
 	} `toml:"http" comment:"######################\n CDS CDN HTTP Configuration \n######################" json:"http"`
 	URL        string                          `default:"http://localhost:8089" json:"url" comment:"Private URL for communication with API"`
-	PublicTCP  string                          `toml:"publicTCP" comment:"Public address to access to CDN TCP server" json:"public_tcp"`
-	PublicHTTP string                          `toml:"publicHTTP" comment:"Public address to access to CDN HTTP server" json:"public_http"`
+	PublicTCP  string                          `toml:"publicTCP" default:"localhost:8090" comment:"Public address to access to CDN TCP server" json:"public_tcp"`
+	PublicHTTP string                          `toml:"publicHTTP" default:"localhost:8089" comment:"Public address to access to CDN HTTP server" json:"public_http"`
 	API        service.APIServiceConfiguration `toml:"api" comment:"######################\n CDS API Settings \n######################" json:"api"`
 	Log        struct {
 		StepMaxSize    int64 `toml:"stepMaxSize" default:"15728640" comment:"Max step logs size in bytes (default: 15MB)" json:"stepMaxSize"`
