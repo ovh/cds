ebfae3812b046ed56f25173a4514cf850e86876e
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-08-03 11:52:50 +0200

    fix(hooks): urlPublic conf is mandatory
    close #5356
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/api/workflow/hook.go
+++ engine/api/workflow/hook.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/fsamin/go-dump"
 
-	"github.com/ovh/cds/engine/cache"
 	"github.com/ovh/cds/engine/api/repositoriesmanager"
 	"github.com/ovh/cds/engine/api/services"
+	"github.com/ovh/cds/engine/cache"
 	"github.com/ovh/cds/engine/gorpmapper"
 	"github.com/ovh/cds/sdk"
 	"github.com/ovh/cds/sdk/log"
