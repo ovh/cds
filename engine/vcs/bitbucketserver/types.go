165a2a22d418b6392b196ad33d066f9833a5a702
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2020-08-18 16:08:56 +0200

    refactor(vcs): remove dead code bbhooks (#5380)
    * refactor(vcs): remove dead code bbhooks
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/vcs/bitbucketserver/types.go
+++ engine/vcs/bitbucketserver/types.go
@@ -1,11 +1,6 @@
 package bitbucketserver
 
 import (
-	"encoding/json"
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/ovh/cds/sdk"
 )
 
@@ -102,83 +97,6 @@ type HookDetail struct {
 	Description   string `json:"description"`
 	Version       string `json:"version"`
 	ConfigFormKey string `json:"configFormKey"`
-}
-
-type HooksConfig struct {
-	Version       string
-	LocationCount string
-	Details       []HookConfigDetail
-}
-
-func (h *HooksConfig) MarshalJSON() ([]byte, error) {
-	m := make(map[string]interface{})
-	m["version"] = h.Version
-	m["locationCount"] = fmt.Sprintf("%d", len(h.Details))
-	for i, d := range h.Details {
-		var keySuffix string
-		if i > 0 {
-			keySuffix = fmt.Sprintf("%d", i+1)
-		}
-
-		m["httpMethod"+keySuffix] = d.Method
-		m["url"+keySuffix] = d.URL
-		m["postContentType"+keySuffix] = d.PostContentType
-		m["postData"+keySuffix] = d.PostData
-		m["branchFilter"+keySuffix] = d.BranchFilter
-		m["tagFilter"+keySuffix] = d.TagFilter
-		m["userFilter"+keySuffix] = d.UserFilter
-		m["skipSsl"+keySuffix] = d.SkipSsl
-		m["useAuth"+keySuffix] = d.UseAuth
-	}
-	return json.Marshal(m)
-}
-
-func (h *HooksConfig) UnmarshalJSON(b []byte) error {
-	m := make(map[string]interface{})
-	if err := json.Unmarshal(b, &m); err != nil {
-		return err
-	}
-	var nbLocation int
-	for k := range m {
-		if strings.HasPrefix(k, "url") {
-			nbLocation++
-		}
-	}
-	h.LocationCount = fmt.Sprintf("%d", nbLocation)
-	h.Version = "3"
-	h.Details = make([]HookConfigDetail, nbLocation)
-	for i := 0; i < nbLocation; i++ {
-		var keySuffix string
-		if i > 0 {
-			keySuffix = fmt.Sprintf("%d", i+1)
-		}
-		skipSsl, _ := strconv.ParseBool(fmt.Sprintf("%s", m["skipSsl"+keySuffix]))
-		useAuth, _ := strconv.ParseBool(fmt.Sprintf("%s", m["useAuth"+keySuffix]))
-		h.Details[i] = HookConfigDetail{
-			BranchFilter:    fmt.Sprintf("%s", m["branchFilter"+keySuffix]),
-			Method:          fmt.Sprintf("%s", m["httpMethod"+keySuffix]),
-			PostContentType: fmt.Sprintf("%s", m["postContentType"+keySuffix]),
-			PostData:        fmt.Sprintf("%s", m["postData"+keySuffix]),
-			TagFilter:       fmt.Sprintf("%s", m["tagFilter"+keySuffix]),
-			URL:             fmt.Sprintf("%s", m["url"+keySuffix]),
-			UserFilter:      fmt.Sprintf("%s", m["userFilter"+keySuffix]),
-			SkipSsl:         skipSsl,
-			UseAuth:         useAuth,
-		}
-	}
-	return nil
-}
-
-type HookConfigDetail struct {
-	Method          string `json:"httpMethod"`
-	URL             string `json:"url"`
-	PostContentType string `json:"postContentType"`
-	PostData        string `json:"postData"`
-	BranchFilter    string `json:"branchFilter"`
-	TagFilter       string `json:"tagFilter"`
-	UserFilter      string `json:"userFilter"`
-	SkipSsl         bool   `json:"skipSsl"`
-	UseAuth         bool   `json:"useAuth"`
 }
 
 type Hook struct {
