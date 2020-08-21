a86dd44ad730742f0ac8a1b44adb2a74c987136e
richardlt <richard.le.terrier@gmail.com>
2020-08-21 17:11:08 +0200

    fix(api): event username should be consumer name for services

--- engine/api/ascode/sync.go
+++ engine/api/ascode/sync.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/go-gorp/gorp"
 
-	"github.com/ovh/cds/engine/cache"
 	"github.com/ovh/cds/engine/api/event"
 	"github.com/ovh/cds/engine/api/repositoriesmanager"
+	"github.com/ovh/cds/engine/cache"
 	"github.com/ovh/cds/sdk"
 	"github.com/ovh/cds/sdk/log"
 )
