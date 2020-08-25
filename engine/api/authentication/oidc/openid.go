68d5d53bfcc7f184dd862770d67cd32b0ae2ad56
phsym <pierre-henri.symoneaux@nokia.com>
2020-08-25 10:09:30 +0200

    feat(api): basic support for openid-connect authentication provider
    This enables basic integration with openid-connect authentication providers like Keycloak or Hydra
    
    Signed-off-by: phsym <pierre-henri.symoneaux@nokia.com>

---
+++ engine/api/authentication/oidc/openid.go
@@ -0,0 +1,131 @@
+package oidc
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/ovh/cds/engine/api/authentication"
+
+	oidc "github.com/coreos/go-oidc"
+	"github.com/ovh/cds/sdk"
+	"golang.org/x/oauth2"
+)
+
+var _ sdk.AuthDriverWithRedirect = (*authDriver)(nil)
+var _ sdk.AuthDriverWithSigninStateToken = (*authDriver)(nil)
+
+// NewDriver returns a new OIDC auth driver for given config.
+func NewDriver(signupDisabled bool, cdsURL, url, clientID, clientSecret string) sdk.AuthDriver {
+	provider, _ := oidc.NewProvider(context.Background(), url)
+	// Configure an OpenID Connect aware OAuth2 client.
+	oauth2Config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  fmt.Sprintf("%s/auth/callback/%s", cdsURL, sdk.ConsumerOIDC),
+		// Discovery returns the OAuth2 endpoints.
+		Endpoint: provider.Endpoint(),
+		// "openid" is a required scope for OpenID Connect flows.
+		Scopes: []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+	oidcConfig := &oidc.Config{
+		ClientID: clientID,
+	}
+	verifier := provider.Verifier(oidcConfig)
+
+	return &authDriver{
+		signupDisabled: signupDisabled,
+		cdsURL:         cdsURL,
+		OAuth2Config:   oauth2Config,
+		Verifier:       verifier,
+	}
+}
+
+type authDriver struct {
+	signupDisabled bool
+	cdsURL         string
+	OAuth2Config   oauth2.Config
+	Verifier       *oidc.IDTokenVerifier
+}
+
+func (d authDriver) GetManifest() sdk.AuthDriverManifest {
+	return sdk.AuthDriverManifest{
+		Type:           sdk.ConsumerOIDC,
+		SignupDisabled: d.signupDisabled,
+	}
+}
+
+func (d authDriver) GetSigninURI(signinState sdk.AuthSigninConsumerToken) (sdk.AuthDriverSigningRedirect, error) {
+	// Generate a new state value for the auth signin request
+	jws, err := authentication.NewDefaultSigninStateToken(signinState.Origin,
+		signinState.RedirectURI, signinState.IsFirstConnection)
+	if err != nil {
+		return sdk.AuthDriverSigningRedirect{}, err
+	}
+
+	var result = sdk.AuthDriverSigningRedirect{
+		Method: http.MethodGet,
+		URL:    d.OAuth2Config.AuthCodeURL(jws),
+	}
+
+	return result, nil
+}
+
+func (d authDriver) GetSessionDuration() time.Duration {
+	return time.Hour * 24 * 30 // 1 month session
+}
+
+func (d authDriver) CheckSigninRequest(req sdk.AuthConsumerSigninRequest) error {
+	if code, ok := req["code"]; !ok || code == "" {
+		return sdk.NewErrorFrom(sdk.ErrWrongRequest, "missing or invalid code")
+	}
+	return nil
+}
+
+func (d authDriver) CheckSigninStateToken(req sdk.AuthConsumerSigninRequest) error {
+	// Check if state is given and if its valid
+	state, okState := req["state"]
+	if !okState {
+		return sdk.NewErrorFrom(sdk.ErrWrongRequest, "missing state value")
+	}
+
+	return authentication.CheckDefaultSigninStateToken(state)
+}
+
+func (d authDriver) GetUserInfo(ctx context.Context, req sdk.AuthConsumerSigninRequest) (sdk.AuthDriverUserInfo, error) {
+	var info sdk.AuthDriverUserInfo
+
+	ctx2 := context.WithValue(context.Background(), oauth2.HTTPClient, http.DefaultClient)
+	oauth2Token, err := d.OAuth2Config.Exchange(ctx2, req["code"])
+	if err != nil {
+		return info, sdk.WrapError(err, "Failed to exchange token")
+	}
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		return info, sdk.WrapError(err, "No id_token field in oauth2 token")
+	}
+	idToken, err := d.Verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		return info, sdk.WrapError(err, "Failed to verify ID Token")
+	}
+	tokenClaim := make(map[string]interface{})
+	if err := idToken.Claims(&tokenClaim); err != nil {
+		return info, sdk.WrapError(err, "Cannot unmarshal OIDC claim")
+	}
+
+	if info.ExternalID, ok = tokenClaim["sub"].(string); !ok {
+		return info, sdk.WrapError(sdk.ErrInvalidUser, "Missing OIDC user ID in token claim")
+	}
+
+	if info.Username, ok = tokenClaim["preferred_username"].(string); !ok {
+		return info, sdk.WrapError(sdk.ErrInvalidUser, "Missing username in OIDC token claim")
+	}
+
+	info.Fullname, _ = tokenClaim["name"].(string)
+	if info.Email, ok = tokenClaim["email"].(string); !ok {
+		return info, sdk.WrapError(sdk.ErrInvalidUser, "Missing user's email in OIDC token claim")
+	}
+
+	return info, nil
+}
