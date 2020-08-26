aaa54949b888c0d581e857b41dd231da4693051d
Steven Guiheux <steven.guiheux@corp.ovh.com>
2020-08-26 13:42:56 +0200

    fix: unit test

--- engine/cdn/index/dao_test.go
+++ engine/cdn/index/dao_test.go
@@ -20,7 +20,8 @@ func TestLoadItem(t *testing.T) {
 	db, _ := test.SetupPGWithMapper(t, m, sdk.TypeCDN)
 
 	i := index.Item{
-		Type: index.TypeItemStepLog,
+		Type:       index.TypeItemStepLog,
+		ApiRefHash: sdk.UUID(),
 	}
 	require.NoError(t, index.InsertItem(context.TODO(), m, db, &i))
 
@@ -35,13 +36,23 @@ func TestLoadOldItemIDsByStatusAndDuration(t *testing.T) {
 	index.InitDBMapping(m)
 	db, _ := test.SetupPGWithMapper(t, m, sdk.TypeCDN)
 
+	// Clean old test
+	time.Sleep(1 * time.Second)
+	ids, err := index.LoadOldItemIDsByStatusAndDuration(db, index.StatusItemIncoming, 1)
+	require.NoError(t, err)
+	for _, id := range ids {
+		i, err := index.LoadItemByID(context.TODO(), m, db, id)
+		require.NoError(t, err)
+		require.NoError(t, index.DeleteItem(m, db, i))
+	}
+
 	i1 := &index.Item{
 		ID:         sdk.UUID(),
 		ApiRefHash: sdk.UUID(),
 		Type:       index.TypeItemStepLog,
 		Status:     index.StatusItemCompleted,
 	}
-	err := index.InsertItem(context.TODO(), m, db, i1)
+	err = index.InsertItem(context.TODO(), m, db, i1)
 	require.NoError(t, err)
 
 	i2 := &index.Item{
@@ -54,12 +65,11 @@ func TestLoadOldItemIDsByStatusAndDuration(t *testing.T) {
 	require.NoError(t, err)
 	defer func() {
 		_ = index.DeleteItem(m, db, i2)
-
 	}()
 
 	time.Sleep(2 * time.Second)
 
-	ids, err := index.LoadOldItemIDsByStatusAndDuration(db, index.StatusItemIncoming, 1)
+	ids, err = index.LoadOldItemIDsByStatusAndDuration(db, index.StatusItemIncoming, 1)
 	require.NoError(t, err)
 	require.Len(t, ids, 1)
 	require.Equal(t, i2.ID, ids[0])
