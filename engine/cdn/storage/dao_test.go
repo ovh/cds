3c906347e91718d45647de4771c6b01ba6e70e7f
Steven Guiheux <steven.guiheux@corp.ovh.com>
2020-09-16 11:42:54 +0200

    fix: import in unit test

--- engine/cdn/storage/dao_test.go
+++ engine/cdn/storage/dao_test.go
@@ -1,7 +1,8 @@
-package storage
+package storage_test
 
 import (
 	"context"
+	"github.com/ovh/cds/engine/cdn/storage"
 	"testing"
 	"time"
 
@@ -17,16 +18,16 @@ import (
 func TestLoadOldItemUnitByItemStatusAndDuration(t *testing.T) {
 	m := gorpmapper.New()
 	index.InitDBMapping(m)
-	InitDBMapping(m)
+	storage.InitDBMapping(m)
 	db, _ := test.SetupPGWithMapper(t, m, sdk.TypeCDN)
 	cfg := test.LoadTestingConf(t, sdk.TypeCDN)
 
 	cdntest.ClearIndex(t, context.TODO(), m, db)
 
-	cdnUnits, err := Init(context.TODO(), m, db.DbMap, Configuration{
-		Buffer: BufferConfiguration{
+	cdnUnits, err := storage.Init(context.TODO(), m, db.DbMap, storage.Configuration{
+		Buffer: storage.BufferConfiguration{
 			Name: "redis_buffer",
-			Redis: RedisBufferConfiguration{
+			Redis: storage.RedisBufferConfiguration{
 				Host:     cfg["redisHost"],
 				Password: cfg["redisPassword"],
 			},
@@ -35,7 +36,7 @@ func TestLoadOldItemUnitByItemStatusAndDuration(t *testing.T) {
 
 	// Clean old test
 	time.Sleep(1 * time.Second)
-	itemUnits, err := LoadOldItemUnitByItemStatusAndDuration(context.TODO(), m, db, index.StatusItemIncoming, 1)
+	itemUnits, err := storage.LoadOldItemUnitByItemStatusAndDuration(context.TODO(), m, db, index.StatusItemIncoming, 1)
 	require.NoError(t, err)
 	for _, itemUnit := range itemUnits {
 		i, err := index.LoadItemByID(context.TODO(), m, db, itemUnit.ItemID)
@@ -57,7 +58,7 @@ func TestLoadOldItemUnitByItemStatusAndDuration(t *testing.T) {
 
 	itemUnit1, err := cdnUnits.NewItemUnit(context.TODO(), cdnUnits.Buffer, i1)
 	require.NoError(t, err)
-	require.NoError(t, InsertItemUnit(context.TODO(), m, db, itemUnit1))
+	require.NoError(t, storage.InsertItemUnit(context.TODO(), m, db, itemUnit1))
 
 	i2 := &index.Item{
 		ID:         sdk.UUID(),
@@ -73,11 +74,11 @@ func TestLoadOldItemUnitByItemStatusAndDuration(t *testing.T) {
 	itemUnit2, err := cdnUnits.NewItemUnit(context.TODO(), cdnUnits.Buffer, i2)
 	require.NoError(t, err)
 
-	require.NoError(t, InsertItemUnit(context.TODO(), m, db, itemUnit2))
+	require.NoError(t, storage.InsertItemUnit(context.TODO(), m, db, itemUnit2))
 
 	time.Sleep(2 * time.Second)
 
-	itemUnits, err = LoadOldItemUnitByItemStatusAndDuration(context.TODO(), m, db, index.StatusItemIncoming, 1)
+	itemUnits, err = storage.LoadOldItemUnitByItemStatusAndDuration(context.TODO(), m, db, index.StatusItemIncoming, 1)
 	require.NoError(t, err)
 	require.Len(t, itemUnits, 1)
 	require.Equal(t, i2.ID, itemUnits[0].ItemID)
