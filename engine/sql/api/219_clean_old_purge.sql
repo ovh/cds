a2b7661333bd9ef9e07a544134c8f42660abe7ac
Steven Guiheux <steven.guiheux@corp.ovh.com>
2020-11-05 14:58:19 +0100

    fix: rmeove sql script

--- engine/sql/api/219_clean_old_purge.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-alter table "workflow" drop column purge_tags;
-alter table "workflow" drop column history_length;
-
-
--- +migrate Down
-ALTER TABLE workflow ADD COLUMN purge_tags JSONB;
-ALTER TABLE workflow ADD COLUMN history_length BIGINT DEFAULT 20;
