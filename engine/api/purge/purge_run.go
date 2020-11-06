78e19ff851a893675666336596625feba9b4aa85
Steven Guiheux <steven.guiheux@corp.ovh.com>
2020-11-06 15:44:21 +0100

    fix: add context check

--- engine/api/purge/purge_run.go
+++ engine/api/purge/purge_run.go
@@ -60,28 +60,28 @@ func markWorkflowRunsToDelete(ctx context.Context, store cache.Store, db *gorp.D
 }
 
 func ApplyRetentionPolicyOnWorkflow(ctx context.Context, store cache.Store, db *gorp.DbMap, wf sdk.Workflow, opts MarkAsDeleteOptions, u *sdk.AuthentifiedUser) error {
-	log.Info(ctx, "ApplyRetentionPolicyOnWorkflow> check retention policy for workflow %s/%s", wf.ProjectKey, wf.Name)
 	var vcsClient sdk.VCSAuthorizedClientService
 	var app sdk.Application
-	appID := wf.WorkflowData.Node.Context.ApplicationID
-	if appID != 0 {
-		app = wf.Applications[appID]
-		if app.RepositoryFullname != "" {
-			tx, err := db.Begin()
-			if err != nil {
-				return sdk.WithStack(err)
-			}
-			//Get the RepositoriesManager Client
-			vcsServer, err := repositoriesmanager.LoadProjectVCSServerLinkByProjectKeyAndVCSServerName(ctx, tx, wf.ProjectKey, app.VCSServer)
-			if err != nil {
-				log.Error(ctx, "SendVCSEvent> No vcsServer found: %v", err)
-				_ = tx.Rollback()
-				return err
-			}
-			vcsClient, err = repositoriesmanager.AuthorizedClient(ctx, tx, store, wf.ProjectKey, vcsServer)
-			if err != nil {
-				_ = tx.Rollback()
-				return sdk.WithStack(err)
+	if wf.WorkflowData.Node.Context != nil {
+		appID := wf.WorkflowData.Node.Context.ApplicationID
+		if appID != 0 {
+			app = wf.Applications[appID]
+			if app.RepositoryFullname != "" {
+				tx, err := db.Begin()
+				if err != nil {
+					return sdk.WithStack(err)
+				}
+				//Get the RepositoriesManager Client
+				vcsServer, err := repositoriesmanager.LoadProjectVCSServerLinkByProjectKeyAndVCSServerName(ctx, tx, wf.ProjectKey, app.VCSServer)
+				if err != nil {
+					_ = tx.Rollback()
+					return err
+				}
+				vcsClient, err = repositoriesmanager.AuthorizedClient(ctx, tx, store, wf.ProjectKey, vcsServer)
+				if err != nil {
+					_ = tx.Rollback()
+					return sdk.WithStack(err)
+				}
 			}
 		}
 	}
