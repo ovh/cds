763f1d0d037ba02870094c9e73c8c03edb2038e7
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2021-02-02 20:33:17 +0100

    fix(cdn): avoid panic on purge
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/cdn/storage/storageunit_purge.go
+++ engine/cdn/storage/storageunit_purge.go
@@ -30,13 +30,18 @@ func (x *RunningStorageUnits) Purge(ctx context.Context, s Interface) error {
 	}
 
 	for _, ui := range unitItems {
-		ctx = context.WithValue(ctx, FieldAPIRef, ui.Item.APIRefHash)
-		ctx = context.WithValue(ctx, FieldSize, ui.Item.Size)
-
-		exists, err := s.ItemExists(ctx, x.m, x.db, *ui.Item)
-		if err != nil {
-			return err
+		var exists bool
+		// here, item could be nil if there are many cdn instances purging the same item
+		if ui.Item != nil {
+			ctx = context.WithValue(ctx, FieldAPIRef, ui.Item.APIRefHash)
+			ctx = context.WithValue(ctx, FieldSize, ui.Item.Size)
+			var err error
+			exists, err = s.ItemExists(ctx, x.m, x.db, *ui.Item)
+			if err != nil {
+				return err
+			}
 		}
+
 		if exists {
 			var hasItemUnit bool
 			if _, hasLocator := s.(StorageUnitWithLocator); hasLocator {
