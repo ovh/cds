27287c41227c1227338b3ea3f2a01f6e0b764931
francois  samin <francois.samin@corp.ovh.com>
2021-06-14 11:30:46 +0200

    fix: cr
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- engine/api/migrate/migrate_auth_consumer_token_expiration.go
+++ engine/api/migrate/migrate_auth_consumer_token_expiration.go
@@ -71,7 +71,7 @@ func authConsumerTokenExpirationPerID(ctx context.Context, tx gorpmapper.SqlExec
 	}
 
 	consumer.ValidityPeriods = sdk.NewAuthConsumerValidityPeriod(time.Now(), duration)
-	log.Info(ctx, "consumer %q IAT=%v Expiration=%v", consumer.ValidityPeriods.Latest().IssuedAt, consumer.ValidityPeriods.Latest().IssuedAt.Add(consumer.ValidityPeriods.Latest().Duration))
+	log.Info(ctx, "consumer %q IAT=%v Expiration=%v", consumer.ID, consumer.ValidityPeriods.Latest().IssuedAt, consumer.ValidityPeriods.Latest().IssuedAt.Add(consumer.ValidityPeriods.Latest().Duration))
 
 	return authentication.UpdateConsumer(ctx, tx, consumer)
 }
