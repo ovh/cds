49e78401398982d45082c7825d372c104f807a04
Marine <ooo-mama-ooo@hotmail.fr>
2021-07-05 15:31:08 +0200

    fix(migration) add missed ; on cdn 014 (#5870)
    Signed-off-by: Marine <marine.bal.1993@gmail.com>

--- engine/sql/cdn/014_cdn_type_index.sql
+++ engine/sql/cdn/014_cdn_type_index.sql
@@ -4,4 +4,4 @@ SELECT create_index('storage_unit_item', 'idx_storage_unit_type_item_unit_id', '
 
 -- +migrate Down
 DROP INDEX "idx_item_type_status";
-DROP INDEX "idx_storage_unit_type_item_unit_id"
+DROP INDEX "idx_storage_unit_type_item_unit_id";
