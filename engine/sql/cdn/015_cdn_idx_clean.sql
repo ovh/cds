b1a23796a82f69f51ed9b53dcc111e082a60a061
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2021-07-13 12:12:14 +0200

    refactor(cdn): clean some unused idx (#5886)
    * refactor(cdn): clean some unused idx
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
    
    * fix: idx_item_status_type
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
    
    * fix: cr
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

---
+++ engine/sql/cdn/015_cdn_idx_clean.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+DROP INDEX "idx_log_item";
+DROP INDEX "idx_log_project_key";
+DROP INDEX "idx_api_ref";
+DROP INDEX "idx_storage_unit_item_hash_locator";
+DROP INDEX "idx_item_status_type";
+
+-- +migrate Down
+CREATE INDEX idx_log_item ON item(type, (api_ref->>'job_id'), (api_ref->>'step_order'));
+CREATE INDEX idx_log_project_key ON item((api_ref->>'project_key'));
+CREATE INDEX idx_api_ref ON "item" USING GIN (api_ref);
+SELECT create_index('storage_unit_item', 'idx_storage_unit_item_hash_locator', 'id,hash_locator');
+SELECT create_index('item', 'idx_item_status_type', 'status,type');
