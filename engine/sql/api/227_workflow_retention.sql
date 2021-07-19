b936ee3b5a215515f15878b3418125d1950f4abe
Steven Guiheux <steven.guiheux@corp.ovh.com>
2021-07-19 10:44:54 +0200

    fix: db rollback

--- engine/sql/api/227_workflow_retention.sql
+++ engine/sql/api/227_workflow_retention.sql
@@ -2,4 +2,4 @@
 UPDATE workflow SET retention_policy = '' WHERE retention_policy='return (git_branch_exist == "false" and run_days_before < 2) or run_days_before < 365';
 
 -- +migrate Down
-UPDATE workflow SET retention_policy = 'return (git_branch_exist == "false" and run_days_before < 2) or (git_branch_exist == "true" and run_days_before < 365)' WHERE retention_policy = ''
+UPDATE workflow SET retention_policy = 'return (git_branch_exist == "false" and run_days_before < 2) or run_days_before < 365' WHERE retention_policy = ''
