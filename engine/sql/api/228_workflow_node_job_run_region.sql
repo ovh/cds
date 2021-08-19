5286553d0629d0027a27c9ca0abe9efa85150357
Richard LT <richard.le.terrier@gmail.com>
2021-08-19 14:41:00 +0200

    fix(sql): job run region index should not be unique (#5904)

--- engine/sql/api/228_workflow_node_job_run_region.sql
+++ engine/sql/api/228_workflow_node_job_run_region.sql
@@ -3,5 +3,5 @@ ALTER TABLE "workflow_node_run_job" ADD COLUMN IF NOT EXISTS "region" VARCHAR(25
 SELECT create_unique_index('workflow_node_run_job','IDX_WORKFLOW_NODE_RUN_JOB_REGION','region');
 
 -- +migrate Down
-DROP INDEX idx_workflow_node_run_job_region;
+DROP INDEX IF EXISTS idx_workflow_node_run_job_region;
 ALTER TABLE "workflow_node_run_job" DROP COLUMN IF EXISTS "region";
