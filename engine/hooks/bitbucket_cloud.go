3d2f3d78daa7c7c404939d2009beecf7ecf2580e
francois  samin <francois.samin@corp.ovh.com>
2021-09-24 11:13:48 +0200

    fix(hooks): remove deprecated branch deletion from hooks
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- engine/hooks/bitbucket_cloud.go
+++ engine/hooks/bitbucket_cloud.go
@@ -17,9 +17,6 @@ func (s *Service) generatePayloadFromBitbucketCloudRequest(ctx context.Context, 
 		return nil, sdk.WrapError(err, "unable ro read bitbucket request: %s", string(t.WebHook.RequestBody))
 	}
 
-	projectKey := t.Config["project"].Value
-	workflowName := t.Config["workflow"].Value
-
 	payload := make(map[string]interface{})
 	payload[GIT_EVENT] = event
 	getVariableFromBitbucketCloudAuthor(payload, request.Actor)
@@ -27,23 +24,6 @@ func (s *Service) generatePayloadFromBitbucketCloudRequest(ctx context.Context, 
 	getPayloadStringVariable(ctx, payload, request)
 
 	for _, pushChange := range request.Push.Changes {
-		if pushChange.Closed {
-			if pushChange.Old.Type == "branch" {
-				if err := s.enqueueBranchDeletion(projectKey, workflowName, strings.TrimPrefix(pushChange.Old.Name, "refs/heads/")); err != nil {
-					log.Error(ctx, "cannot enqueue branch deletion: %v", err)
-				}
-			}
-			continue
-		}
-
-		if pushChange.New.Type == "branch" {
-			branch := strings.TrimPrefix(pushChange.New.Name, "refs/heads/")
-			if err := s.stopBranchDeletionTask(ctx, branch); err != nil {
-				log.Error(ctx, "cannot stop branch deletion task for branch %s : %v", branch, err)
-			}
-
-		}
-
 		payloadChange := make(map[string]interface{})
 		for k, v := range payload {
 			payloadChange[k] = v
