46f2c9fe1c336125b82540ea5e19e124ccf9568e
francois  samin <francois.samin@corp.ovh.com>
2021-10-01 11:45:21 +0200

    feat(cli): delete workflow with dependencies
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- cli/cdsctl/workflow_delete.go
+++ cli/cdsctl/workflow_delete.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/ovh/cds/cli"
 	"github.com/ovh/cds/sdk"
@@ -15,10 +17,24 @@ var workflowDeleteCmd = cli.Command{
 		{Name: _ProjectKey},
 		{Name: _WorkflowName},
 	},
+	Flags: []cli.Flag{
+		{
+			Name:      "with-dependencies",
+			ShortHand: "d",
+			Usage:     "delete and clean workflow dependencies",
+			Type:      cli.FlagBool,
+		},
+	},
 }
 
 func workflowDeleteRun(v cli.Values) error {
-	err := client.WorkflowDelete(v.GetString(_ProjectKey), v.GetString(_WorkflowName))
+	mod := func(r *http.Request) {
+		q := r.URL.Query()
+		b := v.GetBool("with-dependencies")
+		q.Set("withDependencies", strconv.FormatBool(b))
+		r.URL.RawQuery = q.Encode()
+	}
+	err := client.WorkflowDelete(v.GetString(_ProjectKey), v.GetString(_WorkflowName), mod)
 	if err != nil && v.GetBool("force") && sdk.ErrorIs(err, sdk.ErrNotFound) {
 		fmt.Println(err.Error())
 		os.Exit(0)
