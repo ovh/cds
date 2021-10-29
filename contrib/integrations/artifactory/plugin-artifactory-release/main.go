1c2c94734be9ca39062aba1fd0d0858bf5afc85c
Steven Guiheux <steven.guiheux@corp.ovh.com>
2021-10-29 09:25:48 +0200

    fix(contrib): use markdown as default release note format

--- contrib/integrations/artifactory/plugin-artifactory-release/main.go
+++ contrib/integrations/artifactory/plugin-artifactory-release/main.go
@@ -199,7 +199,7 @@ func (e *artifactoryReleasePlugin) createReleaseBundle(distriClient *distributio
 	}
 	if !exist {
 		params.ReleaseNotes = releaseNote
-		params.ReleaseNotesSyntax = "markdown("
+		params.ReleaseNotesSyntax = "markdown"
 
 		paramsBuild := fmt.Sprintf("%s/%s", strings.Replace(buildInfoName, "/", "\\/", -1), version)
 
