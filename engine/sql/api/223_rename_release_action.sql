472d261bb5b9eec2827fe58dfceebf949edc9186
Marine <marine.bal.1993@gmail.com>
2022-01-11 12:32:01 +0100

    Fix missed semilicon sql file api/223

--- engine/sql/api/223_rename_release_action.sql
+++ engine/sql/api/223_rename_release_action.sql
@@ -1,8 +1,5 @@
 -- +migrate Up
-UPDATE action SET name = 'ReleaseVCS' WHERE name = 'Release' and type = 'Builtin'
+UPDATE action SET name = 'ReleaseVCS' WHERE name = 'Release' and type = 'Builtin';
 
 -- +migrate Down
-UPDATE action SET name = 'Release' WHERE name = 'ReleaseVCS' and type = 'Builtin'
-
-
-
+UPDATE action SET name = 'Release' WHERE name = 'ReleaseVCS' and type = 'Builtin';
