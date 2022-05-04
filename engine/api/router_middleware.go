8abbc946f42e0f28a3d904e2c0e5009267580adb
richardlt <richard.le.terrier@gmail.com>
2022-05-04 10:51:20 +0200

    feat(api): maintenance allow worker and service calls

--- engine/api/router_middleware.go
+++ engine/api/router_middleware.go
@@ -9,8 +9,11 @@ import (
 )
 
 func (api *API) maintenanceMiddleware(ctx context.Context, w http.ResponseWriter, req *http.Request, rc *service.HandlerConfig) (context.Context, error) {
-	if !isMaintainer(ctx) && api.Maintenance && !rc.MaintenanceAware && rc.Method != http.MethodGet {
-		return ctx, sdk.WrapError(sdk.ErrServiceUnavailable, "CDS Maintenance ON")
+	if api.Maintenance && !rc.MaintenanceAware && rc.Method != http.MethodGet {
+		isAllowed := isMaintainer(ctx) || isService(ctx) || isWorker(ctx)
+		if !isAllowed {
+			return ctx, sdk.WrapError(sdk.ErrServiceUnavailable, "CDS Maintenance ON")
+		}
 	}
 	return ctx, nil
 }
