20ff0633d20c6cc84cce4e53a4fd671075d47b2d
Steven Guiheux <steven.guiheux@corp.ovh.com>
2022-07-19 17:58:17 +0200

    fix: unit test

--- engine/api/repository/dao_vcs_project_repository_analyze.go
+++ engine/api/repository/dao_vcs_project_repository_analyze.go
@@ -52,11 +52,11 @@ func UpdateAnalyze(ctx context.Context, db gorpmapper.SqlExecutorWithTx, analyze
 }
 
 func getAnalyze(ctx context.Context, db gorp.SqlExecutor, query gorpmapping.Query) (*sdk.ProjectRepositoryAnalyze, error) {
-	var dbData *dbProjectRepositoryAnalyze
-	if _, err := gorpmapping.Get(ctx, db, query, dbData); err != nil {
+	var dbData dbProjectRepositoryAnalyze
+	if _, err := gorpmapping.Get(ctx, db, query, &dbData); err != nil {
 		return nil, err
 	}
-	if dbData == nil {
+	if dbData.ID == "" {
 		return nil, sdk.ErrNotFound
 	}
 	isValid, err := gorpmapping.CheckSignature(dbData, dbData.Signature)
