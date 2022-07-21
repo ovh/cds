881bcb15d87b74052cdce1795ae8dc16daa91b21
Steven Guiheux <steven.guiheux@corp.ovh.com>
2022-07-21 10:31:40 +0200

    fix: add gpgkey on gitea user

--- sdk/cdsclient/client_project_repository.go
+++ sdk/cdsclient/client_project_repository.go
@@ -40,7 +40,6 @@ func (c *client) ProjectRepositoryAnalyze(ctx context.Context, analyze sdk.Analy
 
 func (c *client) ProjectRepositoryAnalyzeList(ctx context.Context, projectKey string, vcsIdentifier string, repositoryIdentifier string) ([]sdk.ProjectRepositoryAnalyze, error) {
 	path := fmt.Sprintf("/v2/project/%s/vcs/%s/repository/%s/analyze", projectKey, url.PathEscape(vcsIdentifier), url.PathEscape(repositoryIdentifier))
-	fmt.Println(path)
 	var analyzes []sdk.ProjectRepositoryAnalyze
 	_, err := c.GetJSON(ctx, path, &analyzes)
 	return analyzes, err
