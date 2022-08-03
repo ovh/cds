0025978d34f76b579435e691ad46ace597d3dbf0
François Samin <francois.samin@corp.ovh.com>
2022-08-03 15:36:18 +0200

    fix(engine): json encryption key rollover (#6247)
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- engine/gorpmapper/dao.go
+++ engine/gorpmapper/dao.go
@@ -78,6 +78,10 @@ func (m *Mapper) UpdateColumns(db gorp.SqlExecutor, i interface{}, columnFilter 
 			hasPlaceHolder = true
 			break
 		}
+		if field.Kind() == reflect.Struct && field.IsZero() {
+			hasPlaceHolder = true
+			break
+		}
 	}
 
 	// If the data has encrypted data
@@ -99,8 +103,9 @@ func (m *Mapper) UpdateColumns(db gorp.SqlExecutor, i interface{}, columnFilter 
 		for _, f := range mapping.EncryptedFields {
 			// Reset the field to the decrypted value if the value is set to the placeholder
 			field := val.FieldByName(f.Name)
-			if field.Interface() == sdk.PasswordPlaceholder {
-				oldVal := valTuple.FieldByName(f.Name)
+			oldVal := valTuple.FieldByName(f.Name)
+
+			if field.Interface() == sdk.PasswordPlaceholder || field.Kind() == reflect.Struct && field.IsZero() {
 				field.Set(oldVal)
 			}
 		}
