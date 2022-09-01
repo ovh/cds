f9424d1bceab10c03a821d04d353ac54d5b2fafb
richardlt <richard.le.terrier@gmail.com>
2022-09-01 16:00:34 +0200

    fix: cr
    Signed-off-by: richardlt <richard.le.terrier@gmail.com>

--- engine/sql/api/251_clean_workflow_run_secrets.sql
+++ engine/sql/api/251_clean_workflow_run_secrets.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-SELECT create_index('workflow_run', 'idx_workflow_run_start', 'start');
+SELECT create_index('workflow_run', 'idx_workflow_run_start', 'read_only,start');
 
 -- +migrate Down
 DROP INDEX idx_workflow_run_start;
