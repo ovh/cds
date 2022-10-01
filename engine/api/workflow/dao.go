a3f62f0689badb8a947ae9a0a5b787e44abe67f8
chavacava <salvadorcavadini+github@gmail.com>
2022-10-01 05:18:25 +0000

    fix #6313
    Signed-off-by: chavacava <salvadorcavadini+github@gmail.com>

--- engine/api/workflow/dao.go
+++ engine/api/workflow/dao.go
@@ -1085,7 +1085,7 @@ func checkHooks(ctx context.Context, db gorp.SqlExecutor, w *sdk.Workflow, n *sd
 			h2 := n.Hooks[j]
 			if i != j && h.Ref() == h2.Ref() {
 				log.ErrorWithStackTrace(ctx, sdk.NewErrorFrom(sdk.ErrWrongRequest, "invalid workflow: duplicate hook %s", model.Name))
-				if !sdk.IsInInt64Array(int64(i), duplicateHookIdx) && !sdk.IsInInt64Array(int64(i), duplicateHookIdx) {
+				if !sdk.IsInInt64Array(int64(i), duplicateHookIdx) && !sdk.IsInInt64Array(int64(j), duplicateHookIdx) {
 					duplicateHookIdx = append(duplicateHookIdx, int64(j))
 				}
 			}
