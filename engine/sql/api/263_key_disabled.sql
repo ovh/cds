9e08c1b6cd68ca3112dba8bd96e43b36dbf6b04a
François Samin <francois.samin@corp.ovh.com>
2023-01-24 17:11:18 +0100

    Update engine/sql/api/263_key_disabled.sql
    Co-authored-by: Guiheux Steven <steven.guiheux@corp.ovh.com>

--- engine/sql/api/263_key_disabled.sql
+++ engine/sql/api/263_key_disabled.sql
@@ -4,4 +4,4 @@ ALTER TABLE project_key ADD COLUMN disabled BOOLEAN NOT NULL DEFAULT FALSE;
 UPDATE project_key SET disabled = false;
 
 -- +migrate Down
-ALTER TABLE project_key DROP COLUMN BOOLEAN;
+ALTER TABLE project_key DROP COLUMN disabled;
