a7b828f12e74cb5877fa8654c33a48a2c64fe5b7
richardlt <richard.le.terrier@gmail.com>
2023-02-20 14:21:50 +0100

    feat(hatchery:swarm): compute worker cpu/memory metrics
    Signed-off-by: richardlt <richard.le.terrier@gmail.com>

--- engine/hatchery/swarm/swarm.go
+++ engine/hatchery/swarm/swarm.go
@@ -54,6 +54,10 @@ func (h *HatcherySwarm) InitHatchery(ctx context.Context) error {
 		return err
 	}
 
+	if err := h.InitWorkersMetrics(ctx); err != nil {
+		return err
+	}
+
 	h.dockerClients = map[string]*dockerClient{}
 
 	if len(h.Config.DockerEngines) == 0 {
@@ -192,6 +196,10 @@ func (h *HatcherySwarm) InitHatchery(ctx context.Context) error {
 		h.routines(ctx)
 	})
 
+	h.GoRoutines.Run(ctx, "worker-metrics", func(ctx context.Context) {
+		h.StartWorkerMetricsRoutine(ctx)
+	})
+
 	return nil
 }
 
@@ -328,6 +336,7 @@ func (h *HatcherySwarm) SpawnWorker(ctx context.Context, spawnArgs hatchery.Spaw
 					LabelServiceWorker: spawnArgs.WorkerName,
 					LabelServiceName:   serviceName,
 					LabelHatchery:      h.Config.Name,
+					LabelJobID:         fmt.Sprintf("%d", spawnArgs.JobID),
 				}
 
 				if spawnArgs.JobID > 0 {
@@ -378,6 +387,7 @@ func (h *HatcherySwarm) SpawnWorker(ctx context.Context, spawnArgs hatchery.Spaw
 		LabelWorkerName:         spawnArgs.WorkerName,
 		LabelWorkerRequirements: strings.Join(services, ","),
 		LabelHatchery:           h.Config.Name,
+		LabelJobID:              fmt.Sprintf("%d", spawnArgs.JobID),
 	}
 
 	// Add new options on hatchery swarm to allow advanced docker option such as addHost, priviledge, port mapping and so one: #4594
