81d3b8b8b84fe7eefdb5dc04fe8ff4e049851aed
Steven Guiheux <steven.guiheux@corp.ovh.com>
2023-04-11 14:25:26 +0200

    fix(sql): add default value

--- engine/sql/api/266_plugin.sql
+++ engine/sql/api/266_plugin.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-ALTER TABLE grpc_plugin ADD COLUMN inputs JSONB;
+ALTER TABLE grpc_plugin ADD COLUMN inputs JSONB DEFAULT '{}';
 
 -- +migrate Down
 ALTER TABLE grpc_plugin DROP COLUMN inputs;
