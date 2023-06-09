be2834db877b3141561facec15accf17d4f70d8e
Steven Guiheux <steven.guiheux@corp.ovh.com>
2023-06-09 14:59:51 +0200

    fix: wrong change

--- engine/sql/api/269_rbac_workflow.sql
+++ engine/sql/api/269_rbac_workflow.sql
@@ -13,7 +13,7 @@ CREATE TABLE rbac_workflow (
 );
 CREATE INDEX rbac_workflow_name_gin_idx ON rbac_workflow USING gin (workflows);
 SELECT create_foreign_key_idx_cascade('FK_rbac_workflow', 'rbac_workflow', 'rbac', 'rbac_id', 'id');
-SELECT create_foreign_key_idx_cascade('FK_rbac_workflow_project', 'rbac_workflow', 'project_key', 'project', 'projectkey');
+SELECT create_foreign_key_idx_cascade('FK_rbac_workflow_project', 'rbac_workflow', 'project', 'project_key', 'projectkey');
 SELECT create_index('rbac_workflow', 'idx_rbac_workflow_project_role', 'role');
 
 CREATE TABLE rbac_workflow_users
