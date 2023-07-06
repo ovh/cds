f9c0e1405f34d66c2be30fb116eb4806d9b23f5f
François Samin <francois.samin@corp.ovh.com>
2023-07-06 16:49:45 +0200

    feat(api): artifact managed in artifactory are signed by CDS API

--- tests/fixtures/ITSCWRKFLW15/SimplePlugin.go
+++ tests/fixtures/ITSCWRKFLW15/SimplePlugin.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/ovh/cds/sdk/grpcplugin/integrationplugin"
 	"strconv"
+
+	"github.com/ovh/cds/sdk/grpcplugin/integrationplugin"
 
 	"github.com/golang/protobuf/ptypes/empty"
 
