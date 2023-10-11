e728821acdf26bfb4218af37d05c308640f565c8
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2023-10-11 13:48:22 +0200

    fix(engine): do not force register on update binary (#6634)
    * fix(engine): do not force register on update binary
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/api/workermodel/registration.go
+++ engine/api/workermodel/registration.go
@@ -25,17 +25,6 @@ func ComputeRegistrationNeeds(db gorp.SqlExecutor, allBinaryReqs sdk.Requirement
 
 	for _, r := range reqs {
 		switch r.Type {
-		case sdk.BinaryRequirement:
-			exist := false
-			for _, e := range allBinaryReqs {
-				if e.Value == r.Value {
-					exist = true
-					break
-				}
-			}
-			if !exist {
-				return updateAllToCheckRegistration(db)
-			}
 		case sdk.OSArchRequirement:
 			nbOSArchReq++
 		case sdk.ModelRequirement:
@@ -60,21 +49,6 @@ func ComputeRegistrationNeeds(db gorp.SqlExecutor, allBinaryReqs sdk.Requirement
 		return sdk.NewErrorFrom(sdk.ErrWrongRequest, "invalid region requirement usage")
 	}
 
-	return nil
-}
-
-// updateAllToCheckRegistration is like need_registration but without exclusive mode
-func updateAllToCheckRegistration(db gorp.SqlExecutor) error {
-	query := `UPDATE worker_model SET check_registration = $1`
-	res, err := db.Exec(query, true)
-	if err != nil {
-		return sdk.WithStack(err)
-	}
-	rows, err := res.RowsAffected()
-	if err != nil {
-		return sdk.WithStack(err)
-	}
-	log.Debug(context.Background(), "updateAllToCheckRegistration> %d worker model(s) check registration", rows)
 	return nil
 }
 
