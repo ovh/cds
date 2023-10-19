b12425c48aeb7cbf7ecd16de57ac7378c1a68476
Steven Guiheux <steven.guiheux@corp.ovh.com>
2023-10-19 13:56:03 +0200

    fix: drop index before create

--- engine/sql/api/279_workflow_run_job_matrix.sql
+++ engine/sql/api/279_workflow_run_job_matrix.sql
@@ -1,8 +1,10 @@
 -- +migrate Up
 ALTER TABLE v2_workflow_run_job ADD COLUMN "matrix" TEXT;
-select create_unique_index('v2_workflow_run_job', 'IDX_workflow_run_job_unq', 'workflow_run_id,job_id,matrix,run_number,run_attempt');
+drop index idx_workflow_run_job_unq;
+select create_unique_index('v2_workflow_run_job', 'idx_workflow_run_job_unq', 'workflow_run_id,job_id,matrix,run_number,run_attempt');
 
 -- +migrate Down
 ALTER TABLE v2_workflow_run_job DROP COLUMN "matrix";
-select create_unique_index('v2_workflow_run_job', 'IDX_workflow_run_job_unq', 'workflow_run_id,job_id,run_number,run_attempt');
+drop index idx_workflow_run_job_unq;
+select create_unique_index('v2_workflow_run_job', 'idx_workflow_run_job_unq', 'workflow_run_id,job_id,run_number,run_attempt');
 
