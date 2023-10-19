80c4cb4f073e4e9d077639df6fac324affdd3ddd
Steven Guiheux <steven.guiheux@corp.ovh.com>
2023-10-19 11:38:52 +0200

    fix: drop index

--- engine/sql/api/279_workflow_run_job_matrix.sql
+++ engine/sql/api/279_workflow_run_job_matrix.sql
@@ -1,5 +1,6 @@
 -- +migrate Up
 ALTER TABLE v2_workflow_run_job ADD COLUMN "matrix" TEXT;
+DROP INDEX idx_workflow_run_job_unq;
 
 -- +migrate Down
 ALTER TABLE v2_workflow_run_job DROP COLUMN "matrix";
