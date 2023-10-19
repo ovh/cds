81067cee3a91b6def59a26ba05596566607e2b91
Steven Guiheux <steven.guiheux@corp.ovh.com>
2023-10-19 11:44:40 +0200

    fix: unique index

--- engine/sql/api/279_workflow_run_job_matrix.sql
+++ engine/sql/api/279_workflow_run_job_matrix.sql
@@ -1,6 +1,8 @@
 -- +migrate Up
 ALTER TABLE v2_workflow_run_job ADD COLUMN "matrix" TEXT;
-DROP INDEX idx_workflow_run_job_unq;
+select create_unique_index('v2_workflow_run_job', 'IDX_workflow_run_job_unq', 'workflow_run_id,job_id,matrix,run_number,run_attempt');
 
 -- +migrate Down
 ALTER TABLE v2_workflow_run_job DROP COLUMN "matrix";
+select create_unique_index('v2_workflow_run_job', 'IDX_workflow_run_job_unq', 'workflow_run_id,job_id,run_number,run_attempt');
+
