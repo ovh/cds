93ad86e0e98ea21a15ae8f010cb9b5e185397c76
Lars Lehtonen <lars.lehtonen@gmail.com>
2024-01-08 11:17:55 -0800

    fix(engine): dropped filepath.Walk() error
    Signed-off-by: Lars Lehtonen <lars.lehtonen@gmail.com>

--- engine/cdn/storage/local/buffer.go
+++ engine/cdn/storage/local/buffer.go
@@ -66,6 +66,9 @@ func (b *Buffer) BufferType() storage.CDNBufferType {
 func (b *Buffer) ResyncWithDatabase(ctx context.Context, db gorp.SqlExecutor, t sdk.CDNItemType, dryRun bool) {
 	root := fmt.Sprintf("%s/%s", b.config.Path, string(t))
 	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 		if info == nil {
 			return nil
 		}
