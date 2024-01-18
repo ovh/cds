0ca42bf830a2ba2da1db4bb0d8ff0aa19666c9e6
Steven Guiheux <steven.guiheux@corp.ovh.com>
2024-01-18 16:01:21 +0000

    fix: migrate contexts from text to jsonb

---
+++ engine/sql/api/289_v2_run_contexts.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+CREATE TABLE old_v2_workflow_run_context AS select id, contexts from v2_workflow_run;
+ALTER TABLE v2_workflow_run DROP COLUMN contexts;
+ALTER TABLE v2_workflow_run ADD COLUMN contexts JSONB;
+
+-- +migrate Down
+ALTER TABLE v2_workflow_run DROP COLUMN contexts;
+ALTER TABLE v2_workflow_run ADD COLUMN contexts TEXT;
+
+UPDATE v2_workflow_run SET contexts = old_v2_workflow_run_context.contexts
+FROM old_v2_workflow_run_context WHERE v2_workflow_run.id = old_v2_workflow_run_context.id;
+
+DROP TABLE old_v2_workflow_run_context;
