4a79cf536f3bdf65c5c4da54a448669cb6007ed8
richardlt <richard.le.terrier@gmail.com>
2024-01-25 15:12:42 +0000

    fix: cr
    Signed-off-by: richardlt <richard.le.terrier@gmail.com>

--- engine/sql/api/291_v2_run_repository_info.sql
+++ engine/sql/api/291_v2_run_repository_info.sql
@@ -1,7 +1,13 @@
 -- +migrate Up
 ALTER TABLE v2_workflow_run ADD COLUMN vcs_server VARCHAR(255) NOT NULL DEFAULT '';
 ALTER TABLE v2_workflow_run ADD COLUMN repository VARCHAR(255) NOT NULL DEFAULT '';
+SELECT create_index('v2_workflow_run', 'idx_v2_workflow_run_username', 'username');
+SELECT create_index('v2_workflow_run', 'idx_v2_workflow_run_workflow_name', 'workflow_name');
+CREATE INDEX idx_v2_workflow_run_git_ref ON v2_workflow_run((contexts -> 'git' ->> 'ref'));
 
 -- +migrate Down
 ALTER TABLE v2_workflow_run DROP COLUMN vcs_server;
 ALTER TABLE v2_workflow_run DROP COLUMN repository;
+DROP INDEX IF EXISTS idx_v2_workflow_run_username;
+DROP INDEX IF EXISTS idx_v2_workflow_run_workflow_name;
+DROP INDEX IF EXISTS idx_v2_workflow_run_git_ref;
