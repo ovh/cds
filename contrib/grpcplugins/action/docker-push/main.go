e8ff46de4a406e5fc5aa7cf2bf549672d9104ad5
francois.samin <francois.samin@corp.ovh.com>
2024-02-02 08:11:08 +0000

    fix: cr

--- contrib/grpcplugins/action/docker-push/main.go
+++ contrib/grpcplugins/action/docker-push/main.go
@@ -54,6 +54,8 @@ func (actPlugin *dockerPushPlugin) Run(ctx context.Context, q *actionplugin.Acti
 	registry := q.GetOptions()["registry"]
 	auth := q.GetOptions()["registryAuth"]
 
+	tags = strings.Replace(tags, " ", ",", -1) // If tags are separated by <space>
+	tags = strings.Replace(tags, ";", ",", -1) // If tags are separated by <semicolon>
 	tagSlice := strings.Split(tags, ",")
 
 	if err := actPlugin.perform(ctx, image, tagSlice, registry, auth); err != nil {
@@ -76,9 +78,6 @@ type img struct {
 func (actPlugin *dockerPushPlugin) perform(ctx context.Context, image string, tags []string, registry, registryAuth string) error {
 	if image == "" {
 		return sdk.Errorf("wrong usage: <image> parameter should not be empty")
-	}
-	if len(tags) == 0 {
-		return sdk.Errorf("wrong usage: <tags> parameter should not be empty")
 	}
 
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -120,8 +119,12 @@ func (actPlugin *dockerPushPlugin) perform(ctx context.Context, image string, ta
 		return sdk.Errorf("image %q not found", image)
 	}
 
+	if len(tags) == 0 { // If no tag is provided, keep the actual tag
+		tags = []string{imgFound.tag}
+	}
+
 	for _, tag := range tags {
-		result, d, err := actPlugin.performImage(ctx, cli, image, imgFound, registry, registryAuth, tag)
+		result, d, err := actPlugin.performImage(ctx, cli, image, imgFound, registry, registryAuth, strings.TrimSpace(tag))
 		if err != nil {
 			grpcplugins.Error(err.Error())
 			return err
@@ -160,7 +163,7 @@ func (actPlugin *dockerPushPlugin) performImage(ctx context.Context, cli *client
 	result := response.RunResult
 
 	var destination string
-	// Upload the file to an artifactory or CDN
+	// Upload the file to an artifactory or the docker registry
 	switch {
 	case result.ArtifactManagerIntegrationName != nil:
 		integration, err := grpcplugins.GetIntegrationByName(ctx, &actPlugin.Common, *response.RunResult.ArtifactManagerIntegrationName)
@@ -187,8 +190,10 @@ func (actPlugin *dockerPushPlugin) performImage(ctx context.Context, cli *client
 			HumanCreated: img.created,
 		}
 
-		if err := cli.ImageTag(ctx, img.imageID, destination); err != nil {
-			return nil, time.Since(t0), errors.Errorf("unable to tag %q to %q: %v", source, destination, err)
+		if tag != img.tag { // if the image already has the right tag, nothing to do
+			if err := cli.ImageTag(ctx, img.imageID, destination); err != nil {
+				return nil, time.Since(t0), errors.Errorf("unable to tag %q to %q: %v", source, destination, err)
+			}
 		}
 
 		auth := types.AuthConfig{
@@ -251,10 +256,19 @@ func (actPlugin *dockerPushPlugin) performImage(ctx context.Context, cli *client
 
 	default:
 		// Push on the registry set as parameter
-		destination = registry + "/" + img.repository + ":" + tag
+		if registry == "" && registryAuth == "" {
+			return nil, time.Since(t0), errors.New("wrong usage: <registry> and <registryAuth> parameters should not be both empty")
+		}
 
-		if err := cli.ImageTag(ctx, img.imageID, destination); err != nil {
-			return nil, time.Since(t0), errors.Errorf("unable to tag %q to %q: %v", source, destination, err)
+		destination = img.repository + ":" + tag
+		if registry != "" {
+			destination = registry + "/" + destination
+		}
+
+		if tag != img.tag { // if the image already has the right tag, nothing to do
+			if err := cli.ImageTag(ctx, img.imageID, destination); err != nil {
+				return nil, time.Since(t0), errors.Errorf("unable to tag %q to %q: %v", source, destination, err)
+			}
 		}
 
 		output, err := cli.ImagePush(ctx, destination, types.ImagePushOptions{RegistryAuth: registryAuth})
