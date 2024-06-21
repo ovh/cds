e79b225ef4ea13ec022a5ca02aa6fef0513a0445
François Samin <francois.samin@corp.ovh.com>
2024-06-21 08:58:16 +0200

    fix(sdk): glob (#7008)
    Signed-off-by: francois.samin <francois.samin@ovhcloud.com>

--- sdk/glob/main_test.go
+++ sdk/glob/main_test.go
@@ -10,10 +10,53 @@ import (
 )
 
 func TestGlob(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
 	pattern := "path/to/**/* !path/to/**/*.tmp"
 	result, err := glob.Glob(os.DirFS("tests/"), "fixtures", pattern)
 	require.NoError(t, err)
 	require.Equal(t, "path/to/artifacts/bar, path/to/artifacts/foo, path/to/results/foo.bin", result.String())
+}
+
+func TestGlobWitDoubleStar0(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	file := "bar"
+	pattern := "**/*"
+	g := glob.New(pattern)
+	r, e := g.MatchString(file)
+	require.NoError(t, e)
+	require.NotNil(t, r)
+	require.Equal(t, file, r.String())
+	require.Equal(t, "bar", r.Result)
+}
+
+func TestGlobWitDoubleStar1(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	file := "path/to/artifacts/bar"
+	pattern := "path/to/**/*"
+	g := glob.New(pattern)
+	r, e := g.MatchString(file)
+	require.NoError(t, e)
+	require.NotNil(t, r)
+	require.Equal(t, file, r.String())
+	require.Equal(t, "artifacts/bar", r.Result)
 }
 
 func TestGlobWithDot(t *testing.T) {
@@ -132,4 +175,196 @@ func TestGlobWithStar6(t *testing.T) {
 	require.NoError(t, e)
 	require.NotNil(t, r)
 	require.Equal(t, "foooo.txt", r.Result)
+}
+
+func TestGlobWithDoubleStar(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	file := "path/to/artifact/foooo.txt"
+	pattern := "path/to/**/foo*.txt"
+	g := glob.New(pattern)
+	r, e := g.MatchString(file)
+	require.NoError(t, e)
+	require.NotNil(t, r)
+	require.Equal(t, "artifact/foooo.txt", r.Result)
+}
+
+func TestGlobWithDoubleStar2(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	file := "path/to/artifact/a/foooo.txt"
+	pattern := "path/to/**/foo*.txt"
+	g := glob.New(pattern)
+	r, e := g.MatchString(file)
+	require.NoError(t, e)
+	require.NotNil(t, r)
+	require.Equal(t, "artifact/a/foooo.txt", r.Result)
+}
+
+func TestGlobDoubleStarMatchString0(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("**/newfile")
+	result, err := g.MatchString("root/newfile")
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "root/newfile", result.String())
+}
+
+func TestGlobDoubleStarMatchString(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("**/newfile")
+	result, err := g.MatchString("root/app/sub/newfile")
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "root/app/sub/newfile", result.String())
+}
+
+func TestGlobDoubleStarMatchString2(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("**/cd/rtgc")
+	result, err := g.MatchString("root/app/sub/cd/rtgc")
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "root/app/sub/cd/rtgc", result.String())
+	t.Logf("result= %s", result.String())
+
+}
+
+func TestGlobDoubleStarMatchString3(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("**/rtgc")
+	result, err := g.MatchString("root/app/sub/cd/rtgc")
+	t.Logf("result= %s", result.String())
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "root/app/sub/cd/rtgc", result.String())
+}
+
+func TestGlobDoubleStarMatchString4(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("root/**/rtgc")
+	result, err := g.MatchString("root/app/sub/cd/rtgc")
+	t.Logf("result= %s", result.String())
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "root/app/sub/cd/rtgc", result.String())
+}
+
+func TestGlobDoubleStarMatchString5(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("**/cd/rtgc")
+	result, err := g.MatchString("root/app/sub/cc/cd/rtgc")
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "root/app/sub/cc/cd/rtgc", result.String())
+	t.Logf("result= %s", result.String())
+
+}
+
+func TestGlobDoubleStarMatchString6(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("path/**/[abc]rtifac?/*")
+	result, err := g.MatchString("path/to/artifact/foo")
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "path/to/artifact/foo", result.String())
+	t.Logf("result= %s", result.String())
+}
+
+func TestGlobDoubleStarMatchString7(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("path/**/[abc]rtifac?/*")
+	result, err := g.MatchString("path/to/my/artifact/foo")
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "path/to/my/artifact/foo", result.String())
+	t.Logf("result= %s", result.String())
+}
+
+func TestSquareBracket(t *testing.T) {
+	glob.DebugEnabled = true
+	glob.DebugFunc = func(a ...any) (n int, err error) {
+		t.Log(a...)
+		return len(a), nil
+	}
+	log.Factory = log.NewTestingWrapper(t)
+
+	g := glob.New("[abc]rtifac?/*")
+	result, err := g.MatchString("artifact/foo")
+	t.Logf("err= %v", err)
+
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, "artifact/foo", result.String())
+	t.Logf("result= %s", result.String())
 }
