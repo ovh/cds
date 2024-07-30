654ce5720bc51e1e9b47c162aa02161ac65a6486
Steven Guiheux <steven.guiheux@corp.ovh.com>
2024-07-30 11:48:31 +0000

    fix: on delete

--- engine/sql/api/303_v2_entity_user.sql
+++ engine/sql/api/303_v2_entity_user.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE entity ADD COLUMN user_id VARCHAR(36) NULL;
-ALTER TABLE entity ADD CONSTRAINT fk_entity_user FOREIGN KEY (user_id) REFERENCES authentified_user (id);
+ALTER TABLE entity ADD CONSTRAINT fk_entity_user FOREIGN KEY (user_id) REFERENCES authentified_user (id) ON DELETE SET NULL;
 
 -- +migrate Down
 ALTER TABLE entity DROP COLUMN user_id;
