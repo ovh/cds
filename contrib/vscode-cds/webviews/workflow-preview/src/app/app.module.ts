553b98395d8129e7eab2d846e74246b3b575d0b4
richardlt <richard.le.terrier@gmail.com>
2024-08-20 09:22:26 +0000

    fix(ui): restart job icons
    Signed-off-by: richardlt <richard.le.terrier@gmail.com>

--- contrib/vscode-cds/webviews/workflow-preview/src/app/app.module.ts
+++ contrib/vscode-cds/webviews/workflow-preview/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { WorkflowGraphModule} from 'workflow-graph';
+import { WorkflowGraphModule } from 'workflow-graph';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CaretRightOutline, CaretDownOutline, UserOutline } from '@ant-design/icons-angular/icons';
 import { IconDefinition } from '@ant-design/icons-angular';
 
-const icons: IconDefinition[] = [ CaretRightOutline, CaretDownOutline, UserOutline ];
+const icons: IconDefinition[] = [CaretRightOutline, CaretDownOutline, UserOutline];
+
 @NgModule({
   declarations: [
     AppComponent,
