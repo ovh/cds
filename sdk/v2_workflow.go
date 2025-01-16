fc24ea590e4473f2e0973ce51660918a8d78bf30
Steven Guiheux <steven.guiheux@corp.ovh.com>
2025-01-16 09:38:38 +0000

    fix: remove unused code

--- sdk/v2_workflow.go
+++ sdk/v2_workflow.go
@@ -362,11 +362,6 @@ type V2JobGateInput struct {
 	Description string      `json:"description,omitempty"`
 }
 
-type V2JobGateValues struct {
-	Multiple bool          `json:"multiple"`
-	Values   []interface{} `json:"values"`
-}
-
 type V2JobGateReviewers struct {
 	Groups []string `json:"groups,omitempty"`
 	Users  []string `json:"users,omitempty"`
