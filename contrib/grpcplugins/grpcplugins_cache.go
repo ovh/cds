7afc488a82bdc5dea92b9748b8c19f70efe6a462
Steven Guiheux <steven.guiheux@corp.ovh.com>
2025-01-28 13:36:10 +0000

    fix: use sdk function

--- contrib/grpcplugins/grpcplugins_cache.go
+++ contrib/grpcplugins/grpcplugins_cache.go
@@ -1,12 +1,9 @@
 package grpcplugins
 
 import (
-	"archive/tar"
 	"bufio"
-	"compress/gzip"
 	"context"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -107,51 +104,5 @@ func Untar(dst string, filePath string) error {
 	if err != nil {
 		return err
 	}
-	gzr, err := gzip.NewReader(bufio.NewReader(file))
-	if err != nil {
-		return err
-	}
-	defer gzr.Close()
-
-	tr := tar.NewReader(gzr)
-	for {
-		header, err := tr.Next()
-		switch {
-		// if no more files are found return
-		case err == io.EOF:
-			return nil
-		// return any other error
-		case err != nil:
-			return err
-		// if the header is nil, just skip it (not sure how this happens)
-		case header == nil:
-			continue
-		}
-
-		// the target location where the dir/file should be created
-		target := filepath.Join(dst, header.Name)
-		// check the file type
-		switch header.Typeflag {
-		// if its a dir and it doesn't exist create it
-		case tar.TypeDir:
-			if _, err := os.Stat(target); err != nil {
-				if err := os.MkdirAll(target, 0755); err != nil {
-					return err
-				}
-			}
-		// if it's a file create it
-		case tar.TypeReg:
-			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
-			if err != nil {
-				return err
-			}
-			// copy over contents
-			if _, err := io.Copy(f, tr); err != nil {
-				return err
-			}
-			if err := f.Close(); err != nil {
-				return err
-			}
-		}
-	}
+	return sdk.UntarGz(afero.NewOsFs(), dst, bufio.NewReader(file))
 }
