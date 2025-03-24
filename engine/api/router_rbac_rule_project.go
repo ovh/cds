1c9f2c422959d7b622a0af6f9ca36c74ae90d270
Steven Guiheux <steven.guiheux@corp.ovh.com>
2025-03-24 08:44:04 +0000

    fix(api): allow hooks to trigger analysis

--- engine/api/router_rbac_rule_project.go
+++ engine/api/router_rbac_rule_project.go
@@ -74,3 +74,10 @@ func (api *API) analysisRead(ctx context.Context, vars map[string]string) error 
 	}
 	return api.projectRead(ctx, vars)
 }
+
+func (api *API) triggerAnalysis(ctx context.Context, vars map[string]string) error {
+	if isHooks(ctx) {
+		return nil
+	}
+	return api.projectRead(ctx, vars)
+}
