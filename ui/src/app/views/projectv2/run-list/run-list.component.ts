aaf5fd893d224de36934c29d5a5737836030e4ea
Alexandre Boucey <alexandre.boucey@gmail.com>
2025-08-01 13:20:10 +0200

    fix(typo): worklfow -> workflow
    Signed-off-by: Alexandre Boucey <alexandre.boucey@gmail.com>

--- ui/src/app/views/projectv2/run-list/run-list.component.ts
+++ ui/src/app/views/projectv2/run-list/run-list.component.ts
@@ -282,7 +282,7 @@ export class ProjectV2RunListComponent implements OnInit, OnDestroy {
 			}
 		});
 		const drawerRef = this._drawerService.create<ProjectV2RunStartComponent, { value: string }, string>({
-			nzTitle: 'Start new worklfow run',
+			nzTitle: 'Start new workflow run',
 			nzContent: ProjectV2RunStartComponent,
 			nzContentParams: {
 				params: {
