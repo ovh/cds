f0be31d0d9456e2bd9d82fff4802b3f8f05f7a92
francois.samin <francois.samin@ovhcloud.com>
2026-02-24 13:22:30 +0000

    feat(cdn): delete orphan items
    Signed-off-by: francois.samin <francois.samin@ovhcloud.com>

--- engine/sql/cdn/019_cdn_idx_orphan_cleanup.sql
+++ engine/sql/cdn/019_cdn_idx_orphan_cleanup.sql
@@ -1,4 +1,4 @@
--- +migrate Up
+-- +migrate Up notransaction
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_item_v1_orphan_cleanup
 ON item (created ASC)
 WHERE type IN ('step-log', 'service-log', 'run-result')
