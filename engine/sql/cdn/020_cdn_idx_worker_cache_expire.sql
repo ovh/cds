a8dc589d9a70ef198f76b71c7d0b2b83688ec632
francois.samin <francois.samin@ovhcloud.com>
2026-05-04 12:07:45 +0000

    feat(cdn): purge CDN worker-cache
    Signed-off-by: francois.samin <francois.samin@ovhcloud.com>

--- engine/sql/cdn/020_cdn_idx_worker_cache_expire.sql
+++ engine/sql/cdn/020_cdn_idx_worker_cache_expire.sql
@@ -1,6 +1,6 @@
 -- +migrate Up notransaction
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_item_worker_cache_expire
-ON item (((api_ref->>'expire_at')::timestamptz) ASC, created ASC)
+ON item (created ASC)
 WHERE type IN ('worker-cache', 'worker-cache-v2')
   AND to_delete = false;
 
