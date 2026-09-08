9ad83c2adb8149a918d750d88b295e90a252b916
steven.guiheux <steven.guiheux@ovhcloud.com>
2026-09-08 08:16:59 +0000

    feat: update entity dao to manage initiator

--- engine/api/entity/gorp_model.go
+++ engine/api/entity/gorp_model.go
@@ -15,9 +15,19 @@ type dbEntity struct {
 	gorpmapper.SignedEntity
 }
 
+// InitiatorCanonical is the owner identity bound into the signature. A stored owner always yields a
+// value ("||" when nobody is identified) so it cannot be confused with a NULL, not yet migrated, column.
+func (v dbEntity) InitiatorCanonical() string {
+	if v.Initiator == nil {
+		return ""
+	}
+	return v.Initiator.UserID + "|" + v.Initiator.VCS + "|" + v.Initiator.VCSUsername
+}
+
 func (v dbEntity) Canonical() gorpmapper.CanonicalForms {
-	_ = []interface{}{v.ID, v.Name, v.ProjectKey, v.ProjectRepositoryID, v.Type, v.Ref, v.Commit, v.Data}
+	_ = []interface{}{v.ID, v.Name, v.ProjectKey, v.ProjectRepositoryID, v.Type, v.Ref, v.Commit, v.Data, v.InitiatorCanonical()}
 	return []gorpmapper.CanonicalForm{
+		"{{.ID}}{{.Name}}{{.ProjectKey}}{{.ProjectRepositoryID}}{{.Type}}{{.Ref}}{{.Commit}}{{md5sum .Data}}{{.InitiatorCanonical}}",
 		"{{.ID}}{{.Name}}{{.ProjectKey}}{{.ProjectRepositoryID}}{{.Type}}{{.Ref}}{{.Commit}}{{md5sum .Data}}",
 		"{{.ID}}{{.Name}}{{.ProjectKey}}{{.ProjectRepositoryID}}{{.Type}}{{.Ref}}{{.Commit}}{{hash .Data}}",
 	}
